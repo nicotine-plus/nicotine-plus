7b740cface0068c2965cf0f14d1362d2f427c48d
Osiris <osiris.contact@gmail.com>
2006-10-12 08:47:10 +0000

    exe tools added.

---
+++ tools/make_exe.bat
@@ -0,0 +1,48 @@
+@echo off
+echo    -----------------------------------------------
+echo    Generate  win32 python excutable for WindowsXP
+echo    Copyright (C) Vandy Omall 2006  
+echo.
+echo    This program is free software; you can redistribute it and/or modify
+echo    it under the terms of the GNU General Public License as published by
+echo    the Free Software Foundation; either version 2 of the License, or
+echo    (at your option) any later version.
+echo.
+echo    This program is distributed in the hope that it will be useful,
+echo    but WITHOUT ANY WARRANTY; without even the implied warranty of
+echo    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+echo    GNU General Public License for more details.
+echo.
+echo    You should have received a copy of the GNU General Public License
+echo    along with this program; if not, write to the Free Software
+echo    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+echo    -----------------------------------------------
+echo.
+echo    First we going to check wheter dependencies are met.
+echo    Required Dependencies are: Python 2.x, PyGTK, Pysco, 
+echo    Pycairo, Pyexe
+echo    Optional are PyOgg, PyVorbis
+echo.
+pause   "Hit a key to continue"
+IF EXIST C:\Python23\pythonw.exe (echo [V] Python 2.3) ELSE (echo [X] Python 2.3)
+IF EXIST C:\Python24\pythonw.exe (echo [V] Python 2.4) ELSE (echo [X] Python 2.4)
+IF EXIST C:\Python24\Removepy2exe.exe (echo [V] Pyexe) ELSE (echo [X] Pyexe)
+IF EXIST C:\Python24\Removepygtk.exe (echo [V] PyGTK) ELSE (echo [X] PyGTK)
+IF EXIST C:\Python24\Removepycairo.exe (echo [V] Pycairo) ELSE (echo [X] Pycairo)
+IF EXIST C:\Python24\Removepsyco.exe (echo [V] Psyco) ELSE (echo [X] Psyco)
+IF EXIST C:\Python24\Removepyogg.exe (echo [V] PyOGG) ELSE (echo [X] PyOGG 2.4)
+IF EXIST C:\Python24\Removepyvorbis.exe (echo [V] Pyvorbis) ELSE (echo [X] Pyvorbis)
+echo.
+echo    [V] FOUND [X] NOT FOUND!
+echo    If you miss something, close this window and install.
+echo    If everything is oke hit a key to continue.
+echo.
+pause   "Hit a key to continue"
+echo   Clean /build from previous generate
+echo.
+rd /S /Q build
+echo  Done...
+echo  Make Python app excutable
+c:\Python24\python setup_win32.py py2exe -O2
+rem
+pause
---
+++ tools/make_exe.py
@@ -0,0 +1,74 @@
+## Generorate win32 excutable for WindowsXP
+## Useage: python setup_win32.py py2exe -O2
+## This file contains snippets from various sources.
+## Copyright (C) 2003-2006 Yann Le Boulanger <asterix@lagaule.org>
+## Copyright (C) 2005-2006 Nikos Kouremenos <kourem@gmail.com>
+## Copyright (C) 2003-2004 Hyriand <hyriand@thegraveyard.org>
+## Copyright (C) daelstorm <daelstorm@gmail.com>
+## Copyright (C) 2006 Vandy Omall <osiris.contact@gmail.com>
+##
+##  This program is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation; either version 2 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program; if not, write to the Free Software
+##  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import sys
+import os
+import glob
+
+from distutils.core import setup
+if sys.platform.startswith("win"):
+  try:
+    import py2exe
+  except ImportError:
+    pass
+
+if __name__ == '__main__' :
+    LONG_DESCRIPTION = \
+""" Nicotine-Plus is a client for SoulSeek filesharing system, forked from Nicotine. """
+
+    from pynicotine.utils import version
+
+    setup(name                  = "nicotine",
+          version               = version,
+          license               = "GPL",
+          description           = "Client for SoulSeek filesharing system.",
+          author                = "daelstorm",
+          author_email          = "daelstorm@gmail.com",
+          url                   = "http://nicotine-plus.sourceforge.net/",
+          packages              = [ 'pynicotine', 'pynicotine.gtkgui' ],
+          scripts               = [ 'nicotine'],
+          long_description      = LONG_DESCRIPTION,
+          windows               = [{'script': 'nicotine', 'icon_resources': [(1, 'nicotine.ico')]}],
+          options               = {
+    'py2exe': {
+        'includes': 'cairo,pango,pangocairo,atk,gobject,psyco,ogg',
+        'dll_excludes': [
+            'iconv.dll','intl.dll',
+            'libatk-1.0-0.dll',
+            'libgdk_pixbuf-2.0-0.dll',
+            'libgdk-win32-2.0-0.dll',
+            'libglib-2.0-0.dll',
+            'libgmodule-2.0-0.dll',
+            'libgobject-2.0-0.dll',
+            'libgthread-2.0-0.dll',
+            'libgtk-win32-2.0-0.dll',
+            'libpango-1.0-0.dll',
+            'libpangowin32-1.0-0.dll',
+            'libpangocairo-1.0-0.dll',
+            'libfontconfig-1.dll',
+            'libcairo-2.dll',
+            'freetype6.dll',
+            'libxml2.dll'
+
+            ],}}
+          )
