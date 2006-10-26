279a433f66ec6c78db4d4c77fb7a2b5bf41f1efc
Daelstorm <daelstorm@gmail.com>
2006-10-26 16:09:44 +0000

    setup.py add manpages to install

--- setup.py
+++ setup.py
@@ -29,6 +29,10 @@ doc_files = glob.glob(os.path.join("doc", "*"))
 for file in doc_files:
 	files.append((os.path.join(sys.prefix, "share", "nicotine", "documentation"), [file]))
 files.append((os.path.join(sys.prefix, "share", "applications"), ["files/nicotine.desktop"]))
+# Manuals
+manpages = glob.glob(os.path.join("manpages", "*.1"))
+for man in manpages:
+	files.append((os.path.join(sys.prefix, "share", "man", "man1"), [man]))
 files.append((os.path.join(sys.prefix, "share", "pixmaps"), ["files/nicotine-plus-32px.png"]))
 if sys.platform.startswith("win"):
   try:
