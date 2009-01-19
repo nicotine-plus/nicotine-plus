1a4799890e7e7c409f9de611a260f22587f6e32c
Quinox <quinox@users.sf.net>
2009-01-19 21:39:13 +0000

    Added private messages to the merged view, renamed tab to "Joined Rooms " (which isn't accurate anymore)

--- plugins/99-mergedview.py
+++ plugins/99-mergedview.py
@@ -7,10 +7,11 @@ from pynicotine import slskmessages
 class Plugin(BasePlugin):
     __name__ = "Merged Chat"
     __version__ = "2009-01-18r00"
-    __mergeroom__ = "MergeView " # last space is important!
+    __mergeroom__ = "JoinedRooms " # last space is important!
     def init(self):
         self.roomsctrl = self.frame.chatrooms.roomsctrl
         self.active = True
+        self.lastmessage = None
     def _createIfNeeded(self):
         if self.__mergeroom__ not in self.roomsctrl.joinedrooms:
             self.room = ChatRoom(self.roomsctrl, self.__mergeroom__, {}, meta = True)
@@ -33,34 +34,55 @@ class Plugin(BasePlugin):
         return returncode['zap']
     def ServerDisconnectNotification(self):
         self.log('Were disconnected, merge needs to be destroyed')
-    def IncomingPublicChatEvent(self, room, user, text):
+    def IncomingPublicChatNotification(self, room, user, text):
         if not self.active:
             return
         if room != self.__mergeroom__:
             self._createIfNeeded()
-            if not self.fakepublic(self.__mergeroom__, self.join(room, user), text):
-                self.log('Failed to fake message!')
+            self.fakepublic(self.__mergeroom__, self.joinPublic(room, user), text)
+            self.lastmessage = ('Public', room)
+    def IncomingPrivateChatNotification(self, user, text):
+        self._createIfNeeded()
+        self.fakepublic(self.__mergeroom__, self.joinPrivate(user, self.parent.myUsername), text)
+        self.lastmessage = ('Private', user)
     def OutgoingPublicChatEvent(self, room, text):
         if not self.active:
             return
-        self._createIfNeeded()
-        if room == self.__mergeroom__:
-            (realroom, sep, realtext) = text.partition(': ')
-            if room in self.roomsctrl.joinedrooms:
-                self.saypublic(realroom, realtext)
-                self.fakepublic(self.__mergeroom__, self.join(realroom, self.parent.myUsername), realtext)
+        if room != self.__mergeroom__:
+            return
+        (realroom, sep, realtext) = text.partition(': ')
+        if realroom in self.roomsctrl.joinedrooms:
+            self.saypublic(realroom, realtext)
+            self.fakepublic(self.__mergeroom__, self.joinPublic(realroom, self.parent.myUsername), realtext)
+        else:
+            if self.lastmessage:
+                (type, destination) = self.lastmessage
+                if type == 'Public':
+                    self.saypublic(destination, text)
+                    self.fakepublic(self.__mergeroom__, self.joinPublic(destination, self.parent.myUsername), realtext)
+                elif type == 'Private':
+                    self.sayprivate(destination, text)
+                    self.fakepublic(self.__mergeroom__, self.joinPrivate(destination, self.parent.myUsername), realtext)
+                else:
+                    self.log("EEK, programming error. (%s, %s)" % self.lastmessage
             else:
-                self.log("You are not in room '%s'!" % (realroom,))
-            return returncode['zap']
+                self.log("Nobody spoke so far, who am I supposed to send it to?")
+        return returncode['zap']
     def OutgoingPublicChatNotification(self, room, text):
         if not self.active:
             return
         if room != self.__mergeroom__:
-            self.fakepublic(self.__mergeroom__, self.join(room, self.parent.myUsername), text)
+            self._createIfNeeded()
+            self.fakepublic(self.__mergeroom__, self.joinPublic(room, self.parent.myUsername), text)
+    def OutgoingPrivateChatNotification(self, user, text):
+        self._createIfNeeded()
+        self.fakepublic(self.__mergeroom__, self.joinPrivate(self.parent.myUsername, user), text)
     def LeaveChatroomNotification(self, room):
         if room == self.__mergeroom__:
             self.active = False
             self.roomsctrl.LeaveRoom(slskmessages.LeaveRoom(room)) # Faking protocol msg
-    def join(self, room, user):
-        return "%s | %s" % (room, user)
+    def joinPublic(self, room, user):
+        return "public %s | %s" % (room, user)
+    def joinPrivate(self, source, destination):
+        return "private %s -> %s" % (source, destination)
     __publiccommands__ = [('mergeview', createMergeview)]
