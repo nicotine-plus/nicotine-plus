e74a9bc02374497ef6bc54ab5e8483457558f408
Quinox <quinox@users.sf.net>
2009-05-25 22:48:52 +0000

    Updated changelog, removed 'svn' from version string

--- pynicotine/utils.py
+++ pynicotine/utils.py
@@ -25,7 +25,7 @@ This module contains utility fuctions.
 
 from __future__ import division
 
-version = "1.2.12svn"
+version = "1.2.12"
 latesturl = "http://nicotine-plus.sourceforge.net/LATEST"
 
 import string
