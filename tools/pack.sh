4a31d4220ee671f7582c055338d4efd7b2b269ff
Quinox <quinox@users.sf.net>
2009-09-22 10:41:13 +0000

    Adding script to pack source code on release

---
+++ tools/pack.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+die() {
+	echo "Failure: $1"
+	exit 1
+}
+
+cd "${PWD%%/tools}" # if we're in /tools we go 1 up, otherwise we stay were we are
+
+VERSION=`sed -n "/^version \?=/{s/.* //;s/^['\"]//;s/['\"]$//;p}" pynicotine/utils.py`
+[ -z "$VERSION" ] && die "Could not retrieve version number"
+[ "$VERSION" != "${VERSION/svn/}" ] && die "It seems your repository still carries the SVN tag in the version: $VERSION"
+
+TMPDIR="/tmp/nic_$RANDOM"
+mkdir "$TMPDIR" || die "Failed to create temp dir $TMPDIR"
+
+NICDIR="nicotine+-$VERSION"
+SRCDIR="$TMPDIR/$NICDIR"
+
+echo "Exporting SVN..."
+svn export . "$SRCDIR" || die "Failed to export SVN to $SRCDIR"
+
+cd "$TMPDIR"
+
+echo "Tarring (BZip2)..."
+tar -cjf "nicotine+-$VERSION.tar.bz2" "$NICDIR" || die "Failed to create BZip2"
+echo "Tarring (GZip)..."
+tar -czf "nicotine+-$VERSION.tar.gz" "$NICDIR" || die "Failed to create GZip"
+
+echo "Nicotine+ $VERSION packed in $TMPDIR"
+
