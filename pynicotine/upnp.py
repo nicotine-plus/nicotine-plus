9efa6096559c0d4ceb8d47f48449ef4ab1cc6050
Quinox <quinox@users.sf.net>
2009-09-29 17:35:33 +0000

    Added support for MiniUPnP's binary

--- pynicotine/upnp.py
+++ pynicotine/upnp.py
@@ -17,9 +17,39 @@
 from time import sleep
 
 from logfacility import log
-from utils import _
+from utils import _, executeCommand
 
-def fixportmapping(internallanport):
+miniupnpc = None
+try:
+	import miniupnpc
+except ImportError, e:
+	miniupnpc_error = str(e)
+
+def fixportmapping(internallanport, externallanport = None):
+	if not externallanport:
+		externallanport = internallanport
+	if miniupnpc:
+		return miniupnpcmodule(internallanport, externallanport)
+	log.addwarning(_('MiniUPnPc module was not imported: %(error)s. Trying binary...') % {'error':miniupnpc_error})
+	return miniupnpcbinary(internallanport, externallanport)
+def miniupnpcbinary(internallanport, externallanport):
+	command = "upnpc -r %s %s" % (internallanport, externallanport)
+	try:
+		output = executeCommand(command, returnoutput=True)
+	except RuntimeError, e:
+		log.addwarning('Failed to use UPnPc binary: %s' % (str(e),))
+		return
+	for line in output.split('\n'):
+		# "external %s:%s %s is redirected to internal %s:%s\n"
+		if line.startswith("external ") and line.find(" is redirected to internal ") > -1:
+			lst = line.split()
+			external = lst[1].split(':')
+			if len(external) == 2 and len(internal) == 2:
+				return (external[0], external[1])
+	log.addwarning('UPnPc output, could not parse output: %s' % (output,))
+	return None
+
+def miniupnpcmodule(internallanport, externallanport):
 	try:
 		import miniupnpc
 	except ImportError, e:
@@ -37,21 +67,21 @@ def fixportmapping(internallanport):
 		print "Selecting one of the IGD. Local address: %s External address: %s" % (lanaddr, externalipaddress)
 		print "Misc: %s\n%s" % (u.statusinfo(), u.connectiontype())
 		print "Selecting random external port..."
-		eport = 15000
-		r = u.getspecificportmapping(eport, 'TCP')
+		externallanport = 15000
+		r = u.getspecificportmapping(externallanport, 'TCP')
 		print "Initial r: %s" % (str(r),)
 		while r != None:
-			eport = eport + 1
-			if eport > 65535:
+			externallanport += 1
+			if externallanport > 65535:
 				print "Failed to find a suitable external port, bailing."
 				return
-			r = u.getspecificportmapping(eport, 'TCP')
+			r = u.getspecificportmapping(externallanport, 'TCP')
 			print "Conseq. r: %s" % (str(r),)
-		print "trying to redirect %s port %u TCP => %s port %u TCP" % (externalipaddress, eport, lanaddr, internallanport)
-		b = u.addportmapping(eport, 'TCP', lanaddr, internallanport, 'Nicotine+', '')
+		print "trying to redirect %s port %u TCP => %s port %u TCP" % (externalipaddress, externallanport, lanaddr, internallanport)
+		b = u.addportmapping(externallanport, 'TCP', lanaddr, internallanport, 'Nicotine+', '')
 		if b:
 			print "Success?"
-			return (externalipaddress, eport)
+			return (externalipaddress, externallanport)
 		else:
 			print "Failed?"
 			return
