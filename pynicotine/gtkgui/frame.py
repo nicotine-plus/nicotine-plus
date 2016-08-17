3bc8b9246cfa0ae69530cd9e875c7c38856ddaa4
Michael Labouebe <gfarmerfr@free.fr>
2016-08-17 08:18:53 +0200

    Update UI links
    - Drop the link to the online NicotineGuide since we don't have access to it.
    - Update Project Website (previously sourceforge) to point to the wiki.
    - Replace the wiki link with the github page.

--- pynicotine/gtkgui/frame.py
+++ pynicotine/gtkgui/frame.py
@@ -2489,16 +2489,12 @@ class NicotineFrame:
 		else:
 			popupWarning(None, _("Cannot Find Guide"), _("The Nicotine Offline Guide ( NicotineGuide.htm ) was not found in either the following directories:\n\n<u>%(pwd)s\n</u><b>and</b>\n<u>%(prefix)s/share/nicotine/documentation/</u>\n\nEither install Nicotine-Plus, or start from inside the Nicotine-Plus source directory." % {'pwd':path1, 'prefix':sys.prefix } ) )
 	
-	def OnOnlineNicotineGuide(self, widget):
-		url = "http://nicotine-plus.sourceforge.net/NicotinePlusGuide/"
-		OpenUri(url)
-			
-	def OnSourceForgeProject(self, widget):
-		url = "http://sourceforge.net/projects/nicotine-plus/"
-		OpenUri(url)
-		
-	def OnTrac(self, widget):
+	def OnProjectWebsite(self, widget):
 		url = "http://nicotine-plus.org/"
+		OpenUri(url)
+
+	def onProjectGithubPage(self, widget):
+		url = "https://github.com/eLvErDe/nicotine-plus"
 		OpenUri(url)
 
 	def OnCheckLatest(self, widget):
