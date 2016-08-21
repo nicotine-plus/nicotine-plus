a82d80f6e01b6f0259369431213f21b6c9fa3b5b
Michael Labouebe <gfarmerfr@free.fr>
2016-08-21 14:54:47 +0200

    Setuptools bdist_rpm wasn't workinh since a long time: fix this

---
+++ tools/install-rpm.sh
@@ -0,0 +1,4 @@
+# Script needed because of this bug: https://bugs.python.org/issue644744
+python setup.py install --root=$RPM_BUILD_ROOT --record=INSTALLED_FILES
+# 'brp-compress' gzips the man pages without distutils knowing... fix this
+sed -i -e 's@man/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/man\1/\2.gz@g' INSTALLED_FILES
