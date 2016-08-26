73adb25b9c953f2b6ef83e709b9a5483b8ac51c0
Michael Labouebe <gfarmerfr@free.fr>
2016-08-26 14:29:46 +0200

    Cleanup copyright headers + code lisibility improvement for encode_bitmaps.py

--- tools/encode_bitmaps.py
+++ tools/encode_bitmaps.py
@@ -1,46 +1,74 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# COPYRIGHT (c) 2016 Michael Labouebe <gfarmerfr@free.fr>
+# COPYRIGHT (c) 2009-2010 Quinox <quinox@users.sf.net>
+# COPYRIGHT (C) 2006-2009 Daelstorm <daelstorm@gmail.com>
+#
+# GNU GENERAL PUBLIC LICENSE
+#    Version 3, 29 June 2007
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
-import sys, string
+import sys
+import string
 from os.path import isfile
 
 table = [
-	["away.png", "away"],
-	["online.png", "online"],
-	["offline.png", "offline"],
-	["connect.png", "connect"],
-	["disconnect.png", "disconnect"],
-	["away2.png", "away2"],
-	["empty.png", "empty"],
-	["hilite.png", "hilite"],
-	["hilite2.png", "hilite2"],
-	["hilite3.png", "hilite3"],
-	["bug.png", "bug"],
-	["money.png", "money"],
-	["plugin.png", "plugin"],
-	["nicotine+.png", "nicotinen"],
-	["n.png", "n"],
-	["notify.png", "notify"],
-	["notify.svg", "notify_vector"],
+    ["away.png", "away"],
+    ["online.png", "online"],
+    ["offline.png", "offline"],
+    ["connect.png", "connect"],
+    ["disconnect.png", "disconnect"],
+    ["away2.png", "away2"],
+    ["empty.png", "empty"],
+    ["hilite.png", "hilite"],
+    ["hilite2.png", "hilite2"],
+    ["hilite3.png", "hilite3"],
+    ["bug.png", "bug"],
+    ["money.png", "money"],
+    ["plugin.png", "plugin"],
+    ["nicotine+.png", "nicotinen"],
+    ["n.png", "n"],
+    ["notify.png", "notify"],
+    ["notify.svg", "notify_vector"],
 ]
+
 flagtable = []
 for name in os.listdir(os.path.join("img", "geoip")):
-	p = os.path.join("img", "geoip", name)
-	if isfile(p):
-		flagtable.append((os.path.join("img", "geoip", name), 'flag_%s' % name[:2].upper()))
+    p = os.path.join("img", "geoip", name)
+    if isfile(p):
+        flagtable.append((os.path.join("img", "geoip", name), 'flag_%s' % name[:2].upper()))
+
 missing = os.path.join("img", "missingflag.png")
 if isfile(missing):
-	flagtable.append(((missing), 'flag_'))
+    flagtable.append(((missing), 'flag_'))
 
-outf = open(os.path.join("pynicotine","gtkgui","imagedata.py"), "w")
+outf = open(os.path.join("pynicotine", "gtkgui", "imagedata.py"), "w")
+
 for image in table:
-	print image[0]
-	f = open(os.path.join("img", image[0]), "rb")
-	d = f.read()
-	f.close()
-	outf.write("%s = %s\n" % (image[1], `d`))
+    print image[0]
+    f = open(os.path.join("img", image[0]), "rb")
+    d = f.read()
+    f.close()
+    outf.write("%s = %s\n" % (image[1], `d`))
+
 for image in flagtable:
-	print image[0]
-	f = open(image[0], "rb")
-	outf.write("%s = %r\n" % (image[1], f.read()))
-	f.close()
+    print image[0]
+    f = open(image[0], "rb")
+    outf.write("%s = %r\n" % (image[1], f.read()))
+    f.close()
+
 outf.close()
