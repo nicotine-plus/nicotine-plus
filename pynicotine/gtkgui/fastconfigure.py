dc0f297a8096242648e1bff112f366508a795da7
Michael Labouebe <gfarmerfr@free.fr>
2016-10-23 00:53:56 +0200

    Fix defining ports in fastconfigure: GtkAdjustment return floats that must be converted to ints

--- pynicotine/gtkgui/fastconfigure.py
+++ pynicotine/gtkgui/fastconfigure.py
@@ -208,8 +208,8 @@ class FastConfigureAssistant(object):
 
         # portpage
         self.config.sections['server']['portrange'] = (
-            self.kids['lowerport'].get_value(),
-            self.kids['upperport'].get_value()
+            int(self.kids['lowerport'].get_value()),
+            int(self.kids['upperport'].get_value())
         )
         self.config.sections['server']['upnp'] = self.kids['useupnp'].get_active()
         self.config.sections['server']['firewalled'] = not self.kids['portopen'].get_active()
