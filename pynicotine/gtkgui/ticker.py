176261c9464f376ac5064b07459de3b0ae3cd428
Michael Labouebe <gfarmerfr@free.fr>
2016-12-30 19:49:42 +0100

    Code legibility improvement

--- pynicotine/gtkgui/ticker.py
+++ pynicotine/gtkgui/ticker.py
@@ -1,4 +1,12 @@
-# Copyright (C) 2007 daelstorm. All rights reserved.
+# -*- coding: utf-8 -*-
+#
+# COPYRIGHT (C) 2016 Michael Labouebe <gfarmerfr@free.fr>
+# COPYRIGHT (C) 2009 Quinox <quinox@users.sf.net>
+# COPYRIGHT (C) 2006-2009 Daelstorm <daelstorm@gmail.com>
+# COPYRIGHT (C) 2003-2004 Hyriand <hyriand@thegraveyard.org>
+#
+# GNU GENERAL PUBLIC LICENSE
+#    Version 3, 29 June 2007
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -12,73 +20,84 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Original copyright below
-# Copyright (c) 2003-2004 Hyriand. All rights reserved.
 
 import gtk
 import gobject
 
+
 class Ticker(gtk.EventBox):
-	def __init__(self):
-		gtk.EventBox.__init__(self)
-		self.entry = gtk.Entry()
-		self.entry.set_editable(False)
-		self.entry.set_has_frame(False)
-		self.entry.show()
-		self.add(self.entry)
-		self.messages = {}
-		self.sortedmessages = []
-		self.ix = 0
-		self.source = None
-		self.enable()
 
-	def __del__(self):
-		gobject.source_remove(self.source)
+    def __init__(self):
 
-	def scroll(self, *args):
-		if not self.messages:
-			self.entry.set_text("")
-			return True
-		if self.ix >= len(self.messages):
-			self.ix = 0
-		(user, message) = self.sortedmessages[self.ix]
-		self.entry.set_text("[%s]: %s" % (user, message))
-		self.ix += 1
-		return True
-		
-	def add_ticker(self, user, message):
-		message = message.replace("\n", " ")
-		self.messages[user] = message
-		self.updatesorted()
+        gtk.EventBox.__init__(self)
 
-	def remove_ticker(self, user):
-		try:
-			del self.messages[user]
-		except KeyError:
-			return
-		self.updatesorted()
+        self.entry = gtk.Entry()
+        self.entry.set_editable(False)
+        self.entry.set_has_frame(False)
+        self.entry.show()
+        self.add(self.entry)
+        self.messages = {}
+        self.sortedmessages = []
+        self.ix = 0
+        self.source = None
+        self.enable()
 
-	def updatesorted(self):
-		lst = [(user, msg) for user, msg in self.messages.iteritems()]
-		lst.sort(cmp=lambda x,y: len(x[1])-len(y[1]))
-		self.sortedmessages = lst
+    def __del__(self):
+        gobject.source_remove(self.source)
 
-	def get_tickers(self):
-		return [x for x in self.messages.iteritems()]
-	def set_ticker(self, msgs):
-		self.messages = msgs
-		self.ix = 0
-		self.scroll()
+    def scroll(self, *args):
 
-	def enable(self):
-		if self.source:
-			return
-		self.source = gobject.timeout_add(2500, self.scroll)
+        if not self.messages:
+            self.entry.set_text("")
+            return True
 
-	def disable(self):
-		if not self.source:
-			return
-		gobject.source_remove(self.source)
-		#self.entry.set_text("")
-		self.source = None
+        if self.ix >= len(self.messages):
+            self.ix = 0
+
+        (user, message) = self.sortedmessages[self.ix]
+        self.entry.set_text("[%s]: %s" % (user, message))
+        self.ix += 1
+
+        return True
+
+    def add_ticker(self, user, message):
+        message = message.replace("\n", " ")
+        self.messages[user] = message
+        self.updatesorted()
+
+    def remove_ticker(self, user):
+
+        try:
+            del self.messages[user]
+        except KeyError:
+            return
+
+        self.updatesorted()
+
+    def updatesorted(self):
+        lst = [(user, msg) for user, msg in self.messages.iteritems()]
+        lst.sort(cmp=lambda x, y: len(x[1])-len(y[1]))
+        self.sortedmessages = lst
+
+    def get_tickers(self):
+        return [x for x in self.messages.iteritems()]
+
+    def set_ticker(self, msgs):
+        self.messages = msgs
+        self.ix = 0
+        self.scroll()
+
+    def enable(self):
+
+        if self.source:
+            return
+
+        self.source = gobject.timeout_add(2500, self.scroll)
+
+    def disable(self):
+
+        if not self.source:
+            return
+
+        gobject.source_remove(self.source)
+        self.source = None
