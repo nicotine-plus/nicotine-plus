nicotine-plus/nicotine-plus

commit 40a0d9210955dcbe43700ad9b7e400477de20b79

Add Icon Theme Directory selector to UI Settings; Replaces away, online, offline images with new blue, yellow and red birdies.

pynicotine/gtkgui/frame.py
@@ -27,8 +27,7 @@ from userbrowse import UserBrowse
 from settingswindow import SettingsWindow
 from about import *
 from checklatest import checklatest
-
-import imagedata
+from pynicotine.config import *
 import utils
 from utils import AppendLine, ImageLabel, IconNotebook, ScrollBottom, PopupMenu, Humanize
 import translux
@@ -60,17 +59,39 @@ class testwin(MainWindow):
 	def __init__(self, config):
 		self.images = {}
 		self.clip_data = ""
-		for i in "empty", "away", "online", "offline", "hilite", "hilite2", "connect", "disconnect", "away2", "n":
-			loader = gtk.gdk.PixbufLoader("png")
-			data = getattr(imagedata, i)
-			loader.write(data, len(data))
-			loader.close()
-			self.images[i] = loader.get_pixbuf()
-
+		self.configfile = config
+		
+		
 		self.chatrooms = None
 		
 		self.got_focus = False
-
+		config2 = Config(config)
+        	config2.readConfig()
+		for i in "empty", "away", "online", "offline", "hilite", "hilite2", "connect", "disconnect", "away2", "n":
+			loader = gtk.gdk.PixbufLoader("png")
+			if "icontheme" in config2.sections["ui"]:
+				path = os.path.expanduser(os.path.join(config2.sections["ui"]["icontheme"], i +".png"))
+				if os.path.exists(path):
+					data = open(path)
+					s = data.read()
+					loader.write(s, len(s))
+					data.close()
+					del s
+				else:
+					# default icons
+					data = getattr(imagedata, i)
+					loader.write(data, len(data))
+			else:
+				# default icons
+				data = getattr(imagedata, i)
+				loader.write(data, len(data))
+			
+			
+			loader.close()
+			self.images[i] = loader.get_pixbuf()
+		del data
+		del config2
+		
 		MainWindow.__init__(self)
 		self.MainWindow.set_title(_("Nicotine+") + " " + version)
 		self.MainWindow.set_icon(self.images["n"])
@@ -80,7 +101,8 @@ class testwin(MainWindow):
 		
 		self.logpopupmenu = PopupMenu(self).setup([_("Clear log"), self.OnClearLogWindow])
 		
-		self.configfile = config
+		
+		self.importimages()
 		self.transfermsgs = {}
 		self.transfermsgspostedtime = 0
 		self.manualdisconnect = 0
@@ -181,6 +203,8 @@ class testwin(MainWindow):
 		utils.PROTOCOL_HANDLERS = self.np.config.sections["urls"]["protocols"].copy()
 		utils.PROTOCOL_HANDLERS["slsk"] = self.OnSoulSeek
 
+
+		
 		for thing in self.np.config.sections["interests"]["likes"]:
 			self.likes[thing] = self.likeslist.append([thing])
 		for thing in self.np.config.sections["interests"]["dislikes"]:
@@ -265,6 +289,12 @@ class testwin(MainWindow):
 		else:
 			self.OnConnect(-1)
 			
+	def importimages(self):
+		try:
+			import imagedata
+		except Exception, e:
+			print e
+		
 	def create_trayicon(self):
 		self.is_mapped = 1
 		self.trayicon = trayicon.TrayIcon("Nicotine")

img/README
@@ -14,7 +14,13 @@ hilite2.png
 Not modified:
 
 hilite.png (renamed from lightbulb.png)
-online.png
-offline.png
+
+Created by daelstorm (released under GPL v2)
 away.png
+online.png
+n.png
+nicotine+.png
+offline.png
+birdies.xcf
+
 

pynicotine/config.py
@@ -62,7 +62,7 @@ class Config:
 	"searches":{"maxresults":50,"re_filter":0,"history":[], \
 	"enablefilters":0, "defilter":["","","","",0,""], "filtercc":[], \
 	"filterin":[],"filterout":[],"filtersize":[],"filterbr":[]}, \
- 	"ui":{"chatme":"FOREST GREEN", "chatremote":"","chatlocal":"BLUE", \
+ 	"ui":{"icontheme": "", "chatme":"FOREST GREEN", "chatremote":"","chatlocal":"BLUE", \
  	"chathilite":"red", "search":"","searchq":"GREY", "decimalsep":",", \
  	"roomlistcollapsed": 0, "tabclosers": 1}, \
  	"urls":{"urlcatching":1,"protocols":{"http":"mozilla \"%s\" &"}, \

commit ee8faa9897f099077917ff7125eeaf5100b9fda8

Fixes loading of images from the launch directory (#113)
The ui/icontheme which defaults as the empty string, was being evaluated
as to whether it existed (as opposed to was non-empty). Thus the code
always ran, and the path for a theme was constructed using the default
empty string ("") + the image path for the icon.

This meant if there was path-local resolvable images of the same name
and no theme, they would get dropped into the interface as if it was
a theme.

The fix is to use `dict.__getitem__'s` shortform of `dict.get` which
evaluates the empty string as a bottom value, as intended.

This bug appears to have been introduced in 40a0d921 when the icontheme
was first implemented in 2006.

pynicotine/gtkgui/frame.py
@@ -794,7 +794,7 @@ class NicotineFrame:
             "plugin"
         ]
 
-        if "icontheme" in self.np.config.sections["ui"]:
+        if self.np.config.sections["ui"].get("icontheme"):
             extensions = ["jpg", "jpeg", "bmp", "png", "svg"]
             for name in names:
                 path = None