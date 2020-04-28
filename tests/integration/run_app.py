fabc7a7c77beeffd5307747cb1317bfab4bf74ed
Lene Preuss <lene.preuss@gmail.com>
2020-04-28 21:29:34 +0200

    dogtail sorta works (on my laptop only)
    Condition: . is in $PATH so that run_app can be called with "nicotine" (not including the path)

--- tests/integration/run_app.py
+++ tests/integration/run_app.py
@@ -1,18 +1,19 @@
 import signal
 import sys
 
-from dogtail.config import config
-config.logDebugToFile = False
 from dogtail.procedural import *
 from dogtail.tc import TCNode, TCBool
+
 tcn = TCNode()
 
 
-def appStartup(binary, roleName = "menu item"):
-    """Launches the given binary, then checks to see that the application
-    started correctly by looking for a Node with the given roleName."""
+def app_startup(executable, role_name="menu item"):
+    """
+    Launches the given executable, then checks to see that the application
+    started correctly by looking for a Node with the given roleName.
+    """
 
-    pid = run(binary)
+    pid = run(executable)
     try:
         tcn.compare("app exists", None, focus.application.node)
         print("focus", focus)
@@ -22,8 +23,8 @@ def appStartup(binary, roleName = "menu item"):
         print("dialog", focus.dialog)
         print("window", focus.window)
         print("widget", focus.widget)
-        focus.widget.node = focus.app.node.child(roleName=roleName)
-        tcn.compare("app has a %s" % roleName, None, focus.widget.node)
+        focus.widget.node = focus.app.node.child(roleName=role_name)
+        tcn.compare("app has a %s" % role_name, None, focus.widget.node)
     finally:
         print('Killing PID', pid)
         os.kill(pid, signal.SIGTERM)
@@ -31,4 +32,4 @@ def appStartup(binary, roleName = "menu item"):
 
 if __name__ == "__main__":
     binary = sys.argv[1]
-    appStartup(binary)
+    app_startup(binary)
