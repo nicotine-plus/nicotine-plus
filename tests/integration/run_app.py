9d28a1f5822341d27e078aa5873bfbdfdfead611
Lene Preuss <lene.preuss@gmail.com>
2020-04-30 20:08:20 +0200

    Slightly more UI testing

--- tests/integration/run_app.py
+++ tests/integration/run_app.py
@@ -1,32 +1,45 @@
 import signal
-import sys
-from time import sleep
 
 from dogtail.procedural import *
-from dogtail.tc import TCNode, TCBool
+from dogtail.tc import TCNode
 
 tcn = TCNode()
+TAB_TITLES = (
+    "Buddy List", "User browse", "User info", "Search files",
+    "Uploads", "Downloads", "Private chat", "Chat rooms"
+)
 
 
-def app_startup(executable="nicotine", role_name="menu item"):
+def app_startup(executable):
     """
-    Launches the given executable, then checks to see that the application
-    started correctly by looking for a Node with the given roleName.
+    Launches the given executable, then tries finding a few UI elements
     """
 
     pid = run(executable)
     try:
         tcn.compare("app exists", None, focus.application.node)
-        focus.widget.node = focus.app.node.child(roleName=role_name)
-        tcn.compare("app has a %s" % role_name, None, focus.widget.node)
+        click_tabs(TAB_TITLES)
+        find_ui_element(focus.app.node, roleName="menu item")
     finally:
-        sleep(5)
         print('Killing PID', pid)
         os.kill(pid, signal.SIGTERM)
 
 
+def click_tabs(tab_titles):
+    tab_bar = find_ui_element(focus.app.node, roleName="page tab list")
+    for tab in tab_titles:
+        find_ui_element(tab_bar, roleName="page tab", name=tab).click()
+
+
+def find_ui_element(parent_node, **kwargs):
+    ui_element_node = parent_node.child(**kwargs)
+    focus.widget.node = ui_element_node
+    tcn.compare(
+        f"app has a {kwargs.get('roleName', '')}: {kwargs.get('name', '')}",
+        None, focus.widget.node
+    )
+    return ui_element_node
+
+
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        app_startup(sys.argv[1])
-    else:
-        app_startup()
+    app_startup("nicotine")
