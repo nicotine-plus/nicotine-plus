0d49406d97fb4a90925ad1205fd50942257f1e8a
mathiascode <mail@mathias.is>
2020-06-03 21:25:28 +0300

    Working CI test

--- test/integration/nicotine_library.py
+++ test/integration/nicotine_library.py
@@ -1,7 +1,4 @@
-import os
-import signal
-from subprocess import Popen
-from time import sleep
+import subprocess
 
 
 # noinspection PyInterpreter
@@ -12,11 +9,17 @@ class nicotine_library:  # noqa
     def running_nicotine_starts_a_process(self, command, timeout):
         if not isinstance(command, list):
             command = [command]
-        proc = Popen(command)
-        sleep(int(timeout))
-        self._result = bool(proc.pid)
-        os.kill(proc.pid, signal.SIGTERM)
-        sleep(1)
+
+        # Assume failure by default
+        exitcode = 1
+
+        try:
+            subprocess.call(command, timeout=int(timeout))
+        except subprocess.TimeoutExpired:
+            # Program was still running, success!
+            exitcode = 0
+
+        self._result = exitcode
 
     def result_should_be(self, expected):
         assert self._result == expected
