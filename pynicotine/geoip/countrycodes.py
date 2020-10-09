eb7a04fc8b175fe47bb5a80b435c1354c1272bc5
mathiascode <mail@mathias.is>
2020-10-09 17:45:31 +0300

    Show country name when requesting IP address

--- pynicotine/geoip/countrycodes.py
+++ pynicotine/geoip/countrycodes.py
@@ -1,4 +1,5 @@
-# Copyright (C) 2009 quinox. All rights reserved.
+# COPYRIGHT (C) 2020 Nicotine+ Team
+# COPYRIGHT (C) 2009 quinox. All rights reserved.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
