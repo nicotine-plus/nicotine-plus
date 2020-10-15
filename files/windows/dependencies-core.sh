cc4661b4567cab189f9c3cdd0ece637a7e76502b
mathiascode <mail@mathias.is>
2020-10-15 04:05:58 +0300

    Don't install unneeded dependencies for unit tests

--- files/windows/dependencies-core.sh
+++ files/windows/dependencies-core.sh
@@ -18,41 +18,30 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-""" This script is used to install dependencies in MinGW """
+### This script is used to install core dependencies in MinGW ###
+### These are enough to run unit tests and use non-UI code ###
 
-# Install most dependencies from the main MinGW repos
+# Install dependencies from the main MinGW repos
 pacman --noconfirm -S --needed \
-  git \
-  upx \
   mingw-w64-$ARCH-cython \
   mingw-w64-$ARCH-gcc \
-  mingw-w64-$ARCH-gspell \
-  mingw-w64-$ARCH-gtk3 \
   mingw-w64-$ARCH-miniupnpc \
-  mingw-w64-$ARCH-nsis \
-  mingw-w64-$ARCH-python3 \
-  mingw-w64-$ARCH-python3-gobject \
-  mingw-w64-$ARCH-python3-pip \
-  mingw-w64-$ARCH-python3-pytest \
-  mingw-w64-$ARCH-python3-flake8 \
+  mingw-w64-$ARCH-python \
+  mingw-w64-$ARCH-python-flake8 \
+  mingw-w64-$ARCH-python-pip \
+  mingw-w64-$ARCH-python-pytest \
   mingw-w64-$ARCH-taglib
 
-# Use pip for packages not available in MinGW repos
-pip install pep8-naming plyer semidbm
-
-# pyinstaller (we should switch back to pip once PyInstaller 4.1 is released)
-wget https://github.com/pyinstaller/pyinstaller/releases/download/v3.6/PyInstaller-3.6.tar.gz
-tar -zxvf PyInstaller-3.6.tar.gz
-cd PyInstaller-3.6/
-python setup.py install
-cd ..
-rm -rf PyInstaller-3.6/
+# Install dependencies with pip
+pip3 install \
+  pep8-naming \
+  semidbm
 
 # pytaglib
 wget https://github.com/supermihi/pytaglib/archive/v1.4.6.tar.gz
 tar -zxvf v1.4.6.tar.gz
 cd pytaglib-1.4.6/
 sed -i "/is_windows = / s/sys.platform.startswith('win')/False/" setup.py
-PYTAGLIB_CYTHONIZE=1 python setup.py install
+python setup.py install
 cd ..
 rm -rf pytaglib-1.4.6/
