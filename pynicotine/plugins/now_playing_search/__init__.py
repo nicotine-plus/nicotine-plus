c80536b86e62125c046059b70e8e3b1f7f85d2e5
mathiascode <mail@mathias.is>
2021-01-25 12:24:18 +0200

    Now Playing Search: fix minor regression

--- pynicotine/plugins/now_playing_search/__init__.py
+++ pynicotine/plugins/now_playing_search/__init__.py
@@ -27,6 +27,7 @@ class Plugin(BasePlugin):
         return (users, self.get_np(text))
 
     def get_np(self, text):
+        self.np_format = text
         now_playing = self.frame.np.now_playing.get_np(get_format=self.get_format)
 
         if now_playing:
