16576f99dfa8dfabc5a446c6d4cd72a36dc1a361
Inso-m-niaC <77669089+Inso-m-niaC@users.noreply.github.com>
2021-04-14 16:01:07 +0100

    Test Replier: incorporate ResponseThrottle, add custom response(s)

--- pynicotine/plugins/examplars/testreplier/__init__.py
+++ pynicotine/plugins/examplars/testreplier/__init__.py
@@ -1,4 +1,5 @@
-from pynicotine.pluginsystem import BasePlugin
+from pynicotine.pluginsystem import BasePlugin, ResponseThrottle
+from random import choice
 
 
 def enable(plugins):
@@ -13,7 +14,20 @@ def disable(plugins):
 
 class Plugin(BasePlugin):
     __name__ = "Test Replier"
+    settings = {
+        'replies': ['Test failed.']
+    }
+    metasettings = {
+        'replies': {
+            'description': 'Replies:',
+            'type': 'list string'}
+    }
+
+    def init(self):
+        self.throttle = ResponseThrottle(self.frame, self.__name__)
 
     def IncomingPublicChatEvent(self, room, nick, line):  # noqa
         if line.lower() == 'test':
-            self.saypublic(room, 'Test failed.')
+            if self.throttle.ok_to_respond(room, nick, line):
+                self.throttle.responded()
+                self.saypublic(room, choice(self.settings['replies']))
