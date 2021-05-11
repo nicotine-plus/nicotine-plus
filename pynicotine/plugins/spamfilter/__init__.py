becebb51222a5e3fadf4f8991f041a35b833a87c
mathiascode <mail@mathias.is>
2021-05-11 11:16:42 +0300

    Spamfilter plugin: filter messages containing phrases in chat rooms too

--- pynicotine/plugins/spamfilter/__init__.py
+++ pynicotine/plugins/spamfilter/__init__.py
@@ -24,22 +24,32 @@ class Plugin(BasePlugin):
         'minlength': {"description": 'The minimum length of a line before it\'s considered as ASCII spam', 'type': 'integer'},
         'maxdiffcharacters': {"description": 'The maximum number of different characters that is still considered ASCII spam', 'type': 'integer'},
         'maxlength': {"description": 'The maximum length of a line before it\'s considered as spam.', 'type': 'integer'},
-        'badprivatephrases': {"description": 'Things people send you in private that is spam.', 'type': 'list string'},
+        'badprivatephrases': {"description": 'Filter chat room and private messages containing the following phrases:', 'type': 'list string'},
     }
 
     def LoadNotification(self):  # noqa
         self.log('A line should be at least %s long with a maximum of %s different characters before it\'s considered ASCII spam.' % (self.settings['minlength'], self.settings['maxdiffcharacters']))
 
-    def IncomingPublicChatEvent(self, room, user, line):  # noqa
-        if len(line) >= self.settings['minlength'] and len(set(line)) < self.settings['maxdiffcharacters']:
-            self.log('Filtered ASCII spam from "%s" in room "%s"' % (user, room))
-            return returncode['zap']
-        if len(line) > self.settings['maxlength']:
-            self.log('Filtered really long line (%s characters) from "%s" in room "%s"' % (len(line), user, room))
-            return returncode['zap']
+    def check_phrases(self, user, line):
 
-    def IncomingPrivateChatEvent(self, user, line):  # noqa
         for phrase in self.settings['badprivatephrases']:
             if line.lower().find(phrase) > -1:
                 self.log("Blocked spam from %s: %s" % (user, line))
                 return returncode['zap']
+
+        return None
+
+    def IncomingPublicChatEvent(self, room, user, line):  # noqa
+
+        if len(line) >= self.settings['minlength'] and len(set(line)) < self.settings['maxdiffcharacters']:
+            self.log('Filtered ASCII spam from "%s" in room "%s"' % (user, room))
+            return returncode['zap']
+
+        if len(line) > self.settings['maxlength']:
+            self.log('Filtered really long line (%s characters) from "%s" in room "%s"' % (len(line), user, room))
+            return returncode['zap']
+
+        return self.check_phrases(user, line)
+
+    def IncomingPrivateChatEvent(self, user, line):  # noqa
+        return self.check_phrases(user, line)
