681f4ce6506e9362c6cdd2520d289755bbd2b98b
mathiascode <mail@mathias.is>
2021-08-28 01:55:55 +0300

    Fix issue where widget focus moves after the first chat autocomplete

--- pynicotine/gtkgui/widgets/textentry.py
+++ pynicotine/gtkgui/widgets/textentry.py
@@ -446,6 +446,11 @@ class ChatEntry:
         if not config.sections["words"]["tab"]:
             return False
 
+        text = self.entry.get_text()
+
+        if not text:
+            return False
+
         # "Hello there Miss<tab> how are you doing"
         # "0  3  6  9  12 15      18 21 24 27 30 33
         #   1  4  7  10 13      16 19 22 25 28 31
@@ -455,11 +460,8 @@ class ChatEntry:
         # text = Miss
         # preix = 12
         ix = self.entry.get_position()
-        text = self.entry.get_text()[:ix].split(" ")[-1]
+        text = text[:ix].split(" ")[-1]
         preix = ix - len(text)
-
-        if not text:
-            return False
 
         if not config.sections["words"]["cycle"]:
             completion, single = self.get_completion(text, self.completion_list)
