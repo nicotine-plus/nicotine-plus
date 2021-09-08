a73b86d108264b2390e592de0da9cbcd1a8d4767
mathiascode <mail@mathias.is>
2021-09-09 00:28:54 +0300

    tinytag.py: update ID3 class

--- pynicotine/metadata/tinytag.py
+++ pynicotine/metadata/tinytag.py
@@ -397,6 +397,7 @@ class ID3(TinyTag):
         'TPOS': 'disc',
         'TPE2': 'albumartist', 'TCOM': 'composer',
         'WXXX': 'extra.url',
+        'TSRC': 'extra.isrc',
         'TXXX': 'extra.text',
         'TKEY': 'extra.initial_key',
         'USLT': 'extra.lyrics',
@@ -455,7 +456,7 @@ class ID3(TinyTag):
     def __init__(self, filehandler, filesize, *args, **kwargs):
         TinyTag.__init__(self, filehandler, filesize, *args, **kwargs)
         # save position after the ID3 tag for duration mesurement speedup
-        self._bytepos_after_id3v2 = 0
+        self._bytepos_after_id3v2 = None
 
     @classmethod
     def set_estimation_precision(cls, estimation_in_seconds):
@@ -506,6 +507,13 @@ class ID3(TinyTag):
         return frames, byte_count, toc, vbr_scale
 
     def _determine_duration(self, fh):
+        if self._bytepos_after_id3v2 is None:
+            # tag reading was disabled, figure out the correct file position here
+            header = self._get_id3v2_header(fh)
+            if header:
+                self._bytepos_after_id3v2 = self._calc_size(header[4:8], 7)
+            else:
+                self._bytepos_after_id3v2 = 0
         max_estimation_frames = (ID3._MAX_ESTIMATION_SEC * 44100) // ID3.samples_per_frame
         frame_size_accu = 0
         header_bytes = 4
@@ -590,12 +598,18 @@ class ID3(TinyTag):
             fh.seek(-128, os.SEEK_END)  # try parsing id3v1 in last 128 bytes
             self._parse_id3v1(fh)
 
-    def _parse_id3v2(self, fh):
-        # for info on the specs, see: http://id3.org/Developer%20Information
+    def _get_id3v2_header(self, fh):
         header = struct.unpack('3sBBB4B', _read(fh, 10))
         tag = codecs.decode(header[0], 'ISO-8859-1')
         # check if there is an ID3v2 tag at the beginning of the file
         if tag == 'ID3':
+            return header
+        return None
+
+    def _parse_id3v2(self, fh):
+        # for info on the specs, see: http://id3.org/Developer%20Information
+        header = self._get_id3v2_header(fh)
+        if header:
             major, rev = header[1:3]
             if DEBUG:
                 stderr('Found id3 v2.%s' % major)
@@ -622,6 +636,7 @@ class ID3(TinyTag):
         if fh.read(3) == b'TAG':  # check if this is an ID3 v1 tag
             def asciidecode(x):
                 return self._unpad(codecs.decode(x, 'latin1'))
+            self._bytepos_after_id3v2 = 0
             fields = fh.read(30 + 30 + 30 + 4 + 30 + 1)
             self._set_field('title', fields[:30], transfunc=asciidecode)
             self._set_field('artist', fields[30:60], transfunc=asciidecode)
@@ -664,11 +679,15 @@ class ID3(TinyTag):
                 if frame_id == 'PIC':  # ID3 v2.2:
                     desc_end_pos = content.index(b'\x00', 1) + 1
                 else:  # ID3 v2.3+
+                    textencoding = content[0]
                     mimetype_end_pos = content.index(b'\x00', 1) + 1
                     desc_start_pos = mimetype_end_pos + 1  # jump over picture type
-                    desc_end_pos = content.index(b'\x00', desc_start_pos) + 1
+                    if textencoding == 0:
+                        desc_end_pos = content.index(b'\x00', desc_start_pos) + 1
+                    else:
+                        desc_end_pos = content.index(b'\x00\x00', desc_start_pos) + 2
                 if content[desc_end_pos:desc_end_pos + 1] == b'\x00':
-                    desc_end_pos += 1  # the description ends with 1 or 2 null bytes
+                    desc_end_pos += 1  # the description ends with 1 null byte
                 self._image_data = content[desc_end_pos:]
             return frame_size
         return 0
@@ -704,6 +723,7 @@ class ID3(TinyTag):
                 bytestr = bytestr[1:]
                 encoding = 'UTF-8'
             else:
+                bytestr = bytestr
                 encoding = 'ISO-8859-1'  # wild guess
             if bytestr[:4] == b'eng\x00':
                 bytestr = bytestr[4:]  # remove language
