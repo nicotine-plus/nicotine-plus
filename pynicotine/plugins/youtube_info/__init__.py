99d3b3b099e852e6701bad1c15231cded4405c0b
Inso-m-niaC <77669089+Inso-m-niaC@users.noreply.github.com>
2021-09-09 11:47:08 +0100

    Remove redundant character escapes in youtube_info

--- pynicotine/plugins/youtube_info/__init__.py
+++ pynicotine/plugins/youtube_info/__init__.py
@@ -90,8 +90,8 @@ class Plugin(BasePlugin):
 
     def get_video_id(self, mode, source, line):
 
-        match = re.search(r'(?:https?:\/\/)?(?:www\.|m\.)?youtu(?:be\-nocookie\.com|\.be|be\.com)\/'
-                          r'(?:.*?v=|v\/|e\/|embed\/|)([0-9a-zA-Z_-]{11})', line)
+        match = re.search(r'(?:https?://(?:www\.)?|www\.)(?:m\.)?youtu(?:\.be|(?:be-nocookie|be)\.com)/'
+                          r'(?:.*?v=|(?:v|e|embed)/|)([0-9a-zA-Z_-]{11})', line)
         if not match:
             return None
 
