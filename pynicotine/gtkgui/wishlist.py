8b25aabc96a00b26588f73e63e8d5e66b67b9f65
mathiascode <mail@mathias.is>
2021-11-18 20:13:11 +0200

    wishlist.py: avoid nested dialogs

--- pynicotine/gtkgui/wishlist.py
+++ pynicotine/gtkgui/wishlist.py
@@ -112,7 +112,7 @@ class WishList(UserInterface):
 
         model, paths = self.list_view.get_selection().get_selected_rows()
 
-        for path in reversed(paths):
+        for path in paths:
             iterator = model.get_iter(path)
             old_wish = model.get_value(iterator, 0)
 
@@ -124,6 +124,7 @@ class WishList(UserInterface):
                 callback=self.on_edit_wish_response,
                 callback_data=old_wish
             )
+            return
 
     def on_remove_wish(self, *args):
 
