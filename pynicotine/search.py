70c4dda3300143f434fc40ec196c8b6ac973637a
slook <88614182+slook@users.noreply.github.com>
2022-01-02 18:28:16 +0000

    search.py: Seperate log strings
    Reverted: Keep separate strings for Normal search and Direct search found results
    Changed: Don't call len(fileinfos) if we already know it is the same length as numresults
    Added: Debug log for ValueError in create_search_result_list()

--- pynicotine/search.py
+++ pynicotine/search.py
@@ -421,13 +421,12 @@ class Search:
             return results
 
         except ValueError:
-            # DB is closed, perhaps due to rescanning shares or closing the application
+            log.add_debug("Error: DB closed during search, perhaps due to rescanning shares or closing the application")
             return None
 
     def process_search_request(self, searchterm, user, searchid, direct=False):
-        """ Note: since this section is accessed every time a search request arrives,
-        several times a second, please keep it as optimized and memory
-        sparse as possible! """
+        """ Note: since this section is accessed every time a search request arrives several
+            times per second, please keep it as optimized and memory sparse as possible! """
 
         if not searchterm:
             return
@@ -508,15 +507,13 @@ class Search:
                 fileinfos.append(fileinfo)
 
         if numresults != len(fileinfos):
-            log.add_debug(("File index inconsistency while responding to search request "
-                           "\"%(query)s\". %(expected_num)s results expected, but only %(total_num)s "
-                           "results were found in database."), {
+            log.add_debug(("Error: File index inconsistency while responding to search request \"%(query)s\". "
+                           "Expected %(expected_num)s results, but found %(total_num)s results in database."), {
                 "query": searchterm_old,
                 "expected_num": numresults,
                 "total_num": len(fileinfos)
             })
-
-        numresults = len(fileinfos)
+            numresults = len(fileinfos)
 
         if not numresults:
             return
@@ -532,10 +529,15 @@ class Search:
 
         self.core.send_message_to_peer(user, message)
 
-        log.add_search(
-            _("User %(user)s is %(mode)s for \"%(query)s\", found %(num)i results"), {
+        if direct:
+            log.add_search(_("User %(user)s is directly searching for \"%(query)s\", found %(num)i results"), {
                 'user': user,
-                'mode': _("directly searching") if direct else _("searching"),
+                'query': searchterm_old,
+                'num': numresults
+            })
+        else:
+            log.add_search(_("User %(user)s is searching for \"%(query)s\", found %(num)i results"), {
+                'user': user,
                 'query': searchterm_old,
                 'num': numresults
             })
