6c1dcd0e8e75ca213922a6304f8aafa59b4e1768
mathiascode <mail@mathias.is>
2022-01-30 09:56:11 +0200

    theme.py: revert border-top/bottom change
    It doesn't work properly in the transfer tabs.

--- pynicotine/gtkgui/widgets/theme.py
+++ pynicotine/gtkgui/widgets/theme.py
@@ -156,11 +156,11 @@ def set_global_css():
 
     .border-top,
     .preferences-border .dialog-action-box {
-        box-shadow: 0 -1px 0 0 @borders;
+        border-top: 1px solid @borders;
     }
 
     .border-bottom {
-        box-shadow: 0 1px 0 0 @borders;
+        border-bottom: 1px solid @borders;
     }
 
     .border-start:dir(ltr),
