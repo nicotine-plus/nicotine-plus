54eb293ecdcb313230a8536d1624a5e72892d585
mathiascode <mail@mathias.is>
2022-04-01 23:08:08 +0300

    debian/copyright: add list of translators

--- pynicotine/gtkgui/dialogs/about.py
+++ pynicotine/gtkgui/dialogs/about.py
@@ -114,7 +114,7 @@ Inactive
 Nicotine Team
 –––––––––––––––––––––––––––––––––––––––>
 
-> Hyriand
+> Ingmar K. Steen (Hyriand)
    - Maintainer (2003–2004)
 
 > daelstorm
@@ -176,7 +176,7 @@ PySoulSeek Contributors
 > Christian Swinehart
    - Fink package maintainer
 
-> Hyriand
+> Ingmar K. Steen (Hyriand)
    - Patches for upload bandwidth management,
      banning, various UI improvements and more
 
@@ -223,67 +223,70 @@ Chinese (Simplified)
 Czech
  - burnmail123 (2021)
 
+Danish
+ - mathsped (2003–2004)
+
 Dutch
  - Han Boetes (hboetes) (2021–2022)
  - Kenny Verstraete (2009)
  - nince78 (2007)
- - hyriand (2003)
+ - Ingmar K. Steen (Hyriand) (2003–2004)
 
 English
- - slook (2021-2022)
- - Han Boetes (hboetes) (2021-2022)
+ - slook (2021–2022)
+ - Han Boetes (hboetes) (2021–2022)
  - Mat (mathiascode) (2020–2022)
  - Michael Labouebe (gfarmerfr) (2016)
- - daelstorm (2004-2009)
- - hyriand (2003)
+ - daelstorm (2004–2009)
+ - Ingmar K. Steen (Hyriand) (2003–2004)
 
 Euskara
- - Julen (2006-2007)
+ - Julen (2006–2007)
 
 Finnish
- - Kari Viittanen (Kalevi) (2006)
+ - Kari Viittanen (Kalevi) (2006–2007)
 
 French
  - Lisapple (2021–2022)
  - melmorabity (2021–2022)
  - m-balthazar (2020)
  - Michael Labouebe (gfarmerfr) (2016–2017)
- - Monsieur Poisson (2009-2010)
+ - Monsieur Poisson (2009–2010)
  - ManWell (2007)
  - zniavre (2007–2022)
  - systr (2006)
- - flashfr (2003)
+ - Julien Wajsberg (flashfr) (2003–2004)
 
 German
- - hboetes (2021–2022)
+ - Han Boetes (hboetes) (2021–2022)
  - Meokater (2007)
  - (._.) (2007)
  - lippel (2004)
- - hyriand (2003)
+ - Ingmar K. Steen (Hyriand) (2003–2004)
 
 Hungarian
  - Szia Tomi (2022)
  - Nils (2009)
- - David Balazs (djbaloo) (2006-2013)
+ - David Balazs (djbaloo) (2006–2020)
 
 Italian
  - Gabboxl (2022)
  - Gianluca Boiano (2020–2022)
  - nicola (2007)
- - dbazza (2003)
+ - dbazza (2003–2004)
 
 Lithuanian
  - mantas (2020)
- - Žygimantas Beručka (2006-2009)
+ - Žygimantas Beručka (2006–2009)
 
 Norwegian Bokmål
  - Allan Nordhøy (comradekingu) (2021)
 
 Polish
- - mariachini (2020–2022)
+ - mariachini (2017–2022)
  - Amun-Ra (2007)
  - thine (2007)
- - owczi (2003)
+ - Wojciech Owczarek (owczi) (2003–2004)
 
 Portuguese (Brazil)
  - Guilherme Santos (2022)
@@ -300,22 +303,22 @@ Slovak
  - Jozef Říha (2006-2008)
 
 Spanish (Chile)
- - MELERIX (2021-2022)
+ - MELERIX (2021–2022)
  - tagomago (2021–2022)
  - Strange (2021)
  - Silvio Orta (2007)
- - Dreslo (2003)
+ - Dreslo (2003–2004)
 
 Spanish (Spain)
  - MELERIX (2021)
  - tagomago (2021–2022)
  - Strange (2021)
  - Silvio Orta (2007)
- - Dreslo (2003)
+ - Dreslo (2003–2004)
 
 Swedish
  - mitramai (2021)
- - alimony (2003)
+ - Markus Magnuson (alimony) (2003–2004)
 
 Turkish
  - Oğuz Ersen (2021–2022)
