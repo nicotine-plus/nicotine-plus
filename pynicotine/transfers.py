5d26547d9afc5d6baee413d3fc09867c066ed352
mathiascode <mail@mathias.is>
2022-05-29 05:57:44 +0300

    transfers.py: revert "Update locally saved upload speed more often"
    Since commit 66fce3fc02ae4095465555384528383a02162484, the upload speed sent in search results is updated every time an upload finishes. This is a bad idea, since the speeds can fluctuate a lot depending on the user we're uploading to. This also causes a discrepancy between the speed reported in search results compared to user info.

--- pynicotine/transfers.py
+++ pynicotine/transfers.py
@@ -1343,7 +1343,6 @@ class Transfers:
 
             elif i.speed is not None:
                 # Inform the server about the last upload speed for this transfer
-                self.upload_speed = i.speed
                 self.queue.append(slskmessages.SendUploadSpeed(i.speed))
 
                 i.speed = None
