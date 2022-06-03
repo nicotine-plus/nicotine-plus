f6648a2930dfeb1cb8aa1aaa441252dfce927540
mathiascode <mail@mathias.is>
2022-06-03 17:04:45 +0300

    interests.py: remove unused import

--- pynicotine/gtkgui/interests.py
+++ pynicotine/gtkgui/interests.py
@@ -18,7 +18,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import Gio
 from gi.repository import GLib
 from gi.repository import GObject
 from gi.repository import Gtk
