ffc5810692967564731fa9d7ee3aa1b150581f2c
Mat <mail@mathias.is>
2022-07-20 19:27:46 +0300

    search.py: remove unused import

--- pynicotine/gtkgui/search.py
+++ pynicotine/gtkgui/search.py
@@ -31,7 +31,6 @@ from collections import OrderedDict
 from gi.repository import GLib
 from gi.repository import GObject
 from gi.repository import Gtk
-from gi.repository import Pango
 
 from pynicotine.config import config
 from pynicotine.gtkgui.application import GTK_API_VERSION
