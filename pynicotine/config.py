1a85a4d6fa0989e76a40fcb71f3045786fa02035
Mat <mail@mathias.is>
2022-08-06 01:40:17 +0300

    Bump version to 3.2.4.dev1

--- pynicotine/config.py
+++ pynicotine/config.py
@@ -55,7 +55,7 @@ class Config:
         config_dir, self.data_dir = self.get_user_directories()
         self.filename = os.path.join(config_dir, "config")
         self.plugin_dir = os.path.join(self.data_dir, "plugins")
-        self.version = "3.2.3"
+        self.version = "3.2.4.dev1"
         self.python_version = sys.version
         self.gtk_version = ""
 
