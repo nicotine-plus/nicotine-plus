0532016e3a934a220d63aa236cd1e41c957f7f8d
Mat <mail@mathias.is>
2022-08-17 20:34:52 +0300

    Fix linting error

--- pynicotine/gtkgui/frame.py
+++ pynicotine/gtkgui/frame.py
@@ -22,6 +22,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
 import threading
 import time
 
