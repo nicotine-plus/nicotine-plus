e62948b69a0b2e15eaaccd285b055031ab32c11c
Mat <mail@mathias.is>
2022-08-22 15:19:40 +0300

    test_i18n.py: add Latvian translation

--- test/unit/test_i18n.py
+++ test/unit/test_i18n.py
@@ -21,8 +21,8 @@ import unittest
 from pynicotine.i18n import build_translations
 from pynicotine.i18n import get_translation_paths
 
-LANGUAGES = ("ca", "cs", "da", "de", "eo", "es_CL", "es_ES", "eu", "fi", "fr", "hu", "it", "lt", "nb_NO", "nl", "pl",
-             "pt_BR", "ru", "sk", "sv", "tr", "uk", "zh_Hans")
+LANGUAGES = ("ca", "cs", "da", "de", "eo", "es_CL", "es_ES", "eu", "fi", "fr", "hu", "it", "lt", "lv", "nb_NO", "nl",
+             "pl", "pt_BR", "ru", "sk", "sv", "tr", "uk", "zh_Hans")
 
 
 class I18nTest(unittest.TestCase):
