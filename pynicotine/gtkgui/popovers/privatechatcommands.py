629dd780a3816d2214c37089e2990fd1702d077b
Mat <mail@mathias.is>
2022-09-13 20:45:23 +0300

    Make command help popovers responsive

--- pynicotine/gtkgui/popovers/privatechatcommands.py
+++ pynicotine/gtkgui/popovers/privatechatcommands.py
@@ -32,5 +32,5 @@ class PrivateChatCommands(UserInterface, Popover):
             window=window,
             content_box=self.container,
             width=600,
-            height=400
+            height=450
         )
