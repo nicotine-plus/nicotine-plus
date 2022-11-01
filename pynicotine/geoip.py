4ae788d7aa77b08b9b337b132076f16fd3105a4e
Mat <mail@mathias.is>
2022-11-01 22:32:22 +0200

    geoip.py: update path to country database

--- pynicotine/geoip.py
+++ pynicotine/geoip.py
@@ -277,7 +277,7 @@ class GeoIP:
     def __init__(self):
 
         from pynicotine.external.ip2location import IP2Location
-        self.ip2location = IP2Location(os.path.join(os.path.dirname(__file__), "ipcountrydb.bin"))
+        self.ip2location = IP2Location(os.path.join(os.path.dirname(__file__), "external", "ipcountrydb.bin"))
 
     @classmethod
     def country_code_to_name(cls, country_code):
