1e68d7d3312d53d0aa167baad3f33e62928f2e71
Han Boetes <hboetes@gmail.com>
2023-01-06 19:08:49 +0100

    declunkificationarry

--- pynicotine/core.py
+++ pynicotine/core.py
@@ -464,8 +464,8 @@ class Core:
         days = hours // 24
 
         if msg.seconds == 0:
-            log.add(_("You have no Soulseek privileges. Privileges are not required, but allow your downloads "
-                      "to be queued ahead of non-privileged users."))
+            log.add(_("You have no Soulseek privileges. With privileges your downloads "
+                      "will always be first in the download queue."))
         else:
             log.add(_("%(days)i days, %(hours)i hours, %(minutes)i minutes, %(seconds)i seconds of "
                       "Soulseek privileges left"), {
