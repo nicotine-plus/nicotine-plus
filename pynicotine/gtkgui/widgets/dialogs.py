2cf3b65cc6ded1aad9158f4c1ce070b44ff81828
Mat <mail@mathias.is>
2023-01-17 02:39:02 +0200

    dialogs.py: add missing import

--- pynicotine/gtkgui/widgets/dialogs.py
+++ pynicotine/gtkgui/widgets/dialogs.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from gi.repository import Gdk
 from gi.repository import Gtk
 
 from pynicotine.config import config
