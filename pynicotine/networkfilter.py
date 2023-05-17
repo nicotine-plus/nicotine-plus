380f16404b447e80ef82b0a31731c3666ad48906
Mat <mail@mathias.is>
2023-05-18 00:46:51 +0300

    networkfilter.py: avoid None usernames in IP list

--- pynicotine/networkfilter.py
+++ pynicotine/networkfilter.py
@@ -322,8 +322,11 @@ class NetworkFilter:
             ip_addresses = self._get_user_ip_addresses(username, ip_list, request_action="add")
             ip_address = next(iter(ip_addresses), f"? ({username})")
 
-        if ip_address and (ip_address not in ip_list or ip_list[ip_address] != username):
-            ip_list[ip_address] = username
+        if not ip_address:
+            return None
+
+        if ip_address not in ip_list or (username and ip_list[ip_address] != username):
+            ip_list[ip_address] = username or ""
             config.write_configuration()
 
         return ip_address
