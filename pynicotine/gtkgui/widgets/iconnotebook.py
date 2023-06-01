nicotine-plus/nicotine-plus

commit 96817b8482d4897dc78a0d6fc8fa4d335d3338d4

iconnotebook.py: don't highlight main tab on global feed message (#2516)
* iconnotebook.py: don't highlight main tab on global feed message
...unless mentioned. See issue #2482

* iconnotebook.py: cleanup don't highlight if is_quiet or is_active

pynicotine/gtkgui/widgets/iconnotebook.py
@@ -465,16 +465,20 @@ class IconNotebook:
 
     """ Tab Highlights """
 
-    def request_tab_changed(self, page, is_important=False):
+    def request_tab_changed(self, page, is_important=False, is_quiet=False):
 
         if self.parent_page is not None:
-            page_active = (self.get_current_page() == page)
+            is_current_page = (self.get_current_page() == page)
 
-            if self.window.current_page_id != self.parent_page.id or not page_active:
+            if is_quiet and not is_important:
+                # Don't highlight top-level tab on global feed message unless mentioned
+                pass
+
+            elif self.window.current_page_id != self.parent_page.id or not is_current_page:
                 # Highlight top-level tab
                 self.window.notebook.request_tab_changed(self.parent_page, is_important)
 
-            elif page_active:
+            elif is_current_page:
                 return
 
             self.append_unread_page(page, is_important)

pynicotine/gtkgui/chatrooms.py
@@ -744,7 +744,7 @@ class ChatRoom:
             return
 
         mentioned = (tag == self.chat_view.tag_highlight)
-        self.chatrooms.request_tab_changed(self.container, is_important=mentioned)
+        self.chatrooms.request_tab_changed(self.container, is_important=mentioned, is_quiet=is_global)
 
         if is_global and room in core.chatrooms.joined_rooms:
             # Don't show notifications about the Public feed that's duplicated in an open tab

commit 965ebf54fa4986866cc7628a20ec5b58153fdcca

iconnotebook.py: don't highlight current tab on global feed Regression since #2516

pynicotine/gtkgui/widgets/iconnotebook.py
@@ -468,20 +468,16 @@ class IconNotebook:
     def request_tab_changed(self, page, is_important=False, is_quiet=False):
 
         if self.parent_page is not None:
+            is_current_parent = (self.window.current_page_id == self.parent_page.id)
             is_current_page = (self.get_current_page() == page)
 
-            if is_quiet and not is_important:
-                # Don't highlight top-level tab on global feed message unless mentioned
-                pass
-
-            elif self.window.current_page_id != self.parent_page.id or not is_current_page:
-                # Highlight top-level tab
-                self.window.notebook.request_tab_changed(self.parent_page, is_important)
-
-            elif is_current_page:
+            if is_current_parent and is_current_page:
                 return
 
-            self.append_unread_page(page, is_important)
+            if not is_quiet or is_important:
+                # Highlight top-level tab, but don't for global feed unless mentioned
+                self.window.notebook.request_tab_changed(self.parent_page, is_important)
+                self.append_unread_page(page, is_important)
 
         tab_label = self.get_tab_label(page)
         tab_label.request_changed(is_important)