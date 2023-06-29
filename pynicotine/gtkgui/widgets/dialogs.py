e11c654b392a1ed01b388ad4359fe1f3c035f777
Mat <mail@mathias.is>
2023-06-30 00:54:17 +0300

    Fix regression in previous commit
    destroy() should always be called.

--- pynicotine/gtkgui/widgets/dialogs.py
+++ pynicotine/gtkgui/widgets/dialogs.py
@@ -326,10 +326,8 @@ class MessageDialog(Window):
 
         Window.active_dialogs.remove(self)
 
-        if response_id in (Gtk.ResponseType.CANCEL, Gtk.ResponseType.CLOSE, Gtk.ResponseType.DELETE_EVENT):
-            return
-
-        if callback:
+        if callback and response_id not in (Gtk.ResponseType.CANCEL, Gtk.ResponseType.CLOSE,
+                                            Gtk.ResponseType.DELETE_EVENT):
             callback(self, response_id, callback_data)
 
         self.widget.destroy()
