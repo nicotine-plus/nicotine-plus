48289ebc026e7dc5fad69678824410866f46c958
Mat <mail@mathias.is>
2023-09-25 20:26:28 +0300

    ResponseThrottle: remove TODO comments
    There's no point in having TODO comments that will never be resolved. These have been
    here for 7 years, and are not needed.

--- pynicotine/pluginsystem.py
+++ pynicotine/pluginsystem.py
@@ -359,8 +359,6 @@ class ResponseThrottle:
         return willing_to_respond
 
     def responded(self):
-        # possible TODO's: we could actually say public the msg here
-        # make more stateful - track past msg's as additional responder willingness criteria, etc
         self.plugin_usage[self.room] = {"last_time": time(), "last_request": self.request, "last_nick": self.nick}
 
 
