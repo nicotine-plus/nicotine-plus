nicotine-plus/nicotine-plus

commit 96b3c9dfbc18a758937db8f0f32f5a511653aa2e

TreeView: copy icon labels instead of icon names

pynicotine/gtkgui/search.py
@@ -495,8 +495,7 @@ class Search:
                     "column_type": "icon",
                     "title": _("Country"),
                     "width": 30,
-                    "hide_header": True,
-                    "sort_column": "country_data"
+                    "hide_header": True
                 },
                 "speed": {
                     "column_type": "number",
@@ -558,7 +557,6 @@ class Search:
                 },
 
                 # Hidden data columns
-                "country_data": {"data_type": str},
                 "speed_data": {"data_type": GObject.TYPE_UINT},
                 "in_queue_data": {"data_type": GObject.TYPE_UINT},
                 "size_data": {"data_type": GObject.TYPE_UINT64},
@@ -829,7 +827,6 @@ class Search:
                     h_size,
                     h_quality,
                     h_length,
-                    country_code,
                     ulspeed,
                     inqueue,
                     size,
@@ -917,7 +914,7 @@ class Search:
 
     def add_row_to_model(self, row):
         (user, flag, h_speed, h_queue, folder_path, _unused, _unused, _unused, _unused,
-            _unused, country_code, speed, queue, _unused, _unused, _unused, file_path, has_free_slots,
+            _unused, speed, queue, _unused, _unused, _unused, file_path, has_free_slots,
             _unused, row_id) = row
 
         expand_user = False
@@ -944,7 +941,6 @@ class Search:
                         empty_str,
                         empty_str,
                         empty_str,
-                        country_code,
                         speed,
                         queue,
                         empty_int,
@@ -984,7 +980,6 @@ class Search:
                             empty_str,
                             empty_str,
                             empty_str,
-                            country_code,
                             speed,
                             queue,
                             empty_int,
@@ -1215,25 +1210,25 @@ class Search:
             if filter_id == "filtertype" and not self.check_file_type(filter_value, row[16].lower()):
                 return False
 
-            if filter_id == "filtercc" and not self.check_country(filter_value, row[10].upper()):
+            if filter_id == "filtercc" and not self.check_country(filter_value, row[1][-2:].upper()):
                 return False
 
-            if filter_id == "filterin" and not filter_value.search(row[16]) and not filter_value.fullmatch(row[0]):
+            if filter_id == "filterin" and not filter_value.search(row[15]) and not filter_value.fullmatch(row[0]):
                 return False
 
-            if filter_id == "filterout" and (filter_value.search(row[16]) or filter_value.fullmatch(row[0])):
+            if filter_id == "filterout" and (filter_value.search(row[15]) or filter_value.fullmatch(row[0])):
                 return False
 
-            if filter_id == "filterslot" and row[12] > 0:
+            if filter_id == "filterslot" and row[11] > 0:
                 return False
 
-            if filter_id == "filtersize" and not self.check_digit(filter_value, row[13], file_size=True):
+            if filter_id == "filtersize" and not self.check_digit(filter_value, row[12], file_size=True):
                 return False
 
-            if filter_id == "filterbr" and not self.check_digit(filter_value, row[14]):
+            if filter_id == "filterbr" and not self.check_digit(filter_value, row[13]):
                 return False
 
-            if filter_id == "filterlength" and not self.check_digit(filter_value, row[15]):
+            if filter_id == "filterlength" and not self.check_digit(filter_value, row[14]):
                 return False
 
         return True
@@ -1497,7 +1492,7 @@ class Search:
             file_size = self.tree_view.get_row_value(iterator, "size_data")
             selected_size += file_size
             selected_length += self.tree_view.get_row_value(iterator, "length_data")
-            country_code = self.tree_view.get_row_value(iterator, "country_data")
+            country_code = self.tree_view.get_row_value(iterator, "country")[-2:].upper()
             country_name = core.network_filter.COUNTRIES.get(country_code, _("Unknown"))
             country = f"{country_name} ({country_code})"
 
@@ -1567,7 +1562,7 @@ class Search:
             visible_files = []
             for row in self.all_data:
                 # Find the wanted folder
-                if folder_path != row[16].rsplit("\\", 1)[0]:
+                if folder_path != row[15].rsplit("\\", 1)[0]:
                     continue
 
                 (_unused, _unused, _unused, _unused, _unused, _unused, _unused, _unused, _unused,

pynicotine/gtkgui/chatrooms.py
@@ -440,14 +440,12 @@ class ChatRoom:
                     "column_type": "icon",
                     "title": _("Status"),
                     "width": 25,
-                    "sort_column": "status_data",
                     "hide_header": True
                 },
                 "country": {
                     "column_type": "icon",
                     "title": _("Country"),
                     "width": 30,
-                    "sort_column": "country_data",
                     "hide_header": True
                 },
                 "user": {
@@ -475,10 +473,8 @@ class ChatRoom:
                 },
 
                 # Hidden data columns
-                "status_data": {"data_type": int},
                 "speed_data": {"data_type": GObject.TYPE_UINT},
                 "files_data": {"data_type": GObject.TYPE_UINT},
-                "country_data": {"data_type": str},
                 "username_weight_data": {"data_type": Pango.Weight},
                 "username_underline_data": {"data_type": Pango.Underline}
             }
@@ -617,10 +613,8 @@ class ChatRoom:
             username,
             h_speed,
             h_files,
-            status,
             avgspeed,
             files,
-            country_code,
             weight,
             underline
         ], select_row=False)
@@ -867,10 +861,7 @@ class ChatRoom:
         status = msg.status
         status_icon_name = USER_STATUS_ICON_NAMES.get(status)
 
-        if not status_icon_name:
-            return
-
-        if status == self.users_list_view.get_row_value(iterator, "status_data"):
+        if not status_icon_name or status_icon_name == self.users_list_view.get_row_value(iterator, "status"):
             return
 
         if status == UserStatus.AWAY:
@@ -889,7 +880,6 @@ class ChatRoom:
                 action % user, timestamp_format=config.sections["logging"]["rooms_timestamp"])
 
         self.users_list_view.set_row_value(iterator, "status", status_icon_name)
-        self.users_list_view.set_row_value(iterator, "status_data", status)
 
         self.chat_view.update_user_tag(user)
 
@@ -900,17 +890,13 @@ class ChatRoom:
         if iterator is None:
             return
 
-        if self.users_list_view.get_row_value(iterator, "country_data") == country_code:
+        flag_icon_name = get_flag_icon_name(country_code)
+
+        if not flag_icon_name or flag_icon_name == self.users_list_view.get_row_value(iterator, "country"):
             # Country didn't change, no need to update
             return
 
-        flag_icon_name = get_flag_icon_name(country_code)
-
-        if not flag_icon_name:
-            return
-
         self.users_list_view.set_row_value(iterator, "country", flag_icon_name)
-        self.users_list_view.set_row_value(iterator, "country_data", country_code)
 
     def username_event(self, pos_x, pos_y, user):
 

pynicotine/gtkgui/interests.py
@@ -124,7 +124,6 @@ class Interests:
                     "column_type": "icon",
                     "title": _("Status"),
                     "width": 25,
-                    "sort_column": "status_data",
                     "hide_header": True
                 },
                 "user": {
@@ -149,7 +148,6 @@ class Interests:
                 },
 
                 # Hidden data columns
-                "status_data": {"data_type": int},
                 "speed_data": {"data_type": GObject.TYPE_UINT},
                 "files_data": {"data_type": GObject.TYPE_UINT},
                 "rating_data": {
@@ -427,7 +425,6 @@ class Interests:
                 user,
                 h_speed,
                 h_files,
-                status,
                 speed,
                 files,
                 rating
@@ -447,14 +444,12 @@ class Interests:
         if iterator is None:
             return
 
-        status = msg.status
-        status_icon_name = USER_STATUS_ICON_NAMES.get(status)
+        status_icon_name = USER_STATUS_ICON_NAMES.get(msg.status)
 
-        if not status_icon_name:
+        if not status_icon_name or status_icon_name == self.similar_users_list_view.get_row_value(iterator, "status"):
             return
 
         self.similar_users_list_view.set_row_value(iterator, "status", status_icon_name)
-        self.similar_users_list_view.set_row_value(iterator, "status_data", status)
 
     def user_stats(self, msg):
 

pynicotine/gtkgui/userlist.py
@@ -69,15 +69,13 @@ class UserList:
                     "column_type": "icon",
                     "title": _("Status"),
                     "width": 25,
-                    "hide_header": True,
-                    "sort_column": "status_data"
+                    "hide_header": True
                 },
                 "country": {
                     "column_type": "icon",
                     "title": _("Country"),
                     "width": 30,
-                    "hide_header": True,
-                    "sort_column": "country_data"
+                    "hide_header": True
                 },
                 "user": {
                     "column_type": "text",
@@ -129,11 +127,9 @@ class UserList:
                 },
 
                 # Hidden data columns
-                "status_data": {"data_type": int},
                 "speed_data": {"data_type": GObject.TYPE_UINT},
                 "files_data": {"data_type": GObject.TYPE_UINT},
-                "last_seen_data": {"data_type": GObject.TYPE_UINT64},
-                "country_data": {"data_type": str}
+                "last_seen_data": {"data_type": GObject.TYPE_UINT64}
             }
         )
 
@@ -224,14 +220,10 @@ class UserList:
         status = msg.status
         status_icon_name = USER_STATUS_ICON_NAMES.get(status)
 
-        if not status_icon_name:
-            return
-
-        if status == self.list_view.get_row_value(iterator, "status_data"):
+        if not status_icon_name or status_icon_name == self.list_view.get_row_value(iterator, "status"):
             return
 
         self.list_view.set_row_value(iterator, "status", status_icon_name)
-        self.list_view.set_row_value(iterator, "status_data", status)
 
     def user_stats(self, msg):
 
@@ -284,11 +276,9 @@ class UserList:
             bool(user_data.is_prioritized),
             str(h_last_seen),
             str(user_data.note),
-            status,
             speed,
             files or 0,
-            last_seen,
-            str(country_code)
+            last_seen
         ], select_row=core.userlist.allow_saving_buddies)
 
         for combobox in (
@@ -375,7 +365,6 @@ class UserList:
             return
 
         self.list_view.set_row_value(iterator, "country", flag_icon_name)
-        self.list_view.set_row_value(iterator, "country_data", country_code)
 
     def on_add_buddy(self, *_args):
 
@@ -452,6 +441,5 @@ class UserList:
             self.list_view.set_row_value(iterator, "status", USER_STATUS_ICON_NAMES[UserStatus.OFFLINE])
             self.list_view.set_row_value(iterator, "speed", "")
             self.list_view.set_row_value(iterator, "files", "")
-            self.list_view.set_row_value(iterator, "status_data", 0)
             self.list_view.set_row_value(iterator, "speed_data", 0)
             self.list_view.set_row_value(iterator, "files_data", 0)

pynicotine/gtkgui/widgets/theme.py
@@ -190,8 +190,13 @@ FILE_TYPE_ICON_LABELS = {
     "x-office-document-symbolic": _("Document"),
     "emblem-documents-symbolic": _("Text")
 }
+USER_STATUS_ICON_LABELS = {
+    "nplus-status-available": _("Online"),
+    "nplus-status-away": _("Away"),
+    "nplus-status-offline": _("Offline")
+}
 USER_STATUS_ICON_NAMES = {
-    UserStatus.ONLINE: "nplus-status-online",
+    UserStatus.ONLINE: "nplus-status-available",
     UserStatus.AWAY: "nplus-status-away",
     UserStatus.OFFLINE: "nplus-status-offline"
 }

pynicotine/gtkgui/widgets/treeview.py
@@ -38,9 +38,8 @@ from pynicotine.gtkgui.widgets import clipboard
 from pynicotine.gtkgui.widgets.accelerator import Accelerator
 from pynicotine.gtkgui.widgets.popupmenu import PopupMenu
 from pynicotine.gtkgui.widgets.theme import FILE_TYPE_ICON_LABELS
-from pynicotine.gtkgui.widgets.theme import USER_STATUS_ICON_NAMES
+from pynicotine.gtkgui.widgets.theme import USER_STATUS_ICON_LABELS
 from pynicotine.gtkgui.widgets.theme import add_css_class
-from pynicotine.slskmessages import UserStatus
 
 
 class TreeView:
@@ -592,26 +591,20 @@ class TreeView:
         self.widget.set_model(self.model)
 
     @staticmethod
-    def get_user_status_tooltip_text(icon_name):
+    def get_icon_label(column, icon_name):
 
-        if icon_name == USER_STATUS_ICON_NAMES[UserStatus.AWAY]:
-            return _("Away")
+        if column.id == "country":
+            country_code = icon_name[-2:].upper()
+            country_name = core.network_filter.COUNTRIES.get(country_code, _("Unknown"))
+            return f"{country_name} ({country_code})"
 
-        if icon_name == USER_STATUS_ICON_NAMES[UserStatus.ONLINE]:
-            return _("Online")
+        if column.id == "status":
+            return USER_STATUS_ICON_LABELS[icon_name]
 
-        return _("Offline")
+        if column.id == "file_type":
+            return FILE_TYPE_ICON_LABELS[icon_name]
 
-    @staticmethod
-    def get_country_tooltip_text(icon_name):
-
-        country_code = icon_name[-2:].upper()
-        country_name = core.network_filter.COUNTRIES.get(country_code, _("Unknown"))
-        return f"{country_name} ({country_code})"
-
-    @staticmethod
-    def get_file_type_tooltip_text(icon_name):
-        return FILE_TYPE_ICON_LABELS.get(icon_name, _("Unknown"))
+        return icon_name
 
     def on_toggle(self, _widget, path, callback):
         callback(self, self.model.get_iter(path))
@@ -722,13 +715,12 @@ class TreeView:
         if is_blank:
             return False
 
-        column_id = column.id
         iterator = self.model.get_iter(path)
 
         if column.tooltip_callback:
             value = column.tooltip_callback(self, iterator)
         else:
-            value = self.get_row_value(iterator, column_id)
+            value = self.get_row_value(iterator, column.id)
 
         if not value:
             return False
@@ -737,14 +729,7 @@ class TreeView:
             return False
 
         if column.type == "icon":
-            if column_id == "country":
-                value = self.get_country_tooltip_text(value)
-
-            elif column_id == "status":
-                value = self.get_user_status_tooltip_text(value)
-
-            elif column_id == "file_type":
-                value = self.get_file_type_tooltip_text(value)
+            value = self.get_icon_label(column, value)
 
         # Update tooltip position
         self.widget.set_tooltip_cell(tooltip, path, column)
@@ -761,12 +746,15 @@ class TreeView:
             return False
 
         iterator = self.model.get_iter(path)
-        cell_value = str(self.model.get_value(iterator, column.get_sort_column_id()))
+        value = str(self.model.get_value(iterator, column.get_sort_column_id()))
 
-        if not cell_value:
+        if not value:
             return False
 
-        clipboard.copy_text(cell_value)
+        if column.type == "icon":
+            value = self.get_icon_label(column, value)
+
+        clipboard.copy_text(value)
         return True
 
 

commit 235e8c32edd0741da7daf4df70536e06d4823ed8

search.py: remove non-existent variable
Regression in 96b3c9dfbc18a758937db8f0f32f5a511653aa2e

pynicotine/gtkgui/search.py
@@ -1566,7 +1566,7 @@ class Search:
                     continue
 
                 (_unused, _unused, _unused, _unused, _unused, _unused, _unused, _unused, _unused,
-                    _unused, _unused, _unused, _unused, size, _unused, _unused, file_path, _unused,
+                    _unused, _unused, _unused, size, _unused, _unused, file_path, _unused,
                     file_attributes, _unused) = row
 
                 visible_files.append((file_path, size, file_attributes))