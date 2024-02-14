d6dc507720922990733f667547d3587c7abfdc61
Mat <mail@mathias.is>
2024-02-14 07:26:12 +0200

    downloads.py: revert to os.path.splitext()
    I forgot that splitext includes the period in the extension.

--- pynicotine/downloads.py
+++ pynicotine/downloads.py
@@ -638,7 +638,7 @@ class Downloads(Transfers):
         max_bytes = self.get_basename_byte_limit(download_folder_path)
 
         basename = clean_file(virtual_path.replace("/", "\\").rpartition("\\")[-1])
-        basename_no_extension, _separator, extension = basename.rpartition(".")
+        basename_no_extension, extension = os.path.splitext(basename)
         basename_limit = max_bytes - len(extension.encode("utf-8"))
         basename_no_extension = truncate_string_byte(basename_no_extension, max(0, basename_limit))
 
@@ -665,7 +665,7 @@ class Downloads(Transfers):
             download_folder_path = self.get_default_download_folder(username)
 
         basename = self.get_download_basename(virtual_path, download_folder_path)
-        basename_no_extension, _separator, extension = basename.rpartition(".")
+        basename_no_extension, extension = os.path.splitext(basename)
         download_file_path = os.path.join(download_folder_path, basename)
         file_exists = False
         counter = 1
@@ -696,7 +696,7 @@ class Downloads(Transfers):
         max_bytes = self.get_basename_byte_limit(incomplete_folder_path)
 
         basename = clean_file(virtual_path.replace("/", "\\").rpartition("\\")[-1])
-        basename_no_extension, _separator, extension = basename.rpartition(".")
+        basename_no_extension, extension = os.path.splitext(basename)
         basename_limit = max_bytes - len(prefix) - len(extension.encode("utf-8"))
         basename_no_extension = truncate_string_byte(basename_no_extension, max(0, basename_limit))
 
