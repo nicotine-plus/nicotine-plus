83a3976d824765c1aee00d53224b1af631a79214
slook <look@gmx.it>
2024-03-04 09:57:20 +0000

    slskproto.py: broadcast with connect_ex() in local_ip_address() to avoid exception 'OSError: [Errno 101] Network is unreachable' # (doesn't need to be reachable)

--- pynicotine/slskproto.py
+++ pynicotine/slskproto.py
@@ -585,7 +585,7 @@ class NetworkThread(Thread):
 
             # Send a broadcast packet on a local address (doesn't need to be reachable,
             # but MacOS requires port to be non-zero)
-            local_socket.connect(("10.255.255.255", 1))
+            local_socket.connect_ex(("10.255.255.255", 1))
 
             # This returns the "primary" IP on the local box, even if that IP is a NAT/private/internal IP
             ip_address = local_socket.getsockname()[0]
