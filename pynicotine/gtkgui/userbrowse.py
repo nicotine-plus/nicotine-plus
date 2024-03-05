c62d663057dd781a7dc30829acc56eb41feba0e0
tegitting <159534945+tegitting@users.noreply.github.com>
2024-03-05 09:20:48 +0000

    Update userbrowse.py

--- pynicotine/gtkgui/userbrowse.py
+++ pynicotine/gtkgui/userbrowse.py
@@ -475,9 +475,8 @@ class UserBrowse:
 
         if browsed_user.private_folders:
             private_size, num_private_folders = self.create_folder_tree(browsed_user.private_folders, private=True)
-        privv = num_private_folders
         privstr = str(num_private_folders)
-        all = num_folders + num_private_folders
+        # all = num_folders + num_private_folders
         total = humanize(num_folders + num_private_folders)
         totalstr = str(total)
         if (num_private_folders > 0):
