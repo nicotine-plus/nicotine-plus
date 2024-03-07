4eafcac41d7c8c0fd1b002d7a0af0b33a7d45bc5
tegitting <159534945+tegitting@users.noreply.github.com>
2024-03-07 13:27:09 +0000

    Update users.py

--- pynicotine/users.py
+++ pynicotine/users.py
@@ -28,7 +28,7 @@ from pynicotine.utils import open_uri
 
 class WatchedUser:
 
-    __slots__ = ("username", "upload_speed", "files", "folders")
+    __slots__ = ("username", "upload_speed", "files", "folders", "pfolders")
 
     def __init__(self, username):
 
@@ -36,6 +36,7 @@ class WatchedUser:
         self.upload_speed = None
         self.files = None
         self.folders = None
+        self.pfolders = None
 
 
 class Users:
