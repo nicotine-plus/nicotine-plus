31fbb0836e890dd95b75ccf82dad622296cf9290
tegitting <159534945+tegitting@users.noreply.github.com>
2024-03-23 23:42:56 +0000

    Update slskmessages.py

--- pynicotine/slskmessages.py
+++ pynicotine/slskmessages.py
@@ -1242,7 +1242,8 @@ class GetUserStats(ServerMessage):
     to the server, but WatchUser should be used instead.
     """
 
-    __slots__ = ("user", "avgspeed", "uploadnum", "files", "dirs")
+    # __slots__ = ("user", "avgspeed", "uploadnum", "files", "dirs")
+    __slots__ = ("user", "avgspeed", "uploadnum", "files", "dirs", "pdirs")
 
     def __init__(self, user=None):
         self.user = user
@@ -1250,6 +1251,7 @@ class GetUserStats(ServerMessage):
         self.uploadnum = None
         self.files = None
         self.dirs = None
+        self.pdirs = None
 
     def make_network_message(self):
         return self.pack_string(self.user)
@@ -1260,6 +1262,7 @@ class GetUserStats(ServerMessage):
         pos, self.uploadnum = self.unpack_uint64(message, pos)
         pos, self.files = self.unpack_uint32(message, pos)
         pos, self.dirs = self.unpack_uint32(message, pos)
+        pos, self.pdirs = self.unpack_uint32(message, pos)
 
 
 class QueuedDownloads(ServerMessage):
