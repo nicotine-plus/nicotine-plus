40dd63d5db5d26e9727f0220d72c844e03a7baf9
tegitting <159534945+tegitting@users.noreply.github.com>
2024-03-27 10:44:18 +0000

    Update __init__.py

--- pynicotine/plugins/leech_detector/__init__.py
+++ pynicotine/plugins/leech_detector/__init__.py
@@ -110,9 +110,9 @@ class Plugin(BasePlugin):
         elif (num_pfolders > 0 and num_pfolders == total_shared_folders):
             shared_folder_percentage = 0
         elif (num_pfolders > 0 and num_folders > 0):
-            shared_folder_percentage = round((num_pfolders / total_shared_folders) * 100)
+            shared_folder_percentage = calculate_percentage(num_pfolders,  total_shared_folders)
         else:
-            shared_folder_percentage = round((num_pfolders / total_shared_folders) * 100)
+            shared_folder_percentage = calculate_percentage(num_pfolders,  total_shared_folders)
             
         # is_user_accepted = (num_files >= self.settings["num_files"] and num_folders >= self.settings["num_folders"] and num_pfolders < num_folders)
         # conditions to be met to avoid ban
