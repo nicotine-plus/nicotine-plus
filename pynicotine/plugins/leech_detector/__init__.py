5d38a8937e635149a3db6168391d76832d2f8b37
tegitting <159534945+tegitting@users.noreply.github.com>
2024-03-27 08:21:40 +0000

    Update __init__.py

--- pynicotine/plugins/leech_detector/__init__.py
+++ pynicotine/plugins/leech_detector/__init__.py
@@ -105,7 +105,7 @@ class Plugin(BasePlugin):
         total_shared_folders = num_folders + num_pfolders
         shared_folder_percentage = round((num_pfolders / total_shared_folders) * 100)
         if (num_pfolders > 0 and num_pfolders == total_shared_folders):
-            percentage = 0
+            shared_folder_percentage = 0
             
         # is_user_accepted = (num_files >= self.settings["num_files"] and num_folders >= self.settings["num_folders"] and num_pfolders < num_folders)
         is_user_accepted = (
