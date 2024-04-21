3e3fbeb696f0f07cf26ac5359f3b3bc62d2535c9
Mat <mail@mathias.is>
2024-04-21 16:10:55 +0300

    macOS: use Python 3.12

--- packaging/windows/setup.py
+++ packaging/windows/setup.py
@@ -79,7 +79,9 @@ include_files = []
 def process_files(folder_path, callback, callback_data=None, starts_with=None, ends_with=None, recursive=False):
 
     for full_path in glob.glob(os.path.join(folder_path, "**"), recursive=recursive):
-        short_path = os.path.relpath(full_path, folder_path)
+        short_folder_path = os.path.dirname(os.path.relpath(full_path, folder_path))
+        real_full_path = os.path.realpath(full_path)
+        short_path = os.path.join(short_folder_path, os.path.basename(real_full_path))
 
         if starts_with and not short_path.startswith(starts_with):
             continue
@@ -87,7 +89,7 @@ def process_files(folder_path, callback, callback_data=None, starts_with=None, e
         if ends_with and not short_path.endswith(ends_with):
             continue
 
-        callback(full_path, short_path, callback_data)
+        callback(real_full_path, short_path, callback_data)
 
 
 def add_file(file_path, output_path):
