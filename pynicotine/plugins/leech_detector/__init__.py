3136ae81ef2018d0bb89e88c23f7380c4c692c74
tegitting <159534945+tegitting@users.noreply.github.com>
2024-05-02 18:16:08 +0100

    Update __init__.py

--- pynicotine/plugins/leech_detector/__init__.py
+++ pynicotine/plugins/leech_detector/__init__.py
@@ -104,7 +104,9 @@ class Plugin(BasePlugin):
             return
 
         # if user has private folders, calculate what percentage is shared
-        if num_pfolders == 0:
+        if num_folders == 0:
+            private_percentage = 100
+        elif num_pfolders == 0:
             private_percentage = 0
         elif num_pfolders == num_folders:
             private_percentage = 100
