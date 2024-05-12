nicotine-plus/nicotine-plus

commit e672d46a1ebff8be498569d88519e721c823df45

Logging: use same file handle for reading and writing
Fixes an issue where after receiving a chat message, quickly closing
and opening the chat tab would not append the latest message to the GUI
again, due to the file still being open for writing.

pynicotine/logfacility.py
@@ -20,6 +20,8 @@ import os
 import sys
 import time
 
+from collections import deque
+
 from pynicotine import slskmessages
 from pynicotine.config import config
 from pynicotine.events import events
@@ -73,9 +75,9 @@ class Logger:
     def __init__(self):
 
         current_date_time = time.strftime("%Y-%m-%d_%H-%M-%S")
-        self.debug_file_name = f"debug_{current_date_time}.log"
-        self.downloads_file_name = f"downloads_{current_date_time}.log"
-        self.uploads_file_name = f"uploads_{current_date_time}.log"
+        self.debug_file_name = f"debug_{current_date_time}"
+        self.downloads_file_name = f"downloads_{current_date_time}"
+        self.uploads_file_name = f"uploads_{current_date_time}"
 
         self.debug_folder_path = None
         self.transfer_folder_path = None
@@ -123,9 +125,9 @@ class Logger:
 
     # Log Files #
 
-    def _get_log_file(self, folder_path, basename):
+    def _get_log_file(self, folder_path, basename, should_create_file=True):
 
-        file_path = os.path.join(folder_path, basename)
+        file_path = os.path.join(folder_path, f"{clean_file(basename)}.log")
         log_file = self._log_files.get(file_path)
 
         if log_file is not None:
@@ -134,11 +136,14 @@ class Logger:
         folder_path_encoded = encode_path(folder_path)
         file_path_encoded = encode_path(file_path)
 
+        if not should_create_file and not os.path.isfile(file_path_encoded):
+            return log_file
+
         if not os.path.exists(folder_path_encoded):
             os.makedirs(folder_path_encoded)
 
-        log_file = self._log_files[file_path] = LogFile(
-            path=file_path, handle=open(encode_path(file_path), "ab"))  # pylint: disable=consider-using-with
+        log_file = LogFile(
+            path=file_path, handle=open(file_path_encoded, "ab+"))  # pylint: disable=consider-using-with
 
         # Disable file access for outsiders
         os.chmod(file_path_encoded, 0o600)
@@ -167,7 +172,7 @@ class Logger:
             should_log_file = (folder_path != self.debug_folder_path)
 
             self.add(_('Couldn\'t write to log file "%(filename)s": %(error)s'), {
-                "filename": os.path.join(folder_path, basename),
+                "filename": os.path.join(folder_path, f"{clean_file(basename)}.log"),
                 "error": error
             }, should_log_file=should_log_file)
 
@@ -208,6 +213,28 @@ class Logger:
 
     def open_log(self, folder_path, basename):
         self._handle_log(folder_path, basename, self.open_log_callback)
+
+    def read_log(self, folder_path, basename, num_lines):
+
+        lines = None
+
+        try:
+            log_file = self._get_log_file(folder_path, basename, should_create_file=False)
+
+            if log_file is not None:
+                # Read the number of lines specified from the beginning of the file,
+                # then go back to the end of the file to append new lines
+                log_file.handle.seek(0)
+                lines = deque(log_file.handle, num_lines)
+                log_file.handle.seek(0, os.SEEK_END)
+
+        except Exception as error:
+            log.add(_("Cannot access log file %(path)s: %(error)s"), {
+                "path": os.path.join(folder_path, f"{clean_file(basename)}.log"),
+                "error": error
+            })
+
+        return lines
 
     def delete_log(self, folder_path, basename):
         self._handle_log(folder_path, basename, self.delete_log_callback)

pynicotine/chatrooms.py
@@ -21,7 +21,6 @@ from pynicotine.config import config
 from pynicotine.core import core
 from pynicotine.events import events
 from pynicotine.logfacility import log
-from pynicotine.utils import clean_file
 from pynicotine.utils import censor_text
 from pynicotine.utils import find_whole_word
 
@@ -509,7 +508,7 @@ class ChatRooms:
         if config.sections["logging"]["chatrooms"] or room in config.sections["logging"]["rooms"]:
             log.write_log_file(
                 folder_path=log.room_folder_path,
-                basename=f"{clean_file(room)}.log", text=msg.formatted_message
+                basename=room, text=msg.formatted_message
             )
 
         if is_global:

pynicotine/gtkgui/chatrooms.py
@@ -22,8 +22,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
-
 from gi.repository import GLib
 from gi.repository import GObject
 from gi.repository import Gtk
@@ -52,7 +50,6 @@ from pynicotine.gtkgui.widgets.theme import get_flag_icon_name
 from pynicotine.gtkgui.widgets.treeview import TreeView
 from pynicotine.logfacility import log
 from pynicotine.slskmessages import UserStatus
-from pynicotine.utils import clean_file
 from pynicotine.utils import humanize
 from pynicotine.utils import human_speed
 
@@ -666,7 +663,8 @@ class ChatRoom:
     def read_room_logs(self):
 
         self.chat_view.append_log_lines(
-            path=os.path.join(log.room_folder_path, f"{clean_file(self.room)}.log"),
+            folder_path=log.room_folder_path,
+            basename=self.room,
             num_lines=config.sections["logging"]["readroomlines"],
             timestamp_format=config.sections["logging"]["rooms_timestamp"]
         )

pynicotine/gtkgui/privatechat.py
@@ -21,8 +21,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
-
 from gi.repository import GLib
 
 from pynicotine.config import config
@@ -42,7 +40,6 @@ from pynicotine.gtkgui.widgets.textentry import TextSearchBar
 from pynicotine.gtkgui.widgets.textview import ChatView
 from pynicotine.logfacility import log
 from pynicotine.slskmessages import UserStatus
-from pynicotine.utils import clean_file
 
 
 class PrivateChats(IconNotebook):
@@ -390,7 +387,8 @@ class PrivateChat:
     def read_private_log(self):
 
         self.chat_view.append_log_lines(
-            path=os.path.join(log.private_chat_folder_path, f"{clean_file(self.user)}.log"),
+            folder_path=log.private_chat_folder_path,
+            basename=self.user,
             num_lines=config.sections["logging"]["readprivatelines"],
             timestamp_format=config.sections["logging"]["private_timestamp"]
         )

pynicotine/gtkgui/widgets/textview.py
@@ -19,8 +19,6 @@
 import re
 import time
 
-from collections import deque
-
 from gi.repository import Gdk
 from gi.repository import Gtk
 
@@ -33,7 +31,6 @@ from pynicotine.gtkgui.widgets.theme import update_tag_visuals
 from pynicotine.gtkgui.widgets.theme import USER_STATUS_COLORS
 from pynicotine.logfacility import log
 from pynicotine.slskmessages import UserStatus
-from pynicotine.utils import encode_path
 from pynicotine.utils import find_whole_word
 from pynicotine.utils import open_uri
 
@@ -407,17 +404,14 @@ class ChatView(TextView):
         Accelerator("Down", self.widget, self.on_page_down_accelerator)
         Accelerator("Page_Down", self.widget, self.on_page_down_accelerator)
 
-    def append_log_lines(self, path, num_lines, timestamp_format):
+    def append_log_lines(self, folder_path, basename, num_lines, timestamp_format):
 
         if not num_lines:
             return
 
-        try:
-            with open(encode_path(path), "rb") as lines:
-                # Only show as many log lines as specified in config
-                lines = deque(lines, num_lines)
+        lines = log.read_log(folder_path, basename, num_lines)
 
-        except OSError:
+        if not lines:
             return
 
         login = config.sections["server"]["login"]
@@ -455,9 +449,8 @@ class ChatView(TextView):
 
             self.append_line(line, message_type=message_type, username=user, usertag=usertag)
 
-        if lines:
-            self.append_line(_("--- old messages above ---"), message_type="hilite",
-                             timestamp_format=timestamp_format)
+        self.append_line(_("--- old messages above ---"), message_type="hilite",
+                         timestamp_format=timestamp_format)
 
     def clear(self):
         super().clear()

pynicotine/privatechat.py
@@ -23,7 +23,6 @@ from pynicotine.core import core
 from pynicotine.events import events
 from pynicotine.logfacility import log
 from pynicotine.utils import censor_text
-from pynicotine.utils import clean_file
 from pynicotine.utils import find_whole_word
 
 
@@ -284,7 +283,7 @@ class PrivateChat:
         if config.sections["logging"]["privatechat"] or username in config.sections["logging"]["private_chats"]:
             log.write_log_file(
                 folder_path=log.private_chat_folder_path,
-                basename=f"{clean_file(username)}.log", text=msg.formatted_message, timestamp=timestamp
+                basename=username, text=msg.formatted_message, timestamp=timestamp
             )
 
         if is_outgoing_message:

commit 417a9122782905722da14203c29739589c7e3a17

logfacility.py: fix file handles not being closed
Regression in e672d46a1ebff8be498569d88519e721c823df45

pynicotine/logfacility.py
@@ -142,7 +142,7 @@ class Logger:
         if not os.path.exists(folder_path_encoded):
             os.makedirs(folder_path_encoded)
 
-        log_file = LogFile(
+        log_file = self._log_files[file_path] = LogFile(
             path=file_path, handle=open(file_path_encoded, "ab+"))  # pylint: disable=consider-using-with
 
         # Disable file access for outsiders
@@ -235,9 +235,8 @@ class Logger:
                 "error": error
             })
 
-            if log_file is not None:
-                # In case seek() failed, close log file to prevent future write attempts
-                self._close_log_file(log_file)
+        if log_file is not None:
+            self._close_log_file(log_file)
 
         return lines
 