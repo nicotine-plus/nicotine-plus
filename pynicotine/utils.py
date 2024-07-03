9f95c7ec263d6c879ca72016ca237e2a58b7ff60
Mat <mail@mathias.is>
2024-07-03 06:20:34 +0300

    Update copyright years

--- pynicotine/utils.py
+++ pynicotine/utils.py
@@ -1,4 +1,4 @@
-# COPYRIGHT (C) 2020-2023 Nicotine+ Contributors
+# COPYRIGHT (C) 2020-2024 Nicotine+ Contributors
 # COPYRIGHT (C) 2020 Lene Preuss <lene.preuss@gmail.com>
 # COPYRIGHT (C) 2016-2017 Michael Labouebe <gfarmerfr@free.fr>
 # COPYRIGHT (C) 2007 daelstorm <daelstorm@gmail.com>
