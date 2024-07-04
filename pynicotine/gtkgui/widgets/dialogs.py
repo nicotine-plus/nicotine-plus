9f17ae1ee928e6d960e1081289d63df61447be05
Mat <mail@mathias.is>
2024-07-05 00:14:30 +0300

    dialogs.py: only unrealize window when CSD is disabled
    We should try to get this fixed upstream at some point.

--- pynicotine/gtkgui/widgets/dialogs.py
+++ pynicotine/gtkgui/widgets/dialogs.py
@@ -177,7 +177,7 @@ class Dialog(Window):
 
         # "Soft-delete" the dialog. This is necessary to prevent the dialog from
         # appearing in window peek on Windows
-        if sys.platform == "win32":
+        if sys.platform == "win32" and self.widget.get_titlebar() is None:
             self.widget.unrealize()
 
         return True
