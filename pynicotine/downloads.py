nicotine-plus/nicotine-plus

commit ad4d87d5485e884c14e3bf145037ce76892458f9

transfers.py: reduce code duplication

pynicotine/downloads.py
@@ -412,27 +412,24 @@ class Downloads(Transfers):
                     "error": error
                 })
 
-    def _move_finished_transfer(self, transfer):
+    def _move_finished_transfer(self, transfer, incomplete_file_path):
 
         download_folder_path = transfer.folder_path or self.get_default_download_folder(transfer.username)
         download_folder_path_encoded = encode_path(download_folder_path)
 
         download_basename = self.get_download_basename(transfer.virtual_path, download_folder_path, avoid_conflict=True)
         download_file_path = os.path.join(download_folder_path, download_basename)
-        incomplete_file_path_encoded = transfer.file_handle.name
-
-        self._close_file(transfer)
 
         try:
             if not os.path.isdir(download_folder_path_encoded):
                 os.makedirs(download_folder_path_encoded)
 
-            shutil.move(incomplete_file_path_encoded, encode_path(download_file_path))
+            shutil.move(incomplete_file_path, encode_path(download_file_path))
 
         except OSError as error:
             log.add(
                 _("Couldn't move '%(tempfile)s' to '%(file)s': %(error)s"), {
-                    "tempfile": incomplete_file_path_encoded.decode("utf-8", "replace"),
+                    "tempfile": incomplete_file_path.decode("utf-8", "replace"),
                     "file": download_file_path,
                     "error": error
                 }
@@ -449,18 +446,16 @@ class Downloads(Transfers):
 
         username = transfer.username
         virtual_path = transfer.virtual_path
+        incomplete_file_path = transfer.file_handle.name
 
-        self._deactivate_transfer(transfer)
+        super()._finish_transfer(transfer)
 
         if not already_exists:
-            download_file_path = self._move_finished_transfer(transfer)
+            download_file_path = self._move_finished_transfer(transfer, incomplete_file_path)
 
             if download_file_path is None:
                 # Download was not moved successfully
                 return
-
-        transfer.status = TransferStatus.FINISHED
-        transfer.current_byte_offset = transfer.size
 
         if not self._auto_clear_transfer(transfer):
             self._update_transfer(transfer)
@@ -484,17 +479,9 @@ class Downloads(Transfers):
             }
         )
 
-    def _abort_transfer(self, transfer, denied_message=None, status=None, update_parent=True):
-
-        transfer.legacy_attempt = False
-        transfer.size_changed = False
-
-        if transfer.sock is not None:
-            core.send_message_to_network_thread(slskmessages.CloseConnection(transfer.sock))
+    def _abort_transfer(self, transfer, status=None, denied_message=None, update_parent=True):
 
         if transfer.file_handle is not None:
-            self._close_file(transfer)
-
             log.add_download(
                 _("Download aborted, user %(user)s file %(file)s"), {
                     "user": transfer.username,
@@ -502,19 +489,10 @@ class Downloads(Transfers):
                 }
             )
 
-        self._deactivate_transfer(transfer)
-        self._dequeue_transfer(transfer)
-        self._unfail_transfer(transfer)
+        super()._abort_transfer(transfer, status=status, denied_message=denied_message)
 
-        if not status:
-            return
-
-        transfer.status = status
-
-        if status not in {TransferStatus.FINISHED, TransferStatus.FILTERED, TransferStatus.PAUSED}:
-            self._fail_transfer(transfer)
-
-        events.emit("abort-download", transfer, status, update_parent)
+        if status:
+            events.emit("abort-download", transfer, status, update_parent)
 
     def _auto_clear_transfer(self, transfer):
 
@@ -524,9 +502,7 @@ class Downloads(Transfers):
 
         return False
 
-    def _clear_transfer(self, transfer, update_parent=True):
-
-        self._abort_transfer(transfer)
+    def _clear_transfer(self, transfer, denied_message=None, update_parent=True):
 
         log.add_transfer("Clearing download %(path)s from user %(user)s", {
             "path": transfer.virtual_path,
@@ -534,7 +510,7 @@ class Downloads(Transfers):
         })
 
         try:
-            del self.transfers[transfer.username + transfer.virtual_path]
+            super()._clear_transfer(transfer, denied_message=denied_message)
 
         except KeyError:
             log.add(("FIXME: failed to remove download %(path)s from user %(user)s, not "
@@ -1177,7 +1153,7 @@ class Downloads(Transfers):
             "user": transfer.username
         })
 
-        self._abort_transfer(transfer, status=TransferStatus.CONNECTION_TIMEOUT)
+        super()._transfer_timeout(transfer)
 
     def _download_file_error(self, username, token, error):
         """Networking thread encountered a local file error for download."""

pynicotine/transfers.py
@@ -359,27 +359,60 @@ class Transfers:
     # Events #
 
     def _transfer_timeout(self, transfer):
-        raise NotImplementedError
+        self._abort_transfer(transfer, status=TransferStatus.CONNECTION_TIMEOUT)
 
     # Transfer Actions #
 
     def _append_transfer(self, transfer):
         self.transfers[transfer.username + transfer.virtual_path] = transfer
 
-    def _abort_transfer(self, transfer, denied_message=None, status=None, update_parent=True):
-        raise NotImplementedError
+    def _abort_transfer(self, transfer, status=None, denied_message=None):
+
+        username = transfer.username
+        virtual_path = transfer.virtual_path
+
+        transfer.legacy_attempt = False
+        transfer.size_changed = False
+
+        if transfer.sock is not None:
+            core.send_message_to_network_thread(slskmessages.CloseConnection(transfer.sock))
+
+        if transfer.file_handle is not None:
+            self._close_file(transfer)
+
+        elif denied_message and virtual_path in self.queued_users.get(username, {}):
+            core.send_message_to_peer(
+                username, slskmessages.UploadDenied(virtual_path, denied_message))
+
+        self._deactivate_transfer(transfer)
+        self._dequeue_transfer(transfer)
+        self._unfail_transfer(transfer)
+
+        if not status:
+            return
+
+        transfer.status = status
+
+        if status not in {TransferStatus.FINISHED, TransferStatus.FILTERED, TransferStatus.PAUSED}:
+            self._fail_transfer(transfer)
 
     def _update_transfer(self, transfer):
         raise NotImplementedError
 
     def _finish_transfer(self, transfer):
-        raise NotImplementedError
+
+        self._deactivate_transfer(transfer)
+        self._close_file(transfer)
+
+        transfer.status = TransferStatus.FINISHED
+        transfer.current_byte_offset = transfer.size
 
     def _auto_clear_transfer(self, transfer):
         raise NotImplementedError
 
-    def _clear_transfer(self, transfer):
-        raise NotImplementedError
+    def _clear_transfer(self, transfer, denied_message=None):
+        self._abort_transfer(transfer, denied_message=denied_message)
+        del self.transfers[transfer.username + transfer.virtual_path]
 
     def _enqueue_transfer(self, transfer):
 
@@ -392,7 +425,8 @@ class Transfers:
         self._user_queue_sizes[transfer.username] += transfer.size
 
     def _enqueue_limited_transfers(self, username):
-        raise NotImplementedError
+        # Optional method
+        pass
 
     def _dequeue_transfer(self, transfer):
 

pynicotine/uploads.py
@@ -337,20 +337,12 @@ class Uploads(Transfers):
 
         events.emit("update-upload", transfer, update_parent)
 
-    def _enqueue_limited_transfers(self, username):
-        # Not used for uploads
-        pass
-
     def _finish_transfer(self, transfer, already_exists=False):
 
         username = transfer.username
         virtual_path = transfer.virtual_path
 
-        self._deactivate_transfer(transfer)
-        self._close_file(transfer)
-
-        transfer.status = TransferStatus.FINISHED
-        transfer.current_byte_offset = transfer.size
+        super()._finish_transfer(transfer)
 
         if not self._auto_clear_transfer(transfer):
             self._update_transfer(transfer)
@@ -371,42 +363,21 @@ class Uploads(Transfers):
 
         self._check_upload_queue()
 
-    def _abort_transfer(self, transfer, denied_message=None, status=None, update_parent=True):
-
-        username = transfer.username
-        virtual_path = transfer.virtual_path
-
-        if transfer.sock is not None:
-            core.send_message_to_network_thread(slskmessages.CloseConnection(transfer.sock))
+    def _abort_transfer(self, transfer, status=None, denied_message=None, update_parent=True):
 
         if transfer.file_handle is not None:
-            self._close_file(transfer)
-
             log.add_upload(
                 _("Upload aborted, user %(user)s file %(file)s"), {
-                    "user": username,
-                    "file": virtual_path
+                    "user": transfer.username,
+                    "file": transfer.virtual_path
                 }
             )
 
-        elif denied_message and virtual_path in self.queued_users.get(username, {}):
-            core.send_message_to_peer(
-                username, slskmessages.UploadDenied(virtual_path, denied_message))
+        super()._abort_transfer(transfer, status=status, denied_message=denied_message)
+        self._update_user_counter(transfer.username)
 
-        self._deactivate_transfer(transfer)
-        self._dequeue_transfer(transfer)
-        self._unfail_transfer(transfer)
-        self._update_user_counter(username)
-
-        if not status:
-            return
-
-        transfer.status = status
-
-        if status not in {TransferStatus.FINISHED, TransferStatus.CANCELLED}:
-            self._fail_transfer(transfer)
-
-        events.emit("abort-upload", transfer, status, update_parent)
+        if status:
+            events.emit("abort-upload", transfer, status, update_parent)
 
     def _auto_clear_transfer(self, transfer):
 
@@ -418,15 +389,13 @@ class Uploads(Transfers):
 
     def _clear_transfer(self, transfer, denied_message=None, update_parent=True):
 
-        self._abort_transfer(transfer, denied_message=denied_message)
-
         log.add_transfer("Clearing upload %(path)s to user %(user)s", {
             "path": transfer.virtual_path,
             "user": transfer.username
         })
 
         try:
-            del self.transfers[transfer.username + transfer.virtual_path]
+            super()._clear_transfer(transfer, denied_message=denied_message)
 
         except KeyError:
             log.add(("FIXME: failed to remove upload %(path)s to user %(user)s, not "
@@ -1035,7 +1004,7 @@ class Uploads(Transfers):
             "user": transfer.username
         })
 
-        self._abort_transfer(transfer, status=TransferStatus.CONNECTION_TIMEOUT)
+        super()._transfer_timeout(transfer)
         self._check_upload_queue()
 
     def _upload_file_error(self, username, token, error):

commit d9e25c504955b3be5f0ef01bb88044c1c3b77434

downloads.py: fix crash when finishing download in some cases
Regression in ad4d87d5485e884c14e3bf145037ce76892458f9

pynicotine/downloads.py
@@ -298,7 +298,7 @@ class Downloads(Transfers):
             username, virtual_path, size, transfer.folder_path)
 
         if file_exists:
-            self._finish_transfer(transfer, already_exists=True)
+            self._finish_transfer(transfer)
             return False
 
         super()._enqueue_transfer(transfer)
@@ -442,11 +442,12 @@ class Downloads(Transfers):
 
         return download_file_path
 
-    def _finish_transfer(self, transfer, already_exists=False):
+    def _finish_transfer(self, transfer):
 
         username = transfer.username
         virtual_path = transfer.virtual_path
-        incomplete_file_path = transfer.file_handle.name
+        already_exists = transfer.file_handle is None
+        incomplete_file_path = transfer.file_handle.name if not already_exists else None
 
         super()._finish_transfer(transfer)
 