nicotine-plus/nicotine-plus

commit 9e27c9df5e45809b5f1c8a8bdc94d6648d4f4f1a

DBus: add missing arguments
Better to be explicit here.

pynicotine/plugins/now_playing_sender/__init__.py
@@ -58,7 +58,8 @@ class Plugin(BasePlugin):
             object_path="/org/mpris/MediaPlayer2",
             arg0=None,
             flags=Gio.DBusSignalFlags.NONE,
-            callback=self.song_change
+            callback=self.song_change,
+            user_data=None
         )
 
     def remove_mpris_signal_receiver(self):
@@ -78,7 +79,8 @@ class Plugin(BasePlugin):
                 info=None,
                 name="org.freedesktop.DBus",
                 object_path="/org/freedesktop/DBus",
-                interface_name="org.freedesktop.DBus"
+                interface_name="org.freedesktop.DBus",
+                cancellable=None
             )
             names = dbus_proxy.ListNames()
 
@@ -98,7 +100,8 @@ class Plugin(BasePlugin):
             info=None,
             name=self.dbus_mpris_service + player,
             object_path="/org/mpris/MediaPlayer2",
-            interface_name="org.freedesktop.DBus.Properties"
+            interface_name="org.freedesktop.DBus.Properties",
+            cancellable=None
         )
         metadata = dbus_proxy.Get("(ss)", "org.mpris.MediaPlayer2.Player", "Metadata")
         song_url = metadata.get("xesam:url")

pynicotine/gtkgui/widgets/theme.py
@@ -58,8 +58,11 @@ if USE_COLOR_SCHEME_PORTAL:
 
         try:
             result = SETTINGS_PORTAL.call_sync(
-                "Read", GLib.Variant("(ss)", ("org.freedesktop.appearance", "color-scheme")),
-                Gio.DBusCallFlags.NONE, -1, None
+                method_name="Read",
+                parameters=GLib.Variant("(ss)", ("org.freedesktop.appearance", "color-scheme")),
+                flags=Gio.DBusCallFlags.NONE,
+                timeout_msec=-1,
+                cancellable=None
             )
 
             return result.unpack()[0]
@@ -83,9 +86,13 @@ if USE_COLOR_SCHEME_PORTAL:
 
     try:
         SETTINGS_PORTAL = Gio.DBusProxy.new_for_bus_sync(
-            Gio.BusType.SESSION, Gio.DBusProxyFlags.NONE, None,
-            "org.freedesktop.portal.Desktop", "/org/freedesktop/portal/desktop",
-            "org.freedesktop.portal.Settings", None
+            bus_type=Gio.BusType.SESSION,
+            flags=Gio.DBusProxyFlags.NONE,
+            info=None,
+            name="org.freedesktop.portal.Desktop",
+            object_path="/org/freedesktop/portal/desktop",
+            interface_name="org.freedesktop.portal.Settings",
+            cancellable=None
         )
         SETTINGS_PORTAL.connect("g-signal", on_color_scheme_changed)
 

pynicotine/gtkgui/widgets/trayicon.py
@@ -249,7 +249,8 @@ class StatusNotifierImplementation(BaseImplementation):
                 object_path=self._object_path,
                 interface_info=Gio.DBusNodeInfo.new_for_xml(xml_output).interfaces[0],
                 method_call_closure=self.on_method_call,
-                get_property_closure=self.on_get_property
+                get_property_closure=self.on_get_property,
+                set_property_closure=None
             )
 
             if not registration_id:
@@ -444,7 +445,8 @@ class StatusNotifierImplementation(BaseImplementation):
                 parameters=GLib.Variant("(s)", (self.bus.get_unique_name(),)),
                 reply_type=None,
                 flags=Gio.DBusCallFlags.NONE,
-                timeout_msec=-1
+                timeout_msec=-1,
+                cancellable=None
             )
 
         except GLib.Error as error:

pynicotine/nowplaying.py
@@ -180,7 +180,8 @@ class NowPlaying:
                 info=None,
                 name="org.freedesktop.DBus",
                 object_path="/org/freedesktop/DBus",
-                interface_name="org.freedesktop.DBus"
+                interface_name="org.freedesktop.DBus",
+                cancellable=None
             )
             names = dbus_proxy.ListNames()
             players = []
@@ -207,7 +208,8 @@ class NowPlaying:
                 info=None,
                 name=dbus_mpris_service + player,
                 object_path="/org/mpris/MediaPlayer2",
-                interface_name="org.freedesktop.DBus.Properties"
+                interface_name="org.freedesktop.DBus.Properties",
+                cancellable=None
             )
             metadata = dbus_proxy.Get("(ss)", "org.mpris.MediaPlayer2.Player", "Metadata")
 

commit 2cc1958dfa2683d8da1d44d05a4438e39390fd19

now_playing_sender: add missing argument
Regression in 9e27c9df5e45809b5f1c8a8bdc94d6648d4f4f1a

pynicotine/plugins/now_playing_sender/__init__.py
@@ -117,7 +117,8 @@ class Plugin(BasePlugin):
             if playing:
                 self.send_public(room, playing)
 
-    def song_change(self, _connection, _sender_name, _object_path, _interface_name, _signal_name, parameters):
+    def song_change(self, _connection, _sender_name, _object_path, _interface_name,
+                    _signal_name, parameters, _user_data):
 
         if self.config.sections["players"]["npplayer"] != "mpris":
             # MPRIS is not active, exit