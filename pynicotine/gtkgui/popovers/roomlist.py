nicotine-plus/nicotine-plus

commit 0313d29b2c1e26c8467bed21edbb20465d395eee

roomlist.py: set correct user count when joining room

pynicotine/gtkgui/popovers/roomlist.py
@@ -176,20 +176,21 @@ class RoomList(Popover):
             text_underline
         ], select_row=False)
 
-    def update_room_user_count(self, room, decrement=False):
+    def update_room_user_count(self, room, user_count=None, decrement=False):
 
         iterator = self.list_view.iterators.get(room)
 
         if iterator is None:
             return
 
-        user_count = self.list_view.get_row_value(iterator, "users_data")
+        if user_count is None:
+            user_count = self.list_view.get_row_value(iterator, "users_data")
 
-        if decrement:
-            if user_count > 0:
-                user_count -= 1
-        else:
-            user_count += 1
+            if decrement:
+                if user_count > 0:
+                    user_count -= 1
+            else:
+                user_count += 1
 
         if self.list_view.get_row_value(iterator, "is_private_data"):
             h_user_count = humanize(user_count - self.PRIVATE_USERS_OFFSET)
@@ -206,7 +207,7 @@ class RoomList(Popover):
         self.add_room(msg.room, is_private=True)
 
     def join_room(self, msg):
-        self.update_room_user_count(msg.room)
+        self.update_room_user_count(msg.room, user_count=len(msg.users))
 
     def show_room(self, room, *_args):
         if room == core.chatrooms.GLOBAL_ROOM_NAME:

commit cd84c09bc177d8b32e9afb84b0b20f68a4ba90b5

roomlist.py: fix negative user count for private rooms
Regression in 0313d29b2c1e26c8467bed21edbb20465d395eee

pynicotine/gtkgui/popovers/roomlist.py
@@ -161,7 +161,7 @@ class RoomList(Popover):
         h_user_count = humanize(user_count)
 
         if is_private:
-            # Show private rooms first
+            # Large internal value to sort private rooms first
             user_count += self.PRIVATE_USERS_OFFSET
 
         text_weight = Pango.Weight.BOLD if is_private else Pango.Weight.NORMAL
@@ -183,6 +183,8 @@ class RoomList(Popover):
         if iterator is None:
             return
 
+        is_private = self.list_view.get_row_value(iterator, "is_private_data")
+
         if user_count is None:
             user_count = self.list_view.get_row_value(iterator, "users_data")
 
@@ -192,10 +194,11 @@ class RoomList(Popover):
             else:
                 user_count += 1
 
-        if self.list_view.get_row_value(iterator, "is_private_data"):
-            h_user_count = humanize(user_count - self.PRIVATE_USERS_OFFSET)
-        else:
-            h_user_count = humanize(user_count)
+        elif is_private:
+            # Large internal value to sort private rooms first
+            user_count += self.PRIVATE_USERS_OFFSET
+
+        h_user_count = humanize(user_count - self.PRIVATE_USERS_OFFSET) if is_private else humanize(user_count)
 
         self.list_view.set_row_value(iterator, "users", h_user_count)
         self.list_view.set_row_value(iterator, "users_data", user_count)