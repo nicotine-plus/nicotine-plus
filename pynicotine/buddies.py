nicotine-plus/nicotine-plus

commit f93a99d089bd588b3dc34a772dcfff5b3bab60e3

GUI: various optimizations when populating lists

pynicotine/buddies.py
@@ -101,7 +101,7 @@ class Buddies:
 
             _username, note, notify_status, is_prioritized, is_trusted, last_seen, country = row
 
-            self.users[username] = user_data = Buddy(
+            self.users[username] = Buddy(
                 username=username,
                 note=note,
                 notify_status=notify_status,
@@ -111,7 +111,6 @@ class Buddies:
                 country=country,
                 status=UserStatus.OFFLINE
             )
-            events.emit("add-buddy", username, user_data)
 
         self.allow_saving_buddies = True
 

pynicotine/gtkgui/buddies.py
@@ -155,6 +155,7 @@ class Buddies:
             ("buddy-trusted", self.buddy_trusted),
             ("remove-buddy", self.remove_buddy),
             ("server-disconnect", self.server_disconnect),
+            ("start", self.start),
             ("user-country", self.user_country),
             ("user-stats", self.user_stats),
             ("user-status", self.user_status)
@@ -162,6 +163,15 @@ class Buddies:
             events.connect(event_name, callback)
 
         self.set_buddy_list_position()
+
+    def start(self):
+
+        self.list_view.disable_sorting()
+
+        for username, user_data in core.buddies.users.items():
+            self.add_buddy(username, user_data)
+
+        self.list_view.enable_sorting()
 
     def destroy(self):
 

pynicotine/gtkgui/dialogs/fastconfigure.py
@@ -94,7 +94,8 @@ class FastConfigure(Dialog):
                     "column_type": "text",
                     "title": _("Virtual Folder"),
                     "width": 1,
-                    "expand_column": True
+                    "expand_column": True,
+                    "default_sort_type": "ascending"
                 },
                 "folder": {
                     "column_type": "text",
@@ -295,6 +296,9 @@ class FastConfigure(Dialog):
         self.download_folder_button.set_path(core.downloads.get_default_download_folder())
 
         self.shares_list_view.clear()
+        self.shares_list_view.disable_sorting()
 
         for virtual_name, folder_path, *_unused in config.sections["transfers"]["shared"]:
             self.shares_list_view.add_row([virtual_name, folder_path], select_row=False)
+
+        self.shares_list_view.enable_sorting()

pynicotine/gtkgui/dialogs/preferences.py
@@ -375,6 +375,8 @@ class DownloadsPage:
         else:
             self.use_unlimited_speed_radio.set_active(True)
 
+        self.filter_list_view.disable_sorting()
+
         for item in config.sections["transfers"]["downloadfilters"]:
             if not isinstance(item, list) or len(item) < 2:
                 continue
@@ -383,6 +385,8 @@ class DownloadsPage:
             enable_regex = not escaped
 
             self.filter_list_view.add_row([dfilter, enable_regex], select_row=False)
+
+        self.filter_list_view.enable_sorting()
 
     def get_settings(self):
 
@@ -508,10 +512,12 @@ class DownloadsPage:
     def on_default_filters(self, *_args):
 
         self.filter_list_view.clear()
+        self.filter_list_view.disable_sorting()
 
         for filter_row in config.defaults["transfers"]["downloadfilters"]:
             self.filter_list_view.add_row(filter_row, select_row=False)
 
+        self.filter_list_view.enable_sorting()
         self.on_verify_filter()
 
     def on_verify_filter(self, *_args):
@@ -632,6 +638,7 @@ class SharesPage:
     def set_settings(self):
 
         self.shares_list_view.clear()
+        self.shares_list_view.disable_sorting()
 
         self.application.preferences.set_widgets_data(self.options)
 
@@ -651,6 +658,7 @@ class SharesPage:
             self.shares_list_view.add_row(
                 [virtual_name, folder_path, _("Trusted")], select_row=False)
 
+        self.shares_list_view.enable_sorting()
         self.rescan_required = self.recompress_shares_required = False
 
     def get_settings(self):
@@ -2453,6 +2461,7 @@ class UrlHandlersPage:
     def set_settings(self):
 
         self.protocol_list_view.clear()
+        self.protocol_list_view.disable_sorting()
         self.protocols.clear()
 
         self.application.preferences.set_widgets_data(self.options)
@@ -2461,6 +2470,8 @@ class UrlHandlersPage:
 
         for protocol, command in self.protocols.items():
             self.protocol_list_view.add_row([str(protocol), str(command)], select_row=False)
+
+        self.protocol_list_view.enable_sorting()
 
     def get_settings(self):
 
@@ -2818,6 +2829,7 @@ class PluginsPage:
     def set_settings(self):
 
         self.plugin_list_view.clear()
+        self.plugin_list_view.disable_sorting()
 
         self.application.preferences.set_widgets_data(self.options)
 
@@ -2830,6 +2842,8 @@ class PluginsPage:
             plugin_name = info.get("Name", plugin_id)
             enabled = (plugin_id in config.sections["plugins"]["enabled"])
             self.plugin_list_view.add_row([enabled, plugin_name, plugin_id], select_row=False)
+
+        self.plugin_list_view.enable_sorting()
 
     def get_settings(self):
 
@@ -3070,6 +3084,8 @@ class Preferences(Dialog):
             widget.set_font(value)
 
         elif isinstance(widget, TreeView):
+            widget.disable_sorting()
+
             if isinstance(value, list):
                 for item in value:
                     if isinstance(item, list):
@@ -3082,6 +3098,8 @@ class Preferences(Dialog):
             elif isinstance(value, dict):
                 for item1, item2 in value.items():
                     widget.add_row([str(item1), str(item2)], select_row=False)
+
+            widget.enable_sorting()
 
         elif isinstance(widget, FileChooserButton):
             widget.set_path(value)

pynicotine/gtkgui/dialogs/wishlist.py
@@ -63,9 +63,13 @@ class WishList(Dialog):
             }
         )
 
+        self.list_view.disable_sorting()
+
         for wish in config.sections["server"]["autosearch"]:
             wish = str(wish)
             self.list_view.add_row([wish], select_row=False)
+
+        self.list_view.enable_sorting()
 
         self.completion_entry = CompletionEntry(self.wish_entry, self.list_view.model)
         Accelerator("<Shift>Tab", self.list_view.widget, self.on_list_focus_entry_accelerator)  # skip column header

pynicotine/gtkgui/interests.py
@@ -165,6 +165,9 @@ class Interests:
             }
         )
 
+        self.likes_list_view.disable_sorting()
+        self.dislikes_list_view.disable_sorting()
+
         for item in config.sections["interests"]["likes"]:
             if isinstance(item, str):
                 self.add_thing_i_like(item)
@@ -172,6 +175,9 @@ class Interests:
         for item in config.sections["interests"]["dislikes"]:
             if isinstance(item, str):
                 self.add_thing_i_hate(item)
+
+        self.likes_list_view.enable_sorting()
+        self.dislikes_list_view.enable_sorting()
 
         # Popup menus
         popup = PopupMenu(self.window.application, self.likes_list_view.widget)
@@ -417,9 +423,12 @@ class Interests:
             self.recommendations_label.set_label(_("Recommendations"))
 
         self.recommendations_list_view.clear()
+        self.recommendations_list_view.disable_sorting()
 
         for thing, rating in recommendations:
             self.recommendations_list_view.add_row([humanize(rating), thing, rating], select_row=False)
+
+        self.recommendations_list_view.enable_sorting()
 
     def global_recommendations(self, msg):
         self.set_recommendations(msg.recommendations + msg.unrecommendations)
@@ -438,6 +447,7 @@ class Interests:
             self.similar_users_label.set_label(_("Similar Users"))
 
         self.similar_users_list_view.clear()
+        self.similar_users_list_view.disable_sorting()
 
         for index, (user, rating) in enumerate(users.items()):
             status = core.users.statuses.get(user, UserStatus.OFFLINE)
@@ -465,6 +475,8 @@ class Interests:
                 files or 0,
                 rating
             ], select_row=False)
+
+        self.similar_users_list_view.enable_sorting()
 
     def similar_users(self, msg):
         self.set_similar_users(msg.users)

pynicotine/gtkgui/popovers/chathistory.py
@@ -184,6 +184,8 @@ class ChatHistory(Popover):
 
     def load_users(self):
 
+        self.list_view.disable_sorting()
+
         try:
             with os.scandir(encode_path(log.private_chat_folder_path)) as entries:
                 for entry in entries:
@@ -201,6 +203,8 @@ class ChatHistory(Popover):
 
         except OSError:
             pass
+
+        self.list_view.enable_sorting()
 
     def remove_user(self, username):
 

pynicotine/gtkgui/userinfo.py
@@ -596,13 +596,18 @@ class UserInfo:
     def user_interests(self, msg):
 
         self.likes_list_view.clear()
+        self.likes_list_view.disable_sorting()
         self.dislikes_list_view.clear()
+        self.dislikes_list_view.disable_sorting()
 
         for like in msg.likes:
             self.likes_list_view.add_row([like], select_row=False)
 
         for hate in msg.hates:
             self.dislikes_list_view.add_row([hate], select_row=False)
+
+        self.likes_list_view.enable_sorting()
+        self.dislikes_list_view.enable_sorting()
 
     # Callbacks #
 

commit 1b355ce4cfcaaf4ad25ccde00781bed8519af60a

buddies.py: fix row selection logic
Regression in f93a99d089bd588b3dc34a772dcfff5b3bab60e3

pynicotine/buddies.py
@@ -43,12 +43,12 @@ class Buddy:
 
 
 class Buddies:
-    __slots__ = ("users", "allow_saving_buddies")
+    __slots__ = ("users", "_allow_saving_buddies")
 
     def __init__(self):
 
         self.users = {}
-        self.allow_saving_buddies = False
+        self._allow_saving_buddies = False
 
         for event_name, callback in (
             ("quit", self._quit),
@@ -113,11 +113,11 @@ class Buddies:
                 status=UserStatus.OFFLINE
             )
 
-        self.allow_saving_buddies = True
+        self._allow_saving_buddies = True
 
     def _quit(self):
         self.users.clear()
-        self.allow_saving_buddies = False
+        self._allow_saving_buddies = False
 
     def _server_login(self, msg):
 
@@ -251,7 +251,7 @@ class Buddies:
 
     def save_buddy_list(self):
 
-        if not self.allow_saving_buddies:
+        if not self._allow_saving_buddies:
             return
 
         user_rows = []

pynicotine/gtkgui/buddies.py
@@ -169,7 +169,7 @@ class Buddies:
         self.list_view.disable_sorting()
 
         for username, user_data in core.buddies.users.items():
-            self.add_buddy(username, user_data)
+            self.add_buddy(username, user_data, select_row=False)
 
         self.list_view.enable_sorting()
 
@@ -314,7 +314,7 @@ class Buddies:
             self.list_view.set_row_value(iterator, "files", h_num_files)
             self.list_view.set_row_value(iterator, "files_data", num_files)
 
-    def add_buddy(self, user, user_data):
+    def add_buddy(self, user, user_data, select_row=True):
 
         status = user_data.status
         country_code = user_data.country.replace("flag_", "")
@@ -356,7 +356,7 @@ class Buddies:
             speed,
             files or 0,
             last_seen
-        ], select_row=core.buddies.allow_saving_buddies)
+        ], select_row=select_row)
 
         for combobox in (
             self.window.search.user_search_combobox,