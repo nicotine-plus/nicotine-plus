nicotine-plus/nicotine-plus

commit a08c06a13be5fc226d20f768dd78a06da4a016c6

treeview.py: always use GObject types

pynicotine/gtkgui/widgets/treeview.py
@@ -241,13 +241,13 @@ class TreeView:
                 column_type = column_data.get("column_type")
 
                 if column_type == "progress":
-                    data_type = int
+                    data_type = GObject.TYPE_INT
 
                 elif column_type == "toggle":
-                    data_type = bool
+                    data_type = GObject.TYPE_BOOLEAN
 
                 else:
-                    data_type = str
+                    data_type = GObject.TYPE_STRING
 
             self._column_ids[column_id] = column_index
             self._data_types.append(data_type)
@@ -506,15 +506,17 @@ class TreeView:
         included_values = []
 
         for index, value in enumerate(values):
+            if not value:
+                continue
+
             if index in self._column_gvalues and value > 2147483647:
                 # Need gvalue conversion for large integers
                 gvalue = self._column_gvalues[index]
                 gvalue.set_value(value)
                 value = gvalue
 
-            if value:
-                value_columns.append(index)
-                included_values.append(value)
+            value_columns.append(index)
+            included_values.append(value)
 
         if self.has_tree:
             self.iterators[key] = iterator = self.model.insert_with_values(

pynicotine/gtkgui/dialogs/preferences.py
@@ -30,6 +30,7 @@ import time
 from operator import itemgetter
 
 from gi.repository import Gdk
+from gi.repository import GObject
 from gi.repository import Gtk
 from gi.repository import Pango
 
@@ -2889,7 +2890,7 @@ class PluginsPage:
                 },
 
                 # Hidden data columns
-                "plugin_id": {"data_type": str, "iterator_key": True}
+                "plugin_id": {"data_type": GObject.TYPE_STRING, "iterator_key": True}
             }
         )
 

pynicotine/gtkgui/interests.py
@@ -113,7 +113,7 @@ class Interests:
                 },
 
                 # Hidden data columns
-                "rating_data": {"data_type": int}
+                "rating_data": {"data_type": GObject.TYPE_INT}
             }
         )
 

pynicotine/gtkgui/popovers/roomlist.py
@@ -78,7 +78,7 @@ class RoomList(Popover):
 
                 # Hidden data columns
                 "users_data": {"data_type": GObject.TYPE_UINT},
-                "is_private_data": {"data_type": bool},
+                "is_private_data": {"data_type": GObject.TYPE_BOOLEAN},
                 "room_weight_data": {"data_type": Pango.Weight},
                 "room_underline_data": {"data_type": Pango.Underline}
             }

pynicotine/gtkgui/search.py
@@ -575,8 +575,8 @@ class Search:
                 "size_data": {"data_type": GObject.TYPE_UINT64},
                 "bitrate_data": {"data_type": GObject.TYPE_UINT},
                 "length_data": {"data_type": GObject.TYPE_UINT},
-                "file_path_data": {"data_type": str},
-                "free_slot_data": {"data_type": bool},
+                "file_path_data": {"data_type": GObject.TYPE_STRING},
+                "free_slot_data": {"data_type": GObject.TYPE_BOOLEAN},
                 "file_attributes_data": {"data_type": GObject.TYPE_PYOBJECT},
                 "id_data": {
                     "data_type": GObject.TYPE_UINT64,

pynicotine/gtkgui/transfers.py
@@ -221,9 +221,9 @@ class Transfers:
                 "current_bytes_data": {"data_type": GObject.TYPE_UINT64},
                 "speed_data": {"data_type": GObject.TYPE_UINT64},
                 "queue_position_data": {"data_type": GObject.TYPE_UINT},
-                "time_elapsed_data": {"data_type": int},
+                "time_elapsed_data": {"data_type": GObject.TYPE_INT},
                 "time_left_data": {"data_type": GObject.TYPE_UINT64},
-                "is_sensitive_data": {"data_type": bool},
+                "is_sensitive_data": {"data_type": GObject.TYPE_BOOLEAN},
                 "transfer_data": {"data_type": GObject.TYPE_PYOBJECT},
                 "id_data": {
                     "data_type": GObject.TYPE_UINT64,

commit 4af948a02beb076f6d6b2da4c2640e83617b67ec

treeview.py: fix incorrect initial sort type for string columns
Regression in a08c06a13be5fc226d20f768dd78a06da4a016c6

pynicotine/gtkgui/widgets/treeview.py
@@ -730,7 +730,7 @@ class TreeView:
             self.save_columns()
             return False
 
-        if self._data_types[sort_column_id] == str or column_id in {"in_queue", "queue_position"}:
+        if self._data_types[sort_column_id] == GObject.TYPE_STRING or column_id in {"in_queue", "queue_position"}:
             # String value (or queue position column): ascending sort by default
             first_sort_type = Gtk.SortType.ASCENDING
             second_sort_type = Gtk.SortType.DESCENDING