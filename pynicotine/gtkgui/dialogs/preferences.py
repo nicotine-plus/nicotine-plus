nicotine-plus/nicotine-plus

commit f5a7b32eaeee946df9c667dae49536ddde19734e

Add optional isolated mode (#3220)
For situations where external applications are not available, such as
Docker containers.

pynicotine/gtkgui/dialogs/preferences.py
@@ -134,7 +134,7 @@ class NetworkPage:
                 "port": core.users.public_port or unknown_label
             })
             self.check_port_status_label.set_markup(f"<a href='{url}' title='{url}'>{port_status_text}</a>")
-            self.check_port_status_label.set_visible(True)
+            self.check_port_status_label.set_visible(not core.isolated_mode)
         else:
             self.current_port_label.set_text(unknown_label)
             self.check_port_status_label.set_visible(False)
@@ -273,25 +273,31 @@ class DownloadsPage:
         self.sent_files_permission_combobox = ComboBox(
             container=self.sent_files_permission_container,
             items=(
-                (_("No one"), None),
-                (_("Everyone"), None),
-                (_("Buddies"), None),
-                (_("Trusted buddies"), None)
+                (_("No one"), 0),
+                (_("Everyone"), 1),
+                (_("Buddies"), 2),
+                (_("Trusted buddies"), 3)
             )
         )
 
+        items = [
+            (_("Nothing"), 0)
+        ]
+        if not core.isolated_mode:
+            items += [
+                (_("Open File"), 1),
+                (_("Open in File Manager"), 2)
+            ]
+        items += [
+            (_("Search"), 3),
+            (_("Abort"), 4),
+            (_("Remove"), 5),
+            (_("Retry"), 6),
+            (_("Browse Folder"), 7)
+        ]
         self.download_double_click_combobox = ComboBox(
             container=self.download_double_click_label.get_parent(), label=self.download_double_click_label,
-            items=(
-                (_("Nothing"), None),
-                (_("Open File"), None),
-                (_("Open in File Manager"), None),
-                (_("Search"), None),
-                (_("Pause"), None),
-                (_("Remove"), None),
-                (_("Resume"), None),
-                (_("Browse Folder"), None)
-            )
+            items=items
         )
 
         self.download_folder_button = FileChooserButton(
@@ -408,7 +414,7 @@ class DownloadsPage:
             "transfers": {
                 "autoclear_downloads": self.autoclear_downloads_toggle.get_active(),
                 "remotedownloads": self.accept_sent_files_toggle.get_active(),
-                "uploadallowed": self.sent_files_permission_combobox.get_selected_pos(),
+                "uploadallowed": self.sent_files_permission_combobox.get_selected_id(),
                 "incompletedir": self.incomplete_folder_button.get_path(),
                 "downloaddir": self.download_folder_button.get_path(),
                 "uploaddir": self.received_folder_button.get_path(),
@@ -420,7 +426,7 @@ class DownloadsPage:
                 "usernamesubfolders": self.enable_username_subfolders_toggle.get_active(),
                 "afterfinish": self.file_finished_command_entry.get_text().strip(),
                 "afterfolder": self.folder_finished_command_entry.get_text().strip(),
-                "download_doubleclick": self.download_double_click_combobox.get_selected_pos()
+                "download_doubleclick": self.download_double_click_combobox.get_selected_id()
             }
         }
 
@@ -795,25 +801,31 @@ class UploadsPage:
 
         self.application = application
 
+        items = [
+            (_("Nothing"), 0)
+        ]
+        if not core.isolated_mode:
+            items += [
+                (_("Open File"), 1),
+                (_("Open in File Manager"), 2)
+            ]
+        items += [
+            (_("Search"), 3),
+            (_("Abort"), 4),
+            (_("Remove"), 5),
+            (_("Retry"), 6),
+            (_("Browse Folder"), 7)
+        ]
         self.upload_double_click_combobox = ComboBox(
             container=self.upload_double_click_label.get_parent(), label=self.upload_double_click_label,
-            items=(
-                (_("Nothing"), None),
-                (_("Open File"), None),
-                (_("Open in File Manager"), None),
-                (_("Search"), None),
-                (_("Abort"), None),
-                (_("Remove"), None),
-                (_("Retry"), None),
-                (_("Browse Folder"), None)
-            )
+            items=items
         )
 
         self.upload_queue_type_combobox = ComboBox(
             container=self.upload_queue_type_label.get_parent(), label=self.upload_queue_type_label,
             items=(
-                (_("Round Robin"), None),
-                (_("First In, First Out"), None)
+                (_("Round Robin"), 0),
+                (_("First In, First Out"), 1)
             )
         )
 
@@ -877,13 +889,13 @@ class UploadsPage:
                 "use_upload_speed_limit": use_speed_limit,
                 "uploadlimit": self.speed_spinner.get_value_as_int(),
                 "uploadlimitalt": self.alt_speed_spinner.get_value_as_int(),
-                "fifoqueue": bool(self.upload_queue_type_combobox.get_selected_pos()),
+                "fifoqueue": bool(self.upload_queue_type_combobox.get_selected_id()),
                 "limitby": self.limit_total_transfers_radio.get_active(),
                 "queuelimit": self.max_queued_size_spinner.get_value_as_int(),
                 "filelimit": self.max_queued_files_spinner.get_value_as_int(),
                 "friendsnolimits": self.no_buddy_limits_toggle.get_active(),
                 "preferfriends": self.prioritize_buddies_toggle.get_active(),
-                "upload_doubleclick": self.upload_double_click_combobox.get_selected_pos()
+                "upload_doubleclick": self.upload_double_click_combobox.get_selected_id()
             }
         }
 
@@ -1323,6 +1335,7 @@ class ChatsPage:
             self.timestamp_private_chat_entry,
             self.timestamp_room_entry,
             self.tts_command_label,
+            self.tts_container,
             self.tts_private_message_entry,
             self.tts_room_message_entry,
         ) = self.widgets = ui.load(scope=self, path="settings/chats.ui")
@@ -1434,6 +1447,8 @@ class ChatsPage:
 
         self.enable_spell_checker_toggle.get_parent().set_visible(SpellChecker.is_available())
         self.enable_ctcp_toggle.set_active(not config.sections["server"]["ctcpmsgs"])
+        self.format_codes_label.set_visible(not core.isolated_mode)
+        self.tts_container.set_margin_top(24 if core.isolated_mode else 0)
 
         self.censored_patterns = config.sections["words"]["censored"][:]
         self.replacements = config.sections["words"]["autoreplaced"].copy()
@@ -1729,9 +1744,9 @@ class UserInterfacePage:
         self.close_action_combobox = ComboBox(
             container=self.close_action_label.get_parent(), label=self.close_action_label,
             items=(
-                (_("Quit Nicotine+"), None),
-                (_("Show confirmation dialog"), None),
-                (_("Run in the background"), None)
+                (_("Quit Nicotine+"), 0),
+                (_("Show confirmation dialog"), 1),
+                (_("Run in the background"), 2)
             )
         )
 
@@ -2011,6 +2026,7 @@ class UserInterfacePage:
 
         self.application.preferences.set_widgets_data(self.options)
 
+        self.close_action_label.get_parent().set_visible(not core.isolated_mode)
         self.tray_options_container.set_visible(self.application.tray_icon.available)
 
         for page_id, enabled in config.sections["ui"]["modes_visible"].items():
@@ -2039,7 +2055,7 @@ class UserInterfacePage:
             },
             "ui": {
                 "dark_mode": self.dark_mode_toggle.get_active(),
-                "exitdialog": self.close_action_combobox.get_selected_pos(),
+                "exitdialog": self.close_action_combobox.get_selected_id(),
                 "trayicon": self.tray_icon_toggle.get_active(),
                 "startup_hidden": self.minimize_tray_startup_toggle.get_active(),
                 "language": self.language_combobox.get_selected_id(),
@@ -2187,6 +2203,7 @@ class LoggingPage:
             self.container,
             self.debug_log_folder_default_button,
             self.debug_log_folder_label,
+            self.folder_locations_container,
             self.format_codes_label,
             self.log_chatroom_toggle,
             self.log_debug_toggle,
@@ -2253,7 +2270,11 @@ class LoggingPage:
         self.__dict__.clear()
 
     def set_settings(self):
+
         self.application.preferences.set_widgets_data(self.options)
+
+        self.format_codes_label.set_visible(not core.isolated_mode)
+        self.folder_locations_container.set_margin_top(24 if core.isolated_mode else 0)
 
     def get_settings(self):
 
@@ -2635,6 +2656,12 @@ class NowPlayingPage:
         ) = self.widgets = ui.load(scope=self, path="settings/nowplaying.ui")
 
         self.application = application
+        self.enable_mpris = (
+            not core.isolated_mode
+            and sys.platform not in {"win32", "darwin"}
+            and "SNAP_NAME" not in os.environ
+        )
+        self.enable_other = not core.isolated_mode
 
         self.format_message_combobox = ComboBox(
             container=self.format_message_label.get_parent(), label=self.format_message_label,
@@ -2672,8 +2699,8 @@ class NowPlayingPage:
             self.get_format                # Callback to retrieve format text
         )
 
-        self.mpris_radio.set_visible(
-            sys.platform not in {"win32", "darwin"} and "SNAP_NAME" not in os.environ)
+        self.mpris_radio.set_visible(self.enable_mpris)
+        self.other_radio.set_visible(self.enable_other)
 
     def destroy(self):
         self.format_message_combobox.destroy()
@@ -2705,20 +2732,16 @@ class NowPlayingPage:
 
     def get_player(self):
 
-        if self.lastfm_radio.get_active():
-            player = "lastfm"
+        player = "lastfm"
 
-        elif self.mpris_radio.get_active():
+        if self.enable_mpris and self.mpris_radio.get_active():
             player = "mpris"
+
+        elif self.enable_other and self.other_radio.get_active():
+            player = "other"
 
         elif self.listenbrainz_radio.get_active():
             player = "listenbrainz"
-
-        elif self.other_radio.get_active():
-            player = "other"
-
-        if player == "mpris" and (sys.platform in {"win32", "darwin"} or "SNAP_NAME" in os.environ):
-            player = "lastfm"
 
         return player
 
@@ -2730,20 +2753,17 @@ class NowPlayingPage:
 
     def set_player(self, player):
 
-        if player == "mpris" and (sys.platform in {"win32", "darwin"} or "SNAP_NAME" in os.environ):
-            player = "lastfm"
+        if player == "mpris" and self.enable_mpris:
+            self.mpris_radio.set_active(True)
 
-        if player == "lastfm":
-            self.lastfm_radio.set_active(True)
+        elif player == "other" and self.enable_other:
+            self.other_radio.set_active(True)
 
         elif player == "listenbrainz":
             self.listenbrainz_radio.set_active(True)
 
-        elif player == "other":
-            self.other_radio.set_active(True)
-
         else:
-            self.mpris_radio.set_active(True)
+            self.lastfm_radio.set_active(True)
 
     def update_now_playing_info(self, *_args):
 
@@ -2823,6 +2843,7 @@ class PluginsPage:
     def __init__(self, application):
 
         (
+            self.add_plugins_button,
             self.container,
             self.enable_plugins_toggle,
             self.plugin_authors_label,
@@ -2867,6 +2888,7 @@ class PluginsPage:
                 "plugin_id": {"data_type": GObject.TYPE_STRING, "iterator_key": True}
             }
         )
+        self.add_plugins_button.set_visible(not core.isolated_mode)
 
     def destroy(self):
 
@@ -2985,23 +3007,6 @@ class PluginsPage:
 
 class Preferences(Dialog):
 
-    PAGE_IDS = [
-        ("network", NetworkPage, _("Network"), "network-wireless-symbolic"),
-        ("user-interface", UserInterfacePage, _("User Interface"), "view-grid-symbolic"),
-        ("shares", SharesPage, _("Shares"), "folder-symbolic"),
-        ("downloads", DownloadsPage, _("Downloads"), "folder-download-symbolic"),
-        ("uploads", UploadsPage, _("Uploads"), "emblem-shared-symbolic"),
-        ("searches", SearchesPage, _("Searches"), "system-search-symbolic"),
-        ("user-profile", UserProfilePage, _("User Profile"), "avatar-default-symbolic"),
-        ("chats", ChatsPage, _("Chats"), "insert-text-symbolic"),
-        ("now-playing", NowPlayingPage, _("Now Playing"), "folder-music-symbolic"),
-        ("logging", LoggingPage, _("Logging"), "folder-documents-symbolic"),
-        ("banned-users", BannedUsersPage, _("Banned Users"), "action-unavailable-symbolic"),
-        ("ignored-users", IgnoredUsersPage, _("Ignored Users"), "microphone-sensitivity-muted-symbolic"),
-        ("url-handlers", UrlHandlersPage, _("URL Handlers"), "insert-link-symbolic"),
-        ("plugins", PluginsPage, _("Plugins"), "application-x-addon-symbolic")
-    ]
-
     def __init__(self, application):
 
         self.application = application
@@ -3038,8 +3043,30 @@ class Preferences(Dialog):
             self.viewport.set_focus_vadjustment(self.content.get_vadjustment())
 
         self.pages = {}
+        self.page_ids = [
+            ("network", NetworkPage, _("Network"), "network-wireless-symbolic"),
+            ("user-interface", UserInterfacePage, _("User Interface"), "view-grid-symbolic"),
+            ("shares", SharesPage, _("Shares"), "folder-symbolic"),
+            ("downloads", DownloadsPage, _("Downloads"), "folder-download-symbolic"),
+            ("uploads", UploadsPage, _("Uploads"), "emblem-shared-symbolic"),
+            ("searches", SearchesPage, _("Searches"), "system-search-symbolic"),
+            ("user-profile", UserProfilePage, _("User Profile"), "avatar-default-symbolic"),
+            ("chats", ChatsPage, _("Chats"), "insert-text-symbolic"),
+            ("now-playing", NowPlayingPage, _("Now Playing"), "folder-music-symbolic"),
+            ("logging", LoggingPage, _("Logging"), "folder-documents-symbolic"),
+            ("banned-users", BannedUsersPage, _("Banned Users"), "action-unavailable-symbolic"),
+            ("ignored-users", IgnoredUsersPage, _("Ignored Users"), "microphone-sensitivity-muted-symbolic"),
+            ("url-handlers", UrlHandlersPage, _("URL Handlers"), "insert-link-symbolic"),
+            ("plugins", PluginsPage, _("Plugins"), "application-x-addon-symbolic")
+        ]
 
-        for _page_id, _page_class, label, icon_name in self.PAGE_IDS:
+        for item in self.page_ids[:]:
+            page_id, _page_class, label, icon_name = item
+
+            if core.isolated_mode and page_id == "url-handlers":
+                self.page_ids.remove(item)
+                continue
+
             box = Gtk.Box(margin_top=8, margin_bottom=8, margin_start=12, margin_end=12, spacing=12, visible=True)
             icon = Gtk.Image(icon_name=icon_name, visible=True)
             label = Gtk.Label(label=label, xalign=0, visible=True)
@@ -3068,7 +3095,7 @@ class Preferences(Dialog):
         if page_id is None:
             return
 
-        for index, (n_page_id, _page_class, _label, _icon_name) in enumerate(self.PAGE_IDS):
+        for index, (n_page_id, _page_class, _label, _icon_name) in enumerate(self.page_ids):
             if n_page_id != page_id:
                 continue
 
@@ -3123,14 +3150,10 @@ class Preferences(Dialog):
                 widget.set_active(value)
 
         elif isinstance(widget, ComboBox):
-            if isinstance(value, str):
-                if widget.entry is not None:
-                    widget.set_text(value)
-                else:
-                    widget.set_selected_id(value)
-
-            elif isinstance(value, int):
-                widget.set_selected_pos(value)
+            if widget.entry is not None:
+                widget.set_text(value)
+            else:
+                widget.set_selected_id(value)
 
         elif isinstance(widget, Gtk.FontButton):
             widget.set_font(value)
@@ -3455,7 +3478,7 @@ class Preferences(Dialog):
         if row is None:
             return
 
-        page_id, page_class, _label, _icon_name = self.PAGE_IDS[row.get_index()]
+        page_id, page_class, _label, _icon_name = self.page_ids[row.get_index()]
         old_page = self.viewport.get_child()
 
         if old_page:

pynicotine/__init__.py
@@ -91,6 +91,9 @@ def check_arguments():
     # Disables critical error dialog; used for integration tests
     parser.add_argument("--ci-mode", action="store_true", help=argparse.SUPPRESS)
 
+    # Disables features that require external applications, useful for e.g. Docker containers
+    parser.add_argument("--isolated", action="store_true", help=argparse.SUPPRESS)
+
     args = parser.parse_args()
     multi_instance = False
 
@@ -105,6 +108,7 @@ def check_arguments():
 
     core.cli_interface_address = args.bindip
     core.cli_listen_port = args.port
+    core.isolated_mode = args.isolated
 
     return args.headless, args.hidden, args.ci_mode, args.rescan, multi_instance
 

pynicotine/config.py
@@ -145,7 +145,7 @@ class Config:
             log.add(_("Can't create directory '%(path)s', reported error: %(error)s"),
                     {"path": self.data_folder_path, "error": error})
 
-    def load_config(self):
+    def load_config(self, isolated_mode=False):
 
         if self.config_loaded:
             return
@@ -154,6 +154,9 @@ class Config:
 
         data_home_env = "${NICOTINE_DATA_HOME}"
         log_folder_path = os.path.join(data_home_env, "logs")
+
+        # Resume/retry (6) action in isolated_mode mode, open in file manager (2) action otherwise
+        transfer_double_click_action = 6 if isolated_mode else 2
 
         self._parser = ConfigParser(
             strict=False, interpolation=None, delimiters=("=",),
@@ -231,8 +234,8 @@ class Config:
                     ["desktop.ini", 1],
                     ["Thumbs.db", 1]
                 ],
-                "download_doubleclick": 2,
-                "upload_doubleclick": 2,
+                "download_doubleclick": transfer_double_click_action,
+                "upload_doubleclick": transfer_double_click_action,
                 "downloadsexpanded": True,
                 "uploadsexpanded": True
             },

pynicotine/core.py
@@ -35,7 +35,7 @@ class Core:
                  "uploads", "interests", "userbrowse", "userinfo", "buddies", "privatechat",
                  "chatrooms", "pluginhandler", "now_playing", "portmapper", "notifications",
                  "update_checker", "_network_thread", "cli_interface_address",
-                 "cli_listen_port", "enabled_components")
+                 "cli_listen_port", "isolated_mode", "enabled_components")
 
     def __init__(self):
 
@@ -61,6 +61,7 @@ class Core:
 
         self.cli_interface_address = None
         self.cli_listen_port = None
+        self.isolated_mode = False
 
         self.enabled_components = set()
 
@@ -87,7 +88,7 @@ class Core:
             from pynicotine.cli import cli
             cli.enable_logging()
 
-        config.load_config()
+        config.load_config(self.isolated_mode)
         events.enable()
 
         for event_name, callback in (

pynicotine/gtkgui/application.py
@@ -358,10 +358,15 @@ class Application:
             ("#" + _("_Keyboard Shortcuts"), "app.keyboard-shortcuts"),
             ("#" + _("_Setup Assistant"), "app.setup-assistant"),
             ("#" + _("_Transfer Statistics"), "app.transfer-statistics"),
-            ("", None),
-            ("#" + _("Report a _Bug"), "app.report-bug"),
-            ("#" + _("Improve T_ranslations"), "app.improve-translations"),
-            ("", None),
+            ("", None)
+        )
+        if not core.isolated_mode:
+            menu.add_items(
+                ("#" + _("Report a _Bug"), "app.report-bug"),
+                ("#" + _("Improve T_ranslations"), "app.improve-translations"),
+                ("", None)
+            )
+        menu.add_items(
             ("^" + _("_About Nicotine+"), "app.about")
         )
 

pynicotine/gtkgui/chatrooms.py
@@ -612,8 +612,13 @@ class ChatRoom:
             ("#" + _("Copy"), self.chat_view.on_copy_text),
             ("#" + _("Copy Link"), self.chat_view.on_copy_link),
             ("#" + _("Copy All"), self.chat_view.on_copy_all_text),
-            ("", None),
-            ("#" + _("View Room Log"), self.on_view_room_log),
+            ("", None)
+        )
+        if not core.isolated_mode:
+            self.popup_menu_chat_view.add_items(
+                ("#" + _("View Room Log"), self.on_view_room_log)
+            )
+        self.popup_menu_chat_view.add_items(
             ("#" + _("Delete Room Log…"), self.on_delete_room_log),
             ("", None),
             ("#" + _("Clear Message View"), self.chat_view.on_clear_all_text),

pynicotine/gtkgui/mainwindow.py
@@ -22,7 +22,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
 import sys
 import time
 
@@ -299,8 +298,6 @@ class MainWindow(Window):
 
     def init_window(self):
 
-        is_broadway_backend = (os.environ.get("GDK_BACKEND") == "broadway")
-
         # Set main window title and icon
         self.set_title(pynicotine.__application_name__)
         self.widget.set_default_icon_name(pynicotine.__application_id__)
@@ -319,12 +316,12 @@ class MainWindow(Window):
             else:
                 self.widget.move(x_pos, y_pos)
 
-        # Hide close button in Broadway backend
-        if is_broadway_backend:
+        # Hide close button in isolated_mode mode (e.g. Broadway backend)
+        if core.isolated_mode:
             self.widget.set_deletable(False)
 
         # Maximize main window if necessary
-        if config.sections["ui"]["maximized"] or is_broadway_backend:
+        if config.sections["ui"]["maximized"] or core.isolated_mode:
             self.widget.maximize()
 
         # Auto-away mode
@@ -1078,10 +1075,15 @@ class MainWindow(Window):
             ("", None),
             ("#" + _("_Copy"), self.log_view.on_copy_text),
             ("#" + _("Copy _All"), self.log_view.on_copy_all_text),
-            ("", None),
-            ("#" + _("View _Debug Logs"), self.on_view_debug_logs),
-            ("#" + _("View _Transfer Logs"), self.on_view_transfer_logs),
-            ("", None),
+            ("", None)
+        )
+        if not core.isolated_mode:
+            self.popup_menu_log_view.add_items(
+                ("#" + _("View _Debug Logs"), self.on_view_debug_logs),
+                ("#" + _("View _Transfer Logs"), self.on_view_transfer_logs),
+                ("", None)
+            )
+        self.popup_menu_log_view.add_items(
             (">" + _("_Log Categories"), self.popup_menu_log_categories),
             ("", None),
             ("#" + _("Clear Log View"), self.on_clear_log_view)

pynicotine/gtkgui/privatechat.py
@@ -366,8 +366,13 @@ class PrivateChat:
             ("#" + _("Copy"), self.chat_view.on_copy_text),
             ("#" + _("Copy Link"), self.chat_view.on_copy_link),
             ("#" + _("Copy All"), self.chat_view.on_copy_all_text),
-            ("", None),
-            ("#" + _("View Chat Log"), self.on_view_chat_log),
+            ("", None)
+        )
+        if not core.isolated_mode:
+            self.popup_menu.add_items(
+                ("#" + _("View Chat Log"), self.on_view_chat_log)
+            )
+        self.popup_menu.add_items(
             ("#" + _("Delete Chat Log…"), self.on_delete_chat_log),
             ("", None),
             ("#" + _("Clear Message View"), self.chat_view.on_clear_all_text),

commit 99c039fa01210e106b8c92015157ba487db2025d

preferences.py: fix incorrect labels for double click download option
Regression in f5a7b32eaeee946df9c667dae49536ddde19734e

pynicotine/gtkgui/dialogs/preferences.py
@@ -290,9 +290,9 @@ class DownloadsPage:
             ]
         items += [
             (_("Search"), 3),
-            (_("Abort"), 4),
+            (_("Pause"), 4),
             (_("Remove"), 5),
-            (_("Retry"), 6),
+            (_("Resume"), 6),
             (_("Browse Folder"), 7)
         ]
         self.download_double_click_combobox = ComboBox(