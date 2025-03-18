69028506ca2afba2688fd5c01d3514e1bf4c61e5
slook <look@gmx.it>
2025-03-18 22:56:10 +0000

    shares.py: accurate current_folder_count during rescan

--- pynicotine/shares.py
+++ pynicotine/shares.py
@@ -532,6 +532,8 @@ class Scanner:
                 # Sharing a folder twice, no go
                 continue
 
+            self.queue.put(self.current_folder_count)
+
             file_list = []
 
             try:
@@ -544,11 +546,7 @@ class Scanner:
                             if self.is_hidden(path, entry=entry):
                                 continue
 
-                            self.current_folder_count += 1
                             folder_paths.append(path)
-
-                            if not self.current_folder_count % 100:
-                                self.queue.put(self.current_folder_count)
                             continue
 
                         try:
@@ -593,6 +591,7 @@ class Scanner:
                 )
 
             self.streams[virtual_folder_path] = self.get_folder_stream(file_list)
+            self.current_folder_count += 1
 
     def get_audio_tag(self, file_path, size):
 
@@ -1107,7 +1106,7 @@ class Shares:
 
         successful = True
 
-        while self._scanner_process.is_alive():
+        while self._scanner_process.is_alive() and successful:
             # Cooldown
             time.sleep(0.05)
 
