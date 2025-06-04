bb3b64e57353cefaa635b2d0e3c830d6b65f8af7
Inso-m-niaC <77669089+Inso-m-niaC@users.noreply.github.com>
2025-06-04 13:01:33 +0100

    Update __init__.py

--- pynicotine/plugins/examplars/now_playing_windows/__init__.py
+++ pynicotine/plugins/examplars/now_playing_windows/__init__.py
@@ -1,7 +1,39 @@
-from window_helper import get_now_playing
+import ctypes
+import ctypes.wintypes
+import re
 
 from pynicotine.pluginsystem import BasePlugin
 from pynicotine.pluginsystem import returncode
+
+
+def get_now_playing(class_name, endswith_text):
+    wnd_enum_proc = ctypes.WINFUNCTYPE(
+        ctypes.wintypes.BOOL,
+        ctypes.wintypes.HWND,
+        ctypes.wintypes.LPARAM
+    )
+    found_text = None
+
+    def enum_windows_callback(hwnd, _lparam):
+        nonlocal found_text
+        class_name_buffer = ctypes.create_unicode_buffer(256)
+        text_buffer = ctypes.create_unicode_buffer(256)
+
+        if ctypes.windll.user32.GetClassNameW(hwnd, class_name_buffer, 256) > 0:
+            if class_name_buffer.value == class_name:
+                if ctypes.windll.user32.GetWindowTextW(hwnd, text_buffer, 256) > 0:
+                    window_text = text_buffer.value
+                    if window_text.endswith(endswith_text):
+                        found_text = window_text.removesuffix(endswith_text)
+                        if endswith_text in {" - Winamp", " - MediaMonkey 2024"}:
+                            found_text = re.sub(r"^\d+\.\s", "", found_text)  # strip track number
+                        return False  # Stop enumeration once found
+        return True  # Continue enumeration
+
+    enum_func = wnd_enum_proc(enum_windows_callback)
+    ctypes.windll.user32.EnumWindows(enum_func, 0)
+
+    return found_text
 
 
 class Plugin(BasePlugin):
@@ -69,7 +101,5 @@ class Plugin(BasePlugin):
             now_playing = prefix + now_playing + suffix
 
             self.send_message(now_playing)
-        else:
-            self.log("%s process not detected." % player)
 
         return returncode['zap']
