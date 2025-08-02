20ba8974fbd23c1a9098e7335ac83307bc05d53e
Mat <mail@mathias.is>
2025-08-02 12:44:51 +0300

    slskmessages.py: stop using latin-1 encoding for search queries
    The original purpose was to receive a few more results from some
    Soulseek NS clients, but it effectively doesn't work anymore since
    clients on the distributed network re-encode search queries as
    UTF-8 before distributing them.

--- pynicotine/slskmessages.py
+++ pynicotine/slskmessages.py
@@ -1159,7 +1159,7 @@ class FileSearch(ServerMessage):
     def make_network_message(self):
         msg = bytearray()
         msg += self.pack_uint32(self.token)
-        msg += self.pack_string(self.searchterm, is_legacy=True)
+        msg += self.pack_string(self.searchterm)
 
         return msg
 
@@ -1366,7 +1366,7 @@ class UserSearch(ServerMessage):
         msg = bytearray()
         msg += self.pack_string(self.search_username)
         msg += self.pack_uint32(self.token)
-        msg += self.pack_string(self.searchterm, is_legacy=True)
+        msg += self.pack_string(self.searchterm)
 
         return msg
 
@@ -2321,7 +2321,7 @@ class RoomSearch(ServerMessage):
         msg = bytearray()
         msg += self.pack_string(self.room)
         msg += self.pack_uint32(self.token)
-        msg += self.pack_string(self.searchterm, is_legacy=True)
+        msg += self.pack_string(self.searchterm)
 
         return msg
 
