f51f2738a559eb8773249b001dee6609cb94cdb1
Mat <mail@mathias.is>
2025-10-05 19:57:30 +0300

    downloads.py: actually close file connection of zero-byte downloads
    Since we will never receive any incoming data on the file connection,
    we need to close the connection here.

--- pynicotine/downloads.py
+++ pynicotine/downloads.py
@@ -27,6 +27,7 @@ from pynicotine.core import core
 from pynicotine.events import events
 from pynicotine.logfacility import log
 from pynicotine.slskmessages import ConnectionType
+from pynicotine.slskmessages import CloseConnection
 from pynicotine.slskmessages import DownloadFile
 from pynicotine.slskmessages import FileOffset
 from pynicotine.slskmessages import FolderContentsRequest
@@ -1172,6 +1173,7 @@ class Downloads(Transfers):
                 core.send_message_to_peer(username, FileOffset(sock, offset))
 
             else:
+                core.send_message_to_network_thread(CloseConnection(sock))
                 self._finish_transfer(download)
                 need_update = False
 
