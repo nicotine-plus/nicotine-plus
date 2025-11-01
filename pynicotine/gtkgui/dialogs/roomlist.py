nicotine-plus/nicotine-plus

commit 2b826ee1205b9f56dbb7c3431738ba54408dbeba

GUI: add context menu to every tree/list view (#3513)
Some list/tree views were missing them, mainly in the Preferences
dialog.

pynicotine/gtkgui/dialogs/roomlist.py
@@ -72,8 +72,8 @@ class RoomList(Dialog):
         self.popup_room = None
         self.popup_menu = PopupMenu(application, self.list_view.widget, self.on_popup_menu)
         self.popup_menu.add_items(
-            ("=" + _("Join Room"), self.on_popup_join),
-            ("=" + _("Leave Room"), self.on_popup_leave),
+            ("=" + _("_Join Room"), self.on_popup_join),
+            ("=" + _("_Leave Room"), self.on_popup_leave),
             ("", None),
             ("=" + _("Disown Private Room"), self.on_popup_private_room_disown),
             ("=" + _("Cancel Room Membership"), self.on_popup_private_room_cancel_membership)
@@ -110,8 +110,8 @@ class RoomList(Dialog):
 
     def destroy(self):
 
-        self.list_view.destroy()
         self.popup_menu.destroy()
+        self.list_view.destroy()
 
         super().destroy()
 

pynicotine/gtkgui/dialogs/chathistory.py
@@ -14,6 +14,8 @@ from pynicotine.events import events
 from pynicotine.gtkgui.widgets import ui
 from pynicotine.gtkgui.widgets.accelerator import Accelerator
 from pynicotine.gtkgui.widgets.dialogs import Dialog
+from pynicotine.gtkgui.widgets.dialogs import OptionDialog
+from pynicotine.gtkgui.widgets.popupmenu import PopupMenu
 from pynicotine.gtkgui.widgets.theme import USER_STATUS_ICON_NAMES
 from pynicotine.gtkgui.widgets.treeview import TreeView
 from pynicotine.logfacility import log
@@ -38,6 +40,7 @@ class ChatHistory(Dialog):
             width=960,
             height=700
         )
+        application.add_window(self.widget)
 
         self.list_view = TreeView(
             application.window, parent=self.list_container, activate_row_callback=self.on_show_user,
@@ -68,6 +71,13 @@ class ChatHistory(Dialog):
             }
         )
 
+        self.popup_menu = PopupMenu(application, self.list_view.widget)
+        self.popup_menu.add_items(
+            ("#" + _("_Open Chat"), self.on_show_user),
+            ("", None),
+            ("#" + _("Delete Chat Log…"), self.on_delete_chat_log)
+        )
+
         Accelerator("<Primary>f", self.widget, self.on_search_accelerator)
 
         self.load_users()
@@ -80,7 +90,10 @@ class ChatHistory(Dialog):
             events.connect(event_name, callback)
 
     def destroy(self):
+
+        self.popup_menu.destroy()
         self.list_view.destroy()
+
         super().destroy()
 
     def server_login(self, msg):
@@ -233,6 +246,25 @@ class ChatHistory(Dialog):
 
             core.privatechat.show_user(username)
             self.close()
+            return
+
+    def on_delete_chat_log_response(self, _dialog, _response_id, username):
+        log.delete_log(log.private_chat_folder_path, username)
+        self.remove_user(username)
+
+    def on_delete_chat_log(self, *_args):
+
+        for iterator in self.list_view.get_selected_rows():
+            username = self.list_view.get_row_value(iterator, "user")
+
+            OptionDialog(
+                parent=self.parent,
+                title=_("Delete Logged Messages?"),
+                message=_("Do you really want to permanently delete all logged messages for this user?"),
+                destructive_response_id="ok",
+                callback=self.on_delete_chat_log_response,
+                callback_data=username
+            ).present()
             return
 
     def on_search_accelerator(self, *_args):

pynicotine/gtkgui/dialogs/download.py
@@ -17,6 +17,7 @@ from pynicotine.gtkgui.widgets.dialogs import Dialog
 from pynicotine.gtkgui.widgets.dialogs import EntryDialog
 from pynicotine.gtkgui.widgets.filechooser import FileChooserButton
 from pynicotine.gtkgui.widgets.infobar import InfoBar
+from pynicotine.gtkgui.widgets.popupmenu import PopupMenu
 from pynicotine.gtkgui.widgets.treeview import TreeView
 from pynicotine.utils import human_size
 from pynicotine.utils import humanize
@@ -58,6 +59,7 @@ class Download(Dialog):
             show_title_buttons=False
         )
         application.add_window(self.widget)
+
         self.application = application
         self.parent_iterators = {}
         self.initial_selected_iterators = set()
@@ -117,6 +119,11 @@ class Download(Dialog):
             }
         )
 
+        self.popup_menu = PopupMenu(application, self.tree_view.widget, self.on_popup_menu)
+        self.popup_menu.add_items(
+            ("#" + _("_Rename…"), self.on_rename)
+        )
+
         self.expand_button.connect("toggled", self.on_expand_tree)
 
         for event_name, callback in (
@@ -130,6 +137,7 @@ class Download(Dialog):
 
         self.clear()
 
+        self.popup_menu.destroy()
         self.download_folder_button.destroy()
         self.info_bar.destroy()
         self.tree_view.destroy()
@@ -467,6 +475,9 @@ class Download(Dialog):
     def server_disconnect(self, *_args):
         if self.indeterminate_progress:
             self.set_failed()
+
+    def on_popup_menu(self, menu, _widget):
+        menu.actions[_("_Rename…")].set_enabled(self.rename_button.get_sensitive())
 
     def on_retry(self, *_args):
 

pynicotine/gtkgui/dialogs/fastconfigure.py
@@ -14,6 +14,7 @@ from pynicotine.gtkgui.widgets.filechooser import FileChooserButton
 from pynicotine.gtkgui.widgets.filechooser import FolderChooser
 from pynicotine.gtkgui.widgets.dialogs import Dialog
 from pynicotine.gtkgui.widgets.dialogs import EntryDialog
+from pynicotine.gtkgui.widgets.popupmenu import PopupMenu
 from pynicotine.gtkgui.widgets.treeview import TreeView
 from pynicotine.slskmessages import UserStatus
 
@@ -62,6 +63,7 @@ class FastConfigure(Dialog):
             height=450,
             show_title=False
         )
+        application.add_window(self.widget)
 
         icon_name = pynicotine.__application_id__
         icon_args = (Gtk.IconSize.BUTTON,) if GTK_API_VERSION == 3 else ()  # pylint: disable=no-member
@@ -96,10 +98,18 @@ class FastConfigure(Dialog):
             }
         )
 
+        self.shares_popup_menu = PopupMenu(application, self.shares_list_view.widget)
+        self.shares_popup_menu.add_items(
+            ("#" + _("_Edit…"), self.on_edit_shared_folder),
+            ("", None),
+            ("#" + _("Remove"), self.on_remove_shared_folder)
+        )
+
         self.reset_completeness()
 
     def destroy(self):
 
+        self.shares_popup_menu.destroy()
         self.download_folder_button.destroy()
         self.shares_list_view.destroy()
 

pynicotine/gtkgui/dialogs/pluginsettings.py
@@ -11,6 +11,7 @@ from pynicotine.gtkgui.widgets.combobox import ComboBox
 from pynicotine.gtkgui.widgets.dialogs import Dialog
 from pynicotine.gtkgui.widgets.dialogs import EntryDialog
 from pynicotine.gtkgui.widgets.filechooser import FileChooserButton
+from pynicotine.gtkgui.widgets.popupmenu import PopupMenu
 from pynicotine.gtkgui.widgets.textview import TextView
 from pynicotine.gtkgui.widgets.theme import add_css_class
 
@@ -51,8 +52,17 @@ class PluginSettings(Dialog):
             height=425,
             show_title_buttons=False
         )
+        application.add_window(self.widget)
 
     def destroy(self):
+
+        from pynicotine.gtkgui.widgets.treeview import TreeView
+
+        for widget in self.option_widgets.values():
+            if isinstance(widget, TreeView):
+                widget.popup_menu.destroy()
+                widget.destroy()
+
         self.__dict__.clear()
 
     @staticmethod
@@ -202,6 +212,13 @@ class PluginSettings(Dialog):
         treeview.description = description
         treeview.row_id = len(rows)
         self.application.preferences.set_widget(treeview, rows)
+
+        treeview.popup_menu = PopupMenu(self.application, treeview.widget)
+        treeview.popup_menu.add_items(
+            ("#" + _("_Edit…"), self.on_edit, treeview),
+            ("", None),
+            ("#" + _("Remove"), self.on_remove, treeview)
+        )
 
         button_container = Gtk.Box(margin_end=6, margin_bottom=6, margin_start=6, margin_top=6,
                                    spacing=6, visible=True)
@@ -364,7 +381,9 @@ class PluginSettings(Dialog):
         treeview.row_id += 1
         treeview.add_row([value, treeview.row_id])
 
-    def on_add(self, _button, treeview):
+    def on_add(self, *args):
+
+        treeview = args[-1]
 
         EntryDialog(
             parent=self,
@@ -388,7 +407,9 @@ class PluginSettings(Dialog):
         treeview.remove_row(iterator)
         treeview.add_row([value, row_id])
 
-    def on_edit(self, _button=None, treeview=None):
+    def on_edit(self, *args):
+
+        treeview = args[-1]
 
         for iterator in treeview.get_selected_rows():
             value = treeview.get_row_value(iterator, "description") or ""
@@ -405,7 +426,10 @@ class PluginSettings(Dialog):
             ).present()
             return
 
-    def on_remove(self, _button=None, treeview=None):
+    def on_remove(self, *args):
+
+        treeview = args[-1]
+
         for iterator in reversed(list(treeview.get_selected_rows())):
             row_id = treeview.get_row_value(iterator, "id_data")
             orig_iterator = treeview.iterators[row_id]
@@ -413,10 +437,10 @@ class PluginSettings(Dialog):
             treeview.remove_row(orig_iterator)
 
     def on_row_activated(self, treeview, *_args):
-        self.on_edit(treeview=treeview)
+        self.on_edit(treeview)
 
     def on_delete_accelerator(self, treeview):
-        self.on_remove(treeview=treeview)
+        self.on_remove(treeview)
 
     def on_cancel(self, *_args):
         self.close()

pynicotine/gtkgui/dialogs/preferences.py
@@ -35,6 +35,7 @@ from pynicotine.gtkgui.widgets.dialogs import MessageDialog
 from pynicotine.gtkgui.widgets.filechooser import FileChooserButton
 from pynicotine.gtkgui.widgets.filechooser import FileChooserSave
 from pynicotine.gtkgui.widgets.filechooser import FolderChooser
+from pynicotine.gtkgui.widgets.popupmenu import PopupMenu
 from pynicotine.gtkgui.widgets.textentry import SpellChecker
 from pynicotine.gtkgui.widgets.textview import TextView
 from pynicotine.gtkgui.widgets.theme import USER_STATUS_ICON_NAMES
@@ -327,6 +328,13 @@ class DownloadsPage:
             }
         )
 
+        self.filter_popup_menu = PopupMenu(application, self.filter_list_view.widget)
+        self.filter_popup_menu.add_items(
+            ("#" + _("_Edit…"), self.on_edit_filter),
+            ("", None),
+            ("#" + _("Remove"), self.on_remove_filter)
+        )
+
         self.options = {
             "transfers": {
                 "autoclear_downloads": self.autoclear_downloads_toggle,
@@ -347,6 +355,7 @@ class DownloadsPage:
 
     def destroy(self):
 
+        self.filter_popup_menu.destroy()
         self.download_folder_button.destroy()
         self.incomplete_folder_button.destroy()
         self.received_folder_button.destroy()
@@ -620,6 +629,14 @@ class SharesPage:
             }
         )
 
+        self.shares_popup_menu = PopupMenu(application, self.shares_list_view.widget)
+        self.shares_popup_menu.add_items(
+            ("#" + _("_Edit…"), self.on_edit_shared_folder),
+            ("#" + _("Open in File _Manager"), self.on_open_file_manager),
+            ("", None),
+            ("#" + _("Remove"), self.on_remove_shared_folder)
+        )
+
         self.options = {
             "transfers": {
                 "rescanonstartup": self.rescan_on_startup_toggle,
@@ -630,6 +647,8 @@ class SharesPage:
         }
 
     def destroy(self):
+
+        self.shares_popup_menu.destroy()
         self.shares_list_view.destroy()
         self.__dict__.clear()
 
@@ -984,6 +1003,11 @@ class IgnoredUsersPage:
             }
         )
 
+        self.ignored_users_popup_menu = PopupMenu(application, self.ignored_users_list_view.widget)
+        self.ignored_users_popup_menu.add_items(
+            ("#" + _("Remove"), self.on_remove_ignored_user)
+        )
+
         self.ignored_ips = {}
         self.ignored_ips_list_view = TreeView(
             application.window, parent=self.ignored_ips_container, multi_select=True,
@@ -1005,6 +1029,15 @@ class IgnoredUsersPage:
             }
         )
 
+        self.ignored_ips_popup_menu = PopupMenu(application, self.ignored_ips_list_view.widget)
+        self.ignored_ips_popup_menu.add_items(
+            ("#" + _("Remove"), self.on_remove_ignored_ip)
+        )
+
+        self.popup_menus = (
+            self.ignored_users_popup_menu, self.ignored_ips_popup_menu
+        )
+
         self.options = {
             "server": {
                 "ignorelist": self.ignored_users_list_view,
@@ -1013,6 +1046,9 @@ class IgnoredUsersPage:
         }
 
     def destroy(self):
+
+        for menu in self.popup_menus:
+            menu.destroy()
 
         self.ignored_users_list_view.destroy()
         self.ignored_ips_list_view.destroy()
@@ -1157,6 +1193,11 @@ class BannedUsersPage:
             }
         )
 
+        self.banned_users_popup_menu = PopupMenu(application, self.banned_users_list_view.widget)
+        self.banned_users_popup_menu.add_items(
+            ("#" + _("Remove"), self.on_remove_banned_user)
+        )
+
         self.banned_ips = {}
         self.banned_ips_list_view = TreeView(
             application.window, parent=self.banned_ips_container, multi_select=True,
@@ -1178,6 +1219,15 @@ class BannedUsersPage:
             }
         )
 
+        self.banned_ips_popup_menu = PopupMenu(application, self.banned_ips_list_view.widget)
+        self.banned_ips_popup_menu.add_items(
+            ("#" + _("Remove"), self.on_remove_banned_ip)
+        )
+
+        self.popup_menus = (
+            self.banned_users_popup_menu, self.banned_ips_popup_menu
+        )
+
         self.options = {
             "server": {
                 "banlist": self.banned_users_list_view,
@@ -1194,6 +1244,9 @@ class BannedUsersPage:
         }
 
     def destroy(self):
+
+        for menu in self.popup_menus:
+            menu.destroy()
 
         self.banned_users_list_view.destroy()
         self.banned_ips_list_view.destroy()
@@ -1368,6 +1421,13 @@ class ChatsPage:
             }
         )
 
+        self.censor_popup_menu = PopupMenu(application, self.censor_list_view.widget)
+        self.censor_popup_menu.add_items(
+            ("#" + _("_Edit…"), self.on_edit_censored),
+            ("", None),
+            ("#" + _("Remove"), self.on_remove_censored)
+        )
+
         self.replacements = {}
         self.replacement_list_view = TreeView(
             application.window, parent=self.replacement_list_container, multi_select=True,
@@ -1387,6 +1447,17 @@ class ChatsPage:
                     "expand_column": True
                 }
             }
+        )
+
+        self.replacement_popup_menu = PopupMenu(application, self.replacement_list_view.widget)
+        self.replacement_popup_menu.add_items(
+            ("#" + _("_Edit…"), self.on_edit_replacement),
+            ("", None),
+            ("#" + _("Remove"), self.on_remove_replacement)
+        )
+
+        self.popup_menus = (
+            self.censor_popup_menu, self.replacement_popup_menu
         )
 
         for widget, name, title in (
@@ -1428,6 +1499,9 @@ class ChatsPage:
         }
 
     def destroy(self):
+
+        for menu in self.popup_menus:
+            menu.destroy()
 
         self.censor_list_view.destroy()
         self.replacement_list_view.destroy()
@@ -2499,8 +2573,16 @@ class UrlHandlersPage:
             }
         )
 
+        self.protocol_popup_menu = PopupMenu(application, self.protocol_list_view.widget)
+        self.protocol_popup_menu.add_items(
+            ("#" + _("_Edit…"), self.on_edit_handler),
+            ("", None),
+            ("#" + _("Remove"), self.on_remove_handler)
+        )
+
     def destroy(self):
 
+        self.protocol_popup_menu.destroy()
         self.file_manager_combobox.destroy()
         self.protocol_list_view.destroy()
 
@@ -2875,10 +2957,20 @@ class PluginsPage:
                 "name_data": {"data_type": GObject.TYPE_STRING, "iterator_key": True}
             }
         )
+
+        self.plugin_popup_menu = PopupMenu(application, self.plugin_list_view.widget, self.on_plugin_popup_menu)
+        self.plugin_popup_menu.add_items(
+            ("=" + _("_Enable"), self.on_toggle_selected_plugin),
+            ("=" + _("_Disable"), self.on_toggle_selected_plugin),
+            ("", None),
+            ("#" + _("_Settings"), self.on_show_plugin_settings)
+        )
+
         self.add_plugins_button.set_visible(not self.application.isolated_mode)
 
     def destroy(self):
 
+        self.plugin_popup_menu.destroy()
         self.plugin_description_view.destroy()
         self.plugin_list_view.destroy()
 
@@ -2917,6 +3009,17 @@ class PluginsPage:
     def check_plugin_settings_button(self, plugin_name):
         self.plugin_settings_button.set_sensitive(bool(core.pluginhandler.get_plugin_metasettings(plugin_name)))
 
+    def on_plugin_popup_menu(self, menu, _widget):
+
+        for iterator in self.plugin_list_view.get_selected_rows():
+            enabled = self.plugin_list_view.get_row_value(iterator, "enabled")
+
+            menu.actions[_("_Enable")].set_enabled(not enabled)
+            menu.actions[_("_Disable")].set_enabled(enabled)
+            break
+
+        menu.actions[_("_Settings")].set_enabled(self.plugin_settings_button.get_sensitive())
+
     def on_select_plugin(self, list_view, iterator):
 
         if iterator is None:
@@ -2948,6 +3051,12 @@ class PluginsPage:
 
         list_view.set_row_value(iterator, "enabled", enabled)
         self.check_plugin_settings_button(plugin_name)
+
+    def on_toggle_selected_plugin(self, *_args):
+
+        for iterator in self.plugin_list_view.get_selected_rows():
+            self.on_toggle_plugin(self.plugin_list_view, iterator)
+            return
 
     def on_toggle_enable_plugins(self, *_args):
 
@@ -3022,8 +3131,8 @@ class Preferences(Dialog):
             height=650,
             show_title_buttons=False
         )
-
         add_css_class(self.widget, "preferences-border")
+        application.add_window(self.widget)
 
         if GTK_API_VERSION == 3:
             # Scroll to focused widgets

pynicotine/gtkgui/dialogs/wishlist.py
@@ -67,7 +67,7 @@ class WishList(Dialog):
         self.popup_menu = PopupMenu(application, self.list_view.widget)
         self.popup_menu.add_items(
             ("#" + _("_Search for Item"), self.on_search_wish),
-            ("#" + _("Edit…"), self.on_edit_wish),
+            ("#" + _("_Edit…"), self.on_edit_wish),
             ("", None),
             ("#" + _("Remove"), self.on_remove_wish)
         )
@@ -80,9 +80,9 @@ class WishList(Dialog):
 
     def destroy(self):
 
+        self.popup_menu.destroy()
         self.wish_combobox.destroy()
         self.list_view.destroy()
-        self.popup_menu.destroy()
 
         super().destroy()
 

commit 11f7cca962b8ace9060fe422474b84de0fb5e76a

dialogs/roomlist.py: fix crash when showing context menu
Regression in 2b826ee1205b9f56dbb7c3431738ba54408dbeba

pynicotine/gtkgui/dialogs/roomlist.py
@@ -253,8 +253,8 @@ class RoomList(Dialog):
         is_private_room_owned = core.chatrooms.is_private_room_owned(room)
         is_private_room_member = core.chatrooms.is_private_room_member(room)
 
-        menu.actions[_("Join Room")].set_enabled(room not in core.chatrooms.joined_rooms)
-        menu.actions[_("Leave Room")].set_enabled(room in core.chatrooms.joined_rooms)
+        menu.actions[_("_Join Room")].set_enabled(room not in core.chatrooms.joined_rooms)
+        menu.actions[_("_Leave Room")].set_enabled(room in core.chatrooms.joined_rooms)
 
         menu.actions[_("Disown Private Room")].set_enabled(is_private_room_owned)
         menu.actions[_("Cancel Room Membership")].set_enabled(is_private_room_member and not is_private_room_owned)