nicotine-plus/nicotine-plus

commit 3aa352ad248e0ed188b98c088c81dbfe692027df

iconnotebook.py: add delay before removing tab highlight (#3455)
* iconnotebook.py: add delay before removing tab highlight
Fixes #3440

* Small cleanups

---------

Co-authored-by: Mat <mail@mathias.is>

pynicotine/gtkgui/widgets/iconnotebook.py
@@ -263,6 +263,7 @@ class IconNotebook:
         self.reorder_page_callback = reorder_page_callback
         self.switch_page_handler = None
         self.reorder_page_handler = None
+        self.switch_page_delay_timer = None
 
         self.pages = {}
         self.tab_labels = {}
@@ -668,6 +669,10 @@ class IconNotebook:
 
     def on_switch_page(self, _notebook, new_page, page_num):
 
+        if self.switch_page_delay_timer is not None:
+            GLib.source_remove(self.switch_page_delay_timer)
+            self.switch_page_delay_timer = None
+
         if self.switch_page_callback is not None:
             self.switch_page_callback(self, new_page, page_num)
 
@@ -683,9 +688,13 @@ class IconNotebook:
         if self.parent_page is None or self.window.current_page_id == self.parent_page.id:
             GLib.idle_add(self.on_focus_page, new_page, priority=GLib.PRIORITY_HIGH_IDLE)
 
-        # Dismiss tab highlight
+        # Dismiss tab highlight after short delay to allow transient switching
         if self.parent_page is not None:
-            self.remove_tab_changed(new_page)
+            self.switch_page_delay_timer = GLib.timeout_add(250, self.on_remove_tab_changed, new_page)
+
+    def on_remove_tab_changed(self, new_page):
+        self.switch_page_delay_timer = None
+        self.remove_tab_changed(new_page)
 
     def on_reorder_page(self, _notebook, page, page_num):
         if self.reorder_page_callback is not None:

commit 8b1e752bbc49613c88146a827d27975acfc86be1

iconnotebook.py: fix possible crash when removing last tab
Fixes #3540

Regression in 3aa352ad248e0ed188b98c088c81dbfe692027df

pynicotine/gtkgui/widgets/iconnotebook.py
@@ -693,8 +693,12 @@ class IconNotebook:
             self.switch_page_delay_timer = GLib.timeout_add(250, self.on_remove_tab_changed, new_page)
 
     def on_remove_tab_changed(self, new_page):
+
         self.switch_page_delay_timer = None
-        self.remove_tab_changed(new_page)
+
+        # Check if tab was removed before timer expired
+        if self.page_num(new_page) != -1:
+            self.remove_tab_changed(new_page)
 
     def on_reorder_page(self, _notebook, page, page_num):
         if self.reorder_page_callback is not None: