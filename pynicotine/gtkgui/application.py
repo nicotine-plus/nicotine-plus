9c27fd285da97cf6b7aa5aa89ee15bacd68c51e8
Mat <mail@mathias.is>
2025-11-28 06:30:54 +0200

    events.py: create scheduler thread during 'start' event
    Otherwise there are cases where the scheduler is never shut down.
    
    Side effect of removing daemon threads in c5ec7ffe59a438b1b109704f641376c2fb1673ed
    
    Fixes #3557

--- pynicotine/gtkgui/application.py
+++ pynicotine/gtkgui/application.py
@@ -977,10 +977,6 @@ class Application:
         from pynicotine.gtkgui.widgets.theme import load_icons
         from pynicotine.gtkgui.widgets.trayicon import TrayIcon
 
-        # Process thread events 10 times per second.
-        # High priority to ensure there are no delays.
-        GLib.timeout_add(100, self.on_process_thread_events, priority=GLib.PRIORITY_HIGH_IDLE)
-
         load_icons()
 
         self._set_up_actions()
@@ -991,6 +987,10 @@ class Application:
         self.window = MainWindow(self)
 
         core.start()
+
+        # Process thread events 10 times per second.
+        # High priority to ensure there are no delays.
+        GLib.timeout_add(100, self.on_process_thread_events, priority=GLib.PRIORITY_HIGH_IDLE)
 
         if config.sections["server"]["auto_connect_startup"]:
             core.connect()
