nicotine-plus/nicotine-plus

commit fdfd39a0c37ea92ef7116828314f58659f2163c7

GUI: insert file lists in reverse to preserve correct order
Since our treeviews always prepend rows for performance reasons,
the rows will appear in the wrong order if we sort by another
column than the default at initialization (e.g. when restoring
a user's preferred sort column). Reverse the file lists before
inserting the rows to ensure they're stored in the correct order
in the model.

Fixes #3372

pynicotine/gtkgui/search.py
@@ -10,6 +10,7 @@ import operator
 import os
 import re
 
+from collections import deque
 from itertools import islice
 
 from gi.repository import GLib
@@ -801,9 +802,15 @@ class Search:
 
         update_ui = False
         search = core.search.searches[self.token]
-        row_id = 0
+        dummy_row_id = 0
 
-        for _code, file_path, size, _ext, file_attributes, *_unused in result_list:
+        # For performance reasons, we prepend rows to our treeview. Since we reverse the result
+        # lists before inserting the rows, we need to decrement the new row ids to preserve the
+        # correct sort order. We don't know how many folder parent rows will be added yet (if
+        # any), so set the end row id to the maximum possible number of new rows.
+        end_row_id = self.row_id = self.row_id + (len(result_list) * 2) + 1
+
+        for _code, file_path, size, _ext, file_attributes, *_unused in reversed(result_list):
             if self.num_results_found >= config.sections["searches"]["max_displayed_results"]:
                 break
 
@@ -859,13 +866,14 @@ class Search:
                     length,
                     not is_private,
                     SearchResultFile(file_path, file_attributes),
-                    row_id
+                    dummy_row_id
                 ]
             )
 
             if is_result_visible:
                 update_ui = True
 
+        self.row_id = end_row_id
         return update_ui
 
     def file_search_response(self, msg):
@@ -985,8 +993,8 @@ class Search:
                 if expand_allowed:
                     expand_user = self.grouping_mode == "folder_grouping" or self.expand_button.get_active()
 
-                self.row_id += 1
-                self.users[user] = (iterator, [])
+                self.row_id -= 1
+                self.users[user] = (iterator, deque())
 
             user_iterator, user_child_iterators = self.users[user]
 
@@ -1022,8 +1030,8 @@ class Search:
                     )
                     user_child_iterators.append(iterator)
                     expand_folder = expand_allowed and self.expand_button.get_active()
-                    self.row_id += 1
-                    self.folders[user_folder_path] = (iterator, [])
+                    self.row_id -= 1
+                    self.folders[user_folder_path] = (iterator, deque())
 
                 row = row[:]
                 row[4] = row[5] = ""  # Folder not visible for file row if "group by folder" is enabled
@@ -1036,18 +1044,18 @@ class Search:
 
         else:
             if user not in self.users:
-                self.users[user] = (None, [])
+                self.users[user] = (None, deque())
 
             user_iterator, user_child_iterators = self.users[user]
 
         row[18] = self.row_id
         iterator = self.tree_view.add_row(row, select_row=False, parent_iterator=parent_iterator)
-        self.row_id += 1
+        self.row_id -= 1
 
         if user_folder_child_iterators is not None:
-            user_folder_child_iterators.append(iterator)
+            user_folder_child_iterators.appendleft(iterator)
         else:
-            user_child_iterators.append(iterator)
+            user_child_iterators.appendleft(iterator)
 
         if expand_user:
             self.tree_view.expand_row(user_iterator)

pynicotine/gtkgui/dialogs/download.py
@@ -3,6 +3,7 @@
 
 import os
 
+from collections import deque
 from operator import itemgetter
 
 from gi.repository import GLib
@@ -180,9 +181,9 @@ class Download(Dialog):
         self.select_all = select_all
         has_unselected_files = False
 
-        for username, file_path, size, file_attributes, selected, root_folder_path in sorted(
+        for username, file_path, size, file_attributes, selected, root_folder_path in reversed(sorted(
             data, key=lambda x: len(x[0])
-        ):
+        )):
             if username + file_path in self.tree_view.iterators:
                 continue
 
@@ -243,7 +244,7 @@ class Download(Dialog):
                 )
                 expand_parent = True
 
-                self.parent_iterators[username + folder_path] = (parent_iterator, [])
+                self.parent_iterators[username + folder_path] = (parent_iterator, deque())
                 self.initial_selected_iterators.add(parent_iterator)
 
             parent_iterator, child_iterators = self.parent_iterators[username + folder_path]
@@ -261,7 +262,7 @@ class Download(Dialog):
                 ],
                 select_row=False, parent_iterator=parent_iterator
             )
-            child_iterators.append(iterator)
+            child_iterators.appendleft(iterator)
             self.num_files[username][folder_path] += 1
 
             if selected:
@@ -418,7 +419,7 @@ class Download(Dialog):
             parent_iterator, child_iterators = self.parent_iterators[username + folder_path]
             unselected_parent = False
 
-            for _code, file_name, size, _ext, file_attributes, *_unused in files:
+            for _code, file_name, size, _ext, file_attributes, *_unused in reversed(files):
                 file_path = "\\".join([folder_path, file_name])
 
                 if username + file_path in self.tree_view.iterators:
@@ -438,7 +439,7 @@ class Download(Dialog):
                     ],
                     select_row=False, parent_iterator=parent_iterator
                 )
-                child_iterators.append(iterator)
+                child_iterators.appendleft(iterator)
                 self.num_files[username][folder_path] += 1
                 has_added_file = True
 

pynicotine/gtkgui/userbrowse.py
@@ -777,7 +777,7 @@ class UserBrowse:
         # Temporarily disable sorting for increased performance
         self.file_list_view.freeze()
 
-        for _code, basename, size, _ext, file_attributes, *_unused in files:
+        for _code, basename, size, _ext, file_attributes, *_unused in reversed(files):
             h_size = human_size(size, config.sections["ui"]["file_size_unit"])
             h_quality, bitrate, h_length, length = FileListMessage.parse_audio_quality_length(size, file_attributes)
 

commit 0d4cb97d6e37de5f6b2639055554784e8ffcd333

search.py: fix crash when refiltering results
Regression in fdfd39a0c37ea92ef7116828314f58659f2163c7

pynicotine/gtkgui/search.py
@@ -873,7 +873,7 @@ class Search:
             if is_result_visible:
                 update_ui = True
 
-        self.row_id = end_row_id
+        self.row_id = end_row_id + 1
         return update_ui
 
     def file_search_response(self, msg):
@@ -1310,11 +1310,19 @@ class Search:
 
     def update_model(self):
 
+        # For performance reasons, we prepend rows to our treeview. Since we reverse the result
+        # lists before inserting the rows, we need to decrement the new row ids to preserve the
+        # correct sort order. We don't know how many user/folder parent rows will be added yet (if
+        # any), so set the end row id to the maximum possible number of new rows.
+        end_row_id = self.row_id = self.row_id + len(self.all_data) * 3
+
         self.tree_view.freeze()
 
         for row in self.all_data:
             if self.check_filter(row):
                 self.add_row_to_model(row)
+
+        self.row_id = end_row_id + 1
 
         # Update number of results
         self.update_result_counter()