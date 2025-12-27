nicotine-plus/nicotine-plus

commit a960b38bfc2272d543fd253412666e7edf7493c3

preferences.py: add icon column for readability of shared folder (#3568)
This mirrors the information in the "Unavailable Shares" dialog when
rescanning.

pynicotine/gtkgui/dialogs/preferences.py
@@ -640,14 +640,20 @@ class SharesPage:
                 "virtual_name": {
                     "column_type": "text",
                     "title": _("Virtual Folder"),
-                    "width": 65,
+                    "width": 55,
                     "expand_column": True,
                     "default_sort_type": "ascending"
+                },
+                "readable": {
+                    "column_type": "icon",
+                    "title": _("Readable"),
+                    "width": 20,
+                    "hide_header": True
                 },
                 "folder": {
                     "column_type": "text",
                     "title": _("Folder"),
-                    "width": 150,
+                    "width": 180,
                     "expand_column": True
                 },
                 "accessible_to": {
@@ -740,17 +746,23 @@ class SharesPage:
         self.trusted_shared_folders = config.sections["transfers"]["trustedshared"][:]
         self.share_filters = config.sections["transfers"]["share_filters"][:]
 
+        unreadable_icon = "dialog-warning-symbolic"
+        unreadable_shares = core.shares.check_shares_available()
+
         for virtual_name, folder_path, *_unused in self.shared_folders:
+            icon = unreadable_icon if (virtual_name, folder_path) in unreadable_shares else ""
             self.shares_list_view.add_row(
-                [virtual_name, folder_path, _("Public")], select_row=False)
+                [virtual_name, icon, folder_path, _("Public")], select_row=False)
 
         for virtual_name, folder_path, *_unused in self.buddy_shared_folders:
+            icon = unreadable_icon if (virtual_name, folder_path) in unreadable_shares else ""
             self.shares_list_view.add_row(
-                [virtual_name, folder_path, _("Buddies")], select_row=False)
+                [virtual_name, icon, folder_path, _("Buddies")], select_row=False)
 
         for virtual_name, folder_path, *_unused in self.trusted_shared_folders:
+            icon = unreadable_icon if (virtual_name, folder_path) in unreadable_shares else ""
             self.shares_list_view.add_row(
-                [virtual_name, folder_path, _("Trusted")], select_row=False)
+                [virtual_name, icon, folder_path, _("Trusted")], select_row=False)
 
         for sfilter in self.share_filters:
             applies_to = _("Folders") if sfilter.endswith("\\") else _("Files")
@@ -788,7 +800,9 @@ class SharesPage:
                 continue
 
             self.last_parent_folder = os.path.dirname(folder_path)
-            self.shares_list_view.add_row([virtual_name, folder_path, _("Public")])
+
+            empty_icon_name = ""
+            self.shares_list_view.add_row([virtual_name, folder_path, empty_icon_name, _("Public")])
 
     def on_add_shared_folder(self, *_args):
 
@@ -824,6 +838,7 @@ class SharesPage:
             return
 
         folder_path = self.shares_list_view.get_row_value(iterator, "folder")
+        readable = self.shares_list_view.get_row_value(iterator, "readable")
         permission_level = self.PERMISSION_LEVELS.get(new_accessible_to)
         orig_iterator = self.shares_list_view.iterators[virtual_name]
 
@@ -837,7 +852,7 @@ class SharesPage:
             validate_path=False
         )
 
-        self.shares_list_view.add_row([new_virtual_name, folder_path, new_accessible_to_short])
+        self.shares_list_view.add_row([new_virtual_name, folder_path, readable, new_accessible_to_short])
 
     def on_edit_shared_folder(self, *_args):
 

pynicotine/gtkgui/widgets/theme.py
@@ -260,6 +260,9 @@ FILE_TYPE_ICON_LABELS = {
 PRIVATE_ICON_LABELS = {
     "changes-prevent-symbolic": _("Private")
 }
+SHARED_FOLDER_ICON_LABELS = {
+    "dialog-warning-symbolic": _("Unreadable Folder")
+}
 USER_STATUS_ICON_LABELS = {
     "nplus-status-available": _("Online"),
     "nplus-status-away": _("Away"),

pynicotine/gtkgui/widgets/treeview.py
@@ -23,6 +23,7 @@ from pynicotine.gtkgui.widgets.accelerator import Accelerator
 from pynicotine.gtkgui.widgets.popupmenu import PopupMenu
 from pynicotine.gtkgui.widgets.theme import FILE_TYPE_ICON_LABELS
 from pynicotine.gtkgui.widgets.theme import PRIVATE_ICON_LABELS
+from pynicotine.gtkgui.widgets.theme import SHARED_FOLDER_ICON_LABELS
 from pynicotine.gtkgui.widgets.theme import USER_STATUS_ICON_LABELS
 from pynicotine.gtkgui.widgets.theme import add_css_class
 
@@ -709,6 +710,9 @@ class TreeView:
 
         if column.id == "file_type":
             return FILE_TYPE_ICON_LABELS[icon_name]
+
+        if column.id == "readable":
+            return SHARED_FOLDER_ICON_LABELS.get(icon_name, "")
 
         return icon_name
 

commit 9e855dee16c396de3887947fb9d6482a014bf700

preferences.py: fix order of added row values
Regression in a960b38bfc2272d543fd253412666e7edf7493c3

pynicotine/gtkgui/dialogs/preferences.py
@@ -802,7 +802,7 @@ class SharesPage:
             self.last_parent_folder = os.path.dirname(folder_path)
 
             empty_icon_name = ""
-            self.shares_list_view.add_row([virtual_name, folder_path, empty_icon_name, _("Public")])
+            self.shares_list_view.add_row([virtual_name, empty_icon_name, folder_path, _("Public")])
 
     def on_add_shared_folder(self, *_args):
 
@@ -852,7 +852,7 @@ class SharesPage:
             validate_path=False
         )
 
-        self.shares_list_view.add_row([new_virtual_name, folder_path, readable, new_accessible_to_short])
+        self.shares_list_view.add_row([new_virtual_name, readable, folder_path, new_accessible_to_short])
 
     def on_edit_shared_folder(self, *_args):
 