a9fb1e9dd44bf3ffff83707a5af051cf0d573429
Mat <mail@mathias.is>
2026-01-12 13:44:46 +0200

    init.py: bump copyright year

--- pynicotine/__init__.py
+++ pynicotine/__init__.py
@@ -1,4 +1,4 @@
-# COPYRIGHT (C) 2020-2025 Nicotine+ Contributors
+# COPYRIGHT (C) 2020-2026 Nicotine+ Contributors
 #
 # GNU GENERAL PUBLIC LICENSE
 #    Version 3, 29 June 2007
@@ -20,7 +20,7 @@ __application_name__ = "Nicotine+"
 __application_id__ = "org.nicotine_plus.Nicotine"
 __version__ = "3.3.11.dev1"
 __author__ = "Nicotine+ Team"
-__copyright__ = """© 2004–2025 Nicotine+ Contributors
+__copyright__ = """© 2004–2026 Nicotine+ Contributors
 © 2003–2004 Nicotine Contributors
 © 2001–2003 PySoulSeek Contributors"""
 __website_url__ = "https://nicotine-plus.org"
