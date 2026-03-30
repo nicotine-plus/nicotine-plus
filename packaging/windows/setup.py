7e2d5cb2338fe140ddadfb94e988f060199e9f05
Mat <mail@mathias.is>
2026-03-30 08:40:28 +0300

    macos/setup.py: fix svg loader path

--- packaging/windows/setup.py
+++ packaging/windows/setup.py
@@ -127,14 +127,17 @@ def add_pixbuf_loaders():
 
     for image_format in image_formats:
         basename = f"libpixbufloader-{image_format}"
+        real_loader_extension = loader_extension
 
         if image_format == "svg":
             if sys.platform == "win32":
                 basename = basename.replace("lib", "")
+            elif sys.platform == "darwin":
+                real_loader_extension = "dylib"
             basename = basename.replace("-", "_")
 
         add_file(
-            file_path=os.path.realpath(os.path.join(pixbuf_loaders_path, f"{basename}.{loader_extension}")),
+            file_path=os.path.realpath(os.path.join(pixbuf_loaders_path, f"{basename}.{real_loader_extension}")),
             output_path=f"lib/libpixbufloader-{image_format}.{loader_extension}"
         )
 
