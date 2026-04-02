bed5b7aac98cd276ca1183536d2b9f20014d366e
Mat <mail@mathias.is>
2026-04-02 05:40:05 +0300

    macos/setup.py: unset bin_excludes
    The application fails to start otherwise. This brings back the
    large ICU dependency, but we'll have to deal with that some other
    way.

--- packaging/windows/setup.py
+++ packaging/windows/setup.py
@@ -43,14 +43,12 @@ if sys.platform == "win32":
     UNAVAILABLE_MODULES = [
         "fcntl", "grp", "nis", "ossaudiodev", "posix", "pwd", "readline", "resource", "spwd", "syslog", "termios"
     ]
-    BIN_EXCLUDES = []
     ICON_NAME = "icon.ico"
 
 elif sys.platform == "darwin":
     SYS_BASE_PATH = sys.prefix
     LIB_PATH = os.path.join(SYS_BASE_PATH, "lib")
     UNAVAILABLE_MODULES = ["msvcrt", "nt", "nturl2path", "ossaudiodev", "spwd", "winreg", "winsound"]
-    BIN_EXCLUDES = ["libappstream*.dylib"]
     ICON_NAME = "icon.icns"
 
 else:
@@ -253,7 +251,6 @@ setup(
             "packages": INCLUDED_MODULES,
             "excludes": EXCLUDED_MODULES,
             "include_files": include_files,
-            "bin_excludes": BIN_EXCLUDES,
             "zip_include_packages": ["*"],
             "zip_exclude_packages": [MODULE_NAME],
             "optimize": 2
