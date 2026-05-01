e5b2e40b7706d85bdcf5053a2caff4a791ab30ed
Sourcerer2 <hackimack@tutanota.com>
2026-05-01 09:05:13 +0200

    Update notifications.py

--- pynicotine/notifications.py
+++ pynicotine/notifications.py
@@ -7,20 +7,20 @@ from pynicotine.events import events
 class Notifications:
     __slots__ = ()
 
-    def show_notification(self, message, title=None):
+    def show_notification(self, message, title=None) -> None:
         events.emit("show-notification", message, title=title)
 
-    def show_chatroom_notification(self, room, message, title=None, high_priority=False):
+    def show_chatroom_notification(self, room, message, title=None, high_priority=False) -> None:
         events.emit("show-chatroom-notification", room, message, title=title, high_priority=high_priority)
 
-    def show_download_notification(self, message, title=None, high_priority=False):
+    def show_download_notification(self, message, title=None, high_priority=False) -> None:
         events.emit("show-download-notification", message, title=title, high_priority=high_priority)
 
-    def show_private_chat_notification(self, username, message, title=None):
+    def show_private_chat_notification(self, username, message, title=None) -> None:
         events.emit("show-private-chat-notification", username, message, title=title)
 
-    def show_search_notification(self, search_token, message, title=None):
+    def show_search_notification(self, search_token, message, title=None) -> None:
         events.emit("show-search-notification", search_token, message, title=title)
 
-    def show_upload_notification(self, message, title=None):
+    def show_upload_notification(self, message, title=None) -> None:
         events.emit("show-upload-notification", message, title=title)
