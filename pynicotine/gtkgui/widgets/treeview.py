d3006ada15087a54a5775f3569d5a365b0e17e95
Mat <mail@mathias.is>
2026-07-25 15:55:43 +0300

    treeview.py: add empty strings to model again
    Otherwise we have get None values, which can cause all sorts of
    problems with crashes with operations that expect strings.

--- pynicotine/gtkgui/widgets/treeview.py
+++ pynicotine/gtkgui/widgets/treeview.py
@@ -534,8 +534,7 @@ class TreeView:
         included_values = []
 
         for index, value in enumerate(values):
-            if not value and index is not self._sort_column:
-                # Skip empty values if not active sort column to avoid unnecessary work
+            if value is None:
                 continue
 
             if index in self._column_gvalues:
